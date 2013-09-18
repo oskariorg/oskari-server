8aa13d45d19ef38a69a4654432608a2b8fc6bd46
Sami Mäkinen <sami.makinen@nls.fi>
2013-09-18 12:58:57 +0300

    [AH-158] Added delete analysis action route

--- service-map/src/main/java/fi/nls/oskari/map/analysis/service/AnalysisDbService.java
+++ service-map/src/main/java/fi/nls/oskari/map/analysis/service/AnalysisDbService.java
@@ -3,6 +3,7 @@ package fi.nls.oskari.map.analysis.service;
 import java.util.List;
 
 import fi.nls.oskari.domain.map.analysis.Analysis;
+import fi.nls.oskari.service.ServiceException;
 import fi.nls.oskari.service.db.BaseService;
 
 public interface AnalysisDbService extends BaseService<Analysis> {    
@@ -12,8 +13,7 @@ public interface AnalysisDbService extends BaseService<Analysis> {
         public int updateAnalysisCols(final Analysis analysis);
         public Analysis getAnalysisById(long id);
         public List<Analysis> getAnalysisByUid(String uid);
-      
-       
-        
+        public void deleteAnalysisById(final long id) throws ServiceException;
+        public void deleteAnalysis(final Analysis analysis) throws ServiceException;
 
 }
