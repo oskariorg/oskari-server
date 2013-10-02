e5fa8ab44743bd889d80887c4c66b13c34f39fad
Sami Mäkinen <sami.makinen@nls.fi>
2013-10-02 11:33:47 +0300

    [AH-312] Fixed permissions sql/docs to match updated db

--- servlet-map/src/main/resources/sql/PostgreSQL/example-wfslayer.sql
+++ servlet-map/src/main/resources/sql/PostgreSQL/example-wfslayer.sql
@@ -2,6 +2,7 @@
 -- http://www.suomi.fi/suomifi/tyohuone/yhteiset_palvelut/palvelukartta/palvelukartan_rajapintakuvaus/palvelukartan_aineiston_kayttoehdot/index.html ;
 -- THIS IS AN EXAMPLE FOR ADDING WFS LAYER ;
 
+-- add map layer;
 INSERT INTO portti_maplayer(
   layerclassid, wmsname, wmsurl, opacity,
   minscale, maxscale, inspire_theme_id,
@@ -12,10 +13,10 @@ INSERT INTO portti_maplayer(
           1, 'wfslayer','',
           -1, '1.3.0', 3067, '{fi:{name:"Palvelupisteiden kyselypalvelu", subtitle:""},sv:{name:"Söktjänst för serviceställen", subtitle:""},en:{name:"Public services query service", subtitle:""}}');
 
+-- add wfs specific layer data;
 INSERT INTO portti_wfs_layer(
   maplayer_id,
   layer_name,
-  locales,
   url,
   username, "password",
   gml_geometry_property, gml_version, wfs_version, max_features,
@@ -25,17 +26,18 @@ INSERT INTO portti_wfs_layer(
   feature_namespace_uri, tile_request, gml2_separator)
   VALUES ((select id from portti_maplayer where wmsname = 'palvelupisteiden_kyselypalvelu'),
           'palvelupisteiden_kyselypalvelu',
-          '{fi:{name:"Palvelupisteiden kyselypalvelu", subtitle:""},sv:{name:"Söktjänst för serviceställen", subtitle:""},en:{name:"Public services query service", subtitle:""}}',
           'http://kartta.suomi.fi/geoserver/wfs',
           '','',
           'the_geom','3.1.1','1.1.0',100,
-          'pkartta','{}',
-          '[]',
+          'pkartta','{}','{}',
           '{}','2d',
           true,true,'EPSG:3067','toimipaikat',
           'www.pkartta.fi',false, false);
 
-INSERT INTO portti_resource_user (resource_name, resource_namespace, resource_type, externalid, externalid_type) values
-('palvelupisteiden_kyselypalvelu', 'wfs', 'WMS_LAYER', 10110, 'ROLE');
+-- add layer as resource for mapping permissions;
+INSERT INTO oskari_resource(resource_type, resource_mapping) values ('wfs_layer', 'wfs+palvelupisteiden_kyselypalvelu');
 
-INSERT INTO portti_permissions (resource_user_id, permissions_type) values ((SELECT MAX(id) FROM portti_resource_user), 'VIEW_LAYER');
+-- give view_layer permission for the resource to ROLE 10110 (guest);
+INSERT INTO oskari_permission(oskari_resource_id, external_type, permission, external_id) values
+((SELECT id FROM oskari_resource WHERE resource_type = 'wfs_layer' AND resource_mapping = 'wfs+palvelupisteiden_kyselypalvelu'), 'ROLE', 'VIEW_LAYER', 10110);
+
