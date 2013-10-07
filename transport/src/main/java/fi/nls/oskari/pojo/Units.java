d9b27b4978f6a702dc7672c51df86951adf8c324
mrLite <aku.peltonen@nls.fi>
2013-10-07 14:32:26 +0300

    [AH-152] tests for units.java

--- transport/src/main/java/fi/nls/oskari/pojo/Units.java
+++ transport/src/main/java/fi/nls/oskari/pojo/Units.java
@@ -3,23 +3,60 @@ package fi.nls.oskari.pojo;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import org.geotools.referencing.CRS;
-import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
-import javax.measure.unit.Unit;
+import javax.measure.unit.*;
 import java.util.HashMap;
 
 public class Units {
     private static final Logger log = LogFactory.getLogger(Units.class);
-    private HashMap<String, Double> inchesPerUnits = new HashMap<String, Double>();
+    private HashMap<String, Double> metersPerUnits = new HashMap<String, Double>();
 
     public Units() {
-        inchesPerUnits.put("m", 39.3701d);          // Meters
-        inchesPerUnits.put("Meter", 39.3701d);      // Meters
-        inchesPerUnits.put("dd", 4374754.0d);       // Degrees
-        inchesPerUnits.put("degrees", 4374754.0d);  // Degrees
+        metersPerUnits.put("in", 0.0254d);                      // Inches
+        metersPerUnits.put("inches", 0.0254d);                  // Inches
+        metersPerUnits.put("Inch", 0.0254d);                    // Inches
+        metersPerUnits.put("m", 1.0d);                          // Meters
+        metersPerUnits.put("metre", 1.0d);                      // Meters
+        metersPerUnits.put("Meter", 1.0d);                      // Meters
+        metersPerUnits.put("°", 0.01745329251994328d);         // Degrees
+        metersPerUnits.put("dd", 0.01745329251994328d);        // Degrees
+        metersPerUnits.put("degree", 0.01745329251994328d);    // Degrees
+        metersPerUnits.put("degrees", 0.01745329251994328d);   // Degrees
+        metersPerUnits.put("degree_angle", 0.01745329251994328d); // Degrees
+        metersPerUnits.put("ft", 0.3048d);                      // Feet
+        metersPerUnits.put("foot", 0.3048d);                    // Feet
+        metersPerUnits.put("Foot", 0.3048d);                    // Feet
+        metersPerUnits.put("foot_survey_us", 0.3048d);          // Feet
+        metersPerUnits.put("mi", 1609.344d);                    // Miles
+        metersPerUnits.put("Mile", 1609.344d);                  // Miles
+        metersPerUnits.put("km", 1000.0d);                      // Kilometers
+        metersPerUnits.put("kilometer", 1000.0d);               // Kilometers
+        metersPerUnits.put("kilometre", 1000.0d);               // Kilometers
+        metersPerUnits.put("Kilometer", 1000.0d);               // Kilometers
+        metersPerUnits.put("yd", 0.914399204289812d);           // Yards
+        metersPerUnits.put("Yard", 0.914399204289812d);         // Yards
+        metersPerUnits.put("yard", 0.914399204289812d);         // Yards
+        metersPerUnits.put("nmi", 1852.0d);                     // Nautical miles
+        metersPerUnits.put("kmi", 1852.0d);                     // Nautical miles
+        metersPerUnits.put("NautM", 1852.0d);                   // Nautical miles
+        metersPerUnits.put("fathom", 1.8288d);                  // International fathom
+        metersPerUnits.put("Fathom", 1.8288d);                  // International fathom
+        metersPerUnits.put("fath", 1.8288d);                    // International fathom
+        metersPerUnits.put("gradians", 0.0157079633267949d);    // Gradians
+        metersPerUnits.put("grad", 0.0157079633267949d);        // Gradians
+        metersPerUnits.put("gr", 0.0157079633267949d);          // Gradians
     }
 
+    /**
+     * Returns the scale transformed from one SRS to another,
+     * or the scale given as an argument if the transformation
+     * fails or if one of the units is unknown.
+     *
+     * @param scale
+     * @param fromSrs
+     * @param toSrs
+     */
     public double getScaleInSrs(double scale, String fromSrs, String toSrs) {
         CoordinateReferenceSystem fromCrs;
         CoordinateReferenceSystem toCrs;
@@ -27,33 +64,32 @@ public class Units {
         Unit<?> toCrsUnit;
         String fromCrsUnitString;
         String toCrsUnitString;
-        double normalizedScale = ((scale > 1.0) ? (1.0 / scale) : scale);
-        double returnScale;
-
-        log.debug("UNITS.JAVA: Scale", scale);
-        log.debug("UNITS.JAVA: Normalized scale", normalizedScale);
+        double toUnitFactor;
+        double fromUnitFactor;
 
         try {
             fromCrs = CRS.decode(fromSrs);
             toCrs = CRS.decode(toSrs);
             fromCrsUnit = fromCrs.getCoordinateSystem().getAxis(0).getUnit();
             toCrsUnit = toCrs.getCoordinateSystem().getAxis(0).getUnit();
-            fromCrsUnitString = fromCrsUnit.toString();
             toCrsUnitString = toCrsUnit.toString();
-            log.debug("UNITS.JAVA: from crs", fromCrsUnitString, "to crs", toCrsUnitString);
+            fromCrsUnitString = fromCrsUnit.toString();
 
-            if (inchesPerUnits.get(toCrsUnitString) == null || inchesPerUnits.get(fromCrsUnitString) == null) {
-                log.warn("Did not know units", toCrsUnitString, fromCrsUnitString);
+            if (metersPerUnits.get(toCrsUnitString) == null) {
+                log.warn("Unknown unit", toCrsUnitString);
                 return scale;
             }
+            toUnitFactor = metersPerUnits.get(toCrsUnitString);
 
-            double toUnitFactor = inchesPerUnits.get(toCrsUnitString);
-            double fromUnitFactor = inchesPerUnits.get(fromCrsUnitString);
-            returnScale = toUnitFactor / (normalizedScale * fromUnitFactor);
-            log.debug("UNITS.JAVA: to unit factor", toUnitFactor);
-            log.debug("UNITS.JAVA: from unit factor", fromUnitFactor);
-            log.debug("UNITS.JAVA: return scale", returnScale);
-            return returnScale;
+            if (metersPerUnits.get(fromCrsUnitString) == null) {
+                log.warn("Unknown unit", fromCrsUnitString);
+                return scale;
+            }
+            fromUnitFactor = metersPerUnits.get(fromCrsUnitString);
+
+            log.debug("Units: from unit", fromCrsUnitString, ", to unit", toCrsUnitString);
+
+            return scale * toUnitFactor / fromUnitFactor;
         } catch (Exception e) {
             log.error(e, "CRS decoding failed");
             return scale;
