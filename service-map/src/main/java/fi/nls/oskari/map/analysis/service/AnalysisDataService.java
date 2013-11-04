6cf946e00ea7a1b4f6a074788d9fa1657995a7f0
Tapio Keisteri <tapio.keisteri@nls.fi>
2013-11-04 11:01:06 +0200

    [AH-422] aggregate to aggregate-union

--- service-map/src/main/java/fi/nls/oskari/map/analysis/service/AnalysisDataService.java
+++ service-map/src/main/java/fi/nls/oskari/map/analysis/service/AnalysisDataService.java
@@ -63,33 +63,25 @@ public class AnalysisDataService {
     private static final String LAYER_PREFIX = "analysis_";
     private static final String ANALYSIS_BASELAYER_ID = "analysis.baselayer.id";
     private static final String ANALYSIS_RENDERING_URL = "analysis.rendering.url";
+    private static final String ANALYSIS_RENDERING_ELEMENT = "analysis.rendering.element";
     private static final String ANALYSIS_ORGNAME = ""; // managed in front
     private static final String ANALYSIS_INSPIRE = ""; // managed in front
-    private static final String ANALYSIS_WPS_RENDERING_ELEMENT_NAME = "ana:analysis_data_style";
+
     private static final String ANALYSIS_GEOMETRY_FIELD = "geometry";
-    private static final List<String> HIDDEN_FIELDS = Arrays.asList("analysis_id");
+
     private static final String NUMERIC_FIELD_TYPE = "numeric";
     private static final String STRING_FIELD_TYPE = "string";
 
     private static final Logger log = LogFactory
             .getLogger(AnalysisDataService.class);
-    private static final String WFSTTEMPLATESTART = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-            + "<wfs:Transaction xmlns:wfs=\"http://www.opengis.net/wfs\" service=\"WFS\" version=\"1.1.0\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:cgf=\"http://www.opengis.net/cite/geometry\" >\n";
-
-    private static final String WFSTTEMPLATEEND = "</wfs:Transaction>\n";
-
-    private static final String WFSTINSERTSTART = "   <wfs:Insert>\n"
-            + "       <feature:analysis_data xmlns:feature=\"http://nls.paikkatietoikkuna.fi/analysis\">\n";
-
-    private static final String WFSTINSERTEND = "       </feature:analysis_data>\n"
-            + "   </wfs:Insert>\n";
 
     private static final AnalysisStyleDbService styleService = new AnalysisStyleDbServiceIbatisImpl();
     private static final AnalysisDbService analysisService = new AnalysisDbServiceIbatisImpl();
+    private static final TransformationService transformationService = new TransformationService();
 
     final String analysisBaseLayerId = PropertyUtil.get(ANALYSIS_BASELAYER_ID);
-    final String analysisRenderingUrl = PropertyUtil
-            .get(ANALYSIS_RENDERING_URL);
+    final String analysisRenderingUrl = PropertyUtil.get(ANALYSIS_RENDERING_URL);
+    final String analysisRenderingElement = PropertyUtil.get(ANALYSIS_RENDERING_ELEMENT);
 
     public Analysis storeAnalysisData(final String featureset,
             AnalysisLayer analysislayer, String json, User user) {
@@ -131,14 +123,14 @@ public class AnalysisDataService {
             // ----------------------------------
             final AnalysisMethodParams params = analysislayer
                     .getAnalysisMethodParams();
-            final String geometryProperty = this.stripNamespace(params.getGeom());
+            final String geometryProperty = transformationService.stripNamespace(params.getGeom());
             // FIXME: wpsToWfst populates fields list AND returns the wfst
             // payload
             // this should be refactored so it returns an object with the fields
             // list and the payload
             // and remove the fields parameter from call
             List<String> fields = new ArrayList<String>();
-            final String wfst = wpsToWfst(featureset, analysis.getUuid(),
+            final String wfst = transformationService.wpsFeatureCollectionToWfst(featureset, analysis.getUuid(),
                     analysis.getId(), fields, analysislayer.getFieldtypeMap(), geometryProperty);
             log.debug("Produced WFS-T:\n" + wfst);
 
@@ -179,153 +171,6 @@ public class AnalysisDataService {
         }
 
         return analysis;
-    }
-
-    private String wpsToWfst(String wps, String uuid, long analysis_id,
-            List<String> fields, Map<String,String> fieldTypes, String geometryProperty)
-            throws ServiceException {
-
-        final Document wpsDoc = createDoc(wps);
-        StringBuilder sb = new StringBuilder(WFSTTEMPLATESTART);
-        List<String> cols = new ArrayList<String>();
-        List<String> geomcols = new ArrayList<String>();
-        geomcols.add("feature:" + geometryProperty);
-        geomcols.add("feature:geometry");
-
-        String geomcol = "";
-
-        // iterate through wpsDoc's gml:featureMember elements
-        // NodeList featureMembers =
-        // wpsDoc.getDocumentElement().getChildNodes();
-        NodeList featureMembers = wpsDoc
-                .getElementsByTagName("gml:featureMember");
-        for (int i = 0; i < featureMembers.getLength(); i++) {
-            // we're only interested in featureMembers...
-            if (!"gml:featureMember".equals(featureMembers.item(i)
-                    .getNodeName())) {
-                continue;
-            }
-            // get features, i.e. all child elements in feature namespace
-            // we trust that featureMember only has one feature...
-            // find the child feature...
-            NodeList meh = featureMembers.item(i).getChildNodes();
-            NodeList features = null;
-            for (int j = 0; j < meh.getLength(); j++) {
-                if (meh.item(j).getNodeName().indexOf("feature:") == 0) {
-                    features = meh.item(j).getChildNodes();
-                    break;
-                }
-            }
-            Node geometry = null;
-            List<String> textFeatures = new ArrayList<String>();
-            List<Double> numericFeatures = new ArrayList<Double>();
-            int ncount = 1;
-            int tcount = 1;
-            for (int j = 0; j < features.getLength(); j++) {
-                Node feature = features.item(j);
-
-                // it's a feature, check if it's geometry
-                if (geomcols.contains(feature.getNodeName())) {
-                    // geometry, store aside for now
-                    geomcol = feature.getNodeName();
-                    geometry = feature;
-                } else if (feature.getNodeName().indexOf("feature:") == 0) {
-                    // only parse 8 first text ( numeric results invalid behavior later use only text) 
-                    //TODO: fix management of Date dateTime types later
-                    // (excluding geometry)
-                    if (textFeatures.size() < 8 && numericFeatures.size() < 8 && this.isHiddenField(feature) == false) {
-                        // get node value
-                        String strVal = feature.getTextContent();
-                        Double dblVal = null;
-                        String col = this.stripNamespace(feature.getNodeName());
-                        dblVal = this.getFieldAsNumeric(col, strVal, fieldTypes);
-
-                        if (null != dblVal) {
-                            numericFeatures.add(dblVal);
-                            if (!cols.contains(col)) {
-                                String colmap = "n" + Integer.toString(ncount)
-                                        + "=" + col;
-                                cols.add(col);
-                                fields.add(colmap);
-                                ncount++;
-                            }
-                        } else {
-                            textFeatures.add(strVal);
-                            if (!cols.contains(col)) {
-                                String colmap = "t" + Integer.toString(tcount)
-                                        + "=" + col;
-                                cols.add(col);
-                                fields.add(colmap);
-                                tcount++;
-                            }
-                        }
-                    }
-                }
-            }
-            // build wfs:Insert element
-            sb.append(WFSTINSERTSTART);
-            // add geometry node
-            sb.append(nodeToString(geometry).replace(geomcol,
-                    "feature:geometry"));
-            // add text feature nodes (1-based)
-            for (int j = 0; j < textFeatures.size(); j++) {
-                sb
-                        .append("         <feature:t" + (j + 1) + ">"
-                                + textFeatures.get(j) + "</feature:t" + (j + 1)
-                                + ">\n");
-            }
-            // add numeric feature nodes (1-based)
-            for (int j = 0; j < numericFeatures.size(); j++) {
-                sb.append("         <feature:n" + (j + 1) + ">"
-                        + numericFeatures.get(j) + "</feature:n" + (j + 1)
-                        + ">\n");
-            }
-
-            sb.append("         <feature:analysis_id>"
-                    + Long.toString(analysis_id) + "</feature:analysis_id>\n");
-
-            sb.append("         <feature:uuid>" + uuid + "</feature:uuid>\n");
-
-            sb.append(WFSTINSERTEND);
-        }
-        sb.append(WFSTTEMPLATEEND);
-        return sb.toString();
-    }
-
-    private static String nodeToString(Node node) throws ServiceException {
-        try {
-            StringWriter sw = new StringWriter();
-            Transformer t = TransformerFactory.newInstance().newTransformer();
-            t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-            t.setOutputProperty(OutputKeys.INDENT, "yes");
-            t.transform(new DOMSource(node), new StreamResult(sw));
-            return sw.toString();
-        } catch (Exception ex) {
-            throw new ServiceException("Unable to write XML node to string");
-        }
-    }
-
-    private String stripNamespace(final String tag) {
-
-        String splitted[] = tag.split(":");
-        if (splitted.length > 1) {
-            return splitted[1];
-        }
-        return splitted[0];
-    }
-
-    private Document createDoc(final String content) throws ServiceException {
-        try {
-            final DocumentBuilderFactory dbf = DocumentBuilderFactory
-                    .newInstance();
-            // dbf.setNamespaceAware(true);
-            final DocumentBuilder builder = dbf.newDocumentBuilder();
-            final Document wpsDoc = builder.parse(new InputSource(
-                    new ByteArrayInputStream(content.getBytes("UTF-8"))));
-            return wpsDoc;
-        } catch (Exception ex) {
-            throw new ServiceException("Unable to create XML doc from content");
-        }
     }
 
     /**
@@ -558,7 +403,7 @@ public class AnalysisDataService {
             json.put(JSKEY_FIELDS, this.getAnalyseNativeFields(al));
             json.put(JSKEY_LOCALES, this.getAnalyseFields(al));
             json.put(JSKEY_WPSURL, analysisRenderingUrl);
-            json.put(JSKEY_WPSNAME, ANALYSIS_WPS_RENDERING_ELEMENT_NAME);
+            json.put(JSKEY_WPSNAME, analysisRenderingElement);
             json.put(JSKEY_WPSLAYERID, wpsid);
             json.put(JSKEY_RESULT, "");
         } catch (Exception ex) {
@@ -568,26 +413,6 @@ public class AnalysisDataService {
         return json;
     }
 
-    public JSONObject getAnalyseFieldsMapping(Analysis analysis) {
-        JSONObject fm = new JSONObject();
-        try {
-            if (analysis != null) {
-                for (int j = 1; j < 11; j++) {
-                    String colx = analysis.getColx(j);
-                    if (colx != null && !colx.isEmpty()) {
-                        if (colx.indexOf("=") != -1) {
-                            fm.put(colx.split("=")[0], colx.split("=")[1]);
-                        }
-                    }
-
-                }
-
-            }
-        } catch (Exception ex) {
-            log.debug("Unable to get analysis field mapping layer json", ex);
-        }
-        return fm;
-    }
     public JSONArray getAnalyseFields(Analysis analysis) {
         JSONArray fm = new JSONArray();
         try {
@@ -639,38 +464,6 @@ public class AnalysisDataService {
         return fm;
     }
 
-
-    private boolean isHiddenField(Node feature)
-    {
-        String[] acol = feature.getNodeName().split(":");
-        if (acol.length > 1) return HIDDEN_FIELDS.contains(acol[1]); 
-            
-        return false;
-    }
-
-    /**
-     *
-     * @param fieldName
-     * @param fieldTypes  field types like in WFS DescribeFeatureType
-     * @return true, if numeric value (int,double,long,..)
-     */
-    private Double getFieldAsNumeric(String fieldName, String strVal, Map<String,String> fieldTypes)
-    {
-        Double numericValue = null;
-       if(fieldTypes.containsKey(fieldName.toUpperCase()))
-       {
-           //Check type
-           if(fieldTypes.get(fieldName.toUpperCase()).equals(NUMERIC_FIELD_TYPE))
-           {
-               try {
-                   numericValue = Double.parseDouble(strVal);
-               } catch (NumberFormatException nfe) {
-                   // ignore
-               }
-           }
-       }
-        return numericValue;
-    }
 
 
 }
