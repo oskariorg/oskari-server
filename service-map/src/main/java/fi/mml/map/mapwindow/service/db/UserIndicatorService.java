a58a815df716bfb5e49e224ded68a510d47b7866
Esko Vääräsmäki <esko.vaarasmaki@nls.fi>
2013-11-22 15:50:37 +0200

    AH-617

---
+++ service-map/src/main/java/fi/mml/map/mapwindow/service/db/UserIndicatorService.java
@@ -0,0 +1,15 @@
+package fi.mml.map.mapwindow.service.db;
+
+import fi.nls.oskari.domain.map.indicator.UserIndicator;
+import fi.nls.oskari.service.db.BaseService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: EVAARASMAKI
+ * Date: 22.11.2013
+ * Time: 13:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+public interface UserIndicatorService extends BaseService<UserIndicator> {
+}
