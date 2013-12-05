00a98ca9e61124ce9d71e85cab3e37f99992d918
Vesa Luusua <vesa.luusua@nls.fi>
2013-12-05 10:50:18 +0200

    added publishedstatehandler sql

--- content-resources/src/main/resources/sql/views/04-published-template-view.sql
+++ content-resources/src/main/resources/sql/views/04-published-template-view.sql
@@ -261,3 +261,34 @@ UPDATE portti_view_bundle_seq set config = '{
         "adaptable": true
     }' WHERE bundle_id = (SELECT id FROM portti_bundle WHERE name = 'infobox') 
     AND view_id=(SELECT id FROM portti_view WHERE type='PUBLISH');
+
+--------------------------------------------
+-- 5. PublishedStatehandler
+--------------------------------------------
+
+-- add bundle to view
+INSERT INTO portti_view_bundle_seq (view_id, bundle_id, seqno, config, state, startup) 
+    VALUES ((SELECT id FROM portti_view WHERE type='PUBLISH'), 
+    (SELECT id FROM portti_bundle WHERE name = 'publishedstatehandler'), 
+    (SELECT (max(seqno) + 1) FROM portti_view_bundle_seq WHERE view_id = (SELECT id FROM portti_view WHERE type='PUBLISH')), 
+    '{}','{}', '{}');
+
+-- update proper startup for view
+UPDATE portti_view_bundle_seq set startup = '{
+        "title" : "PublishedStatehandler",
+        "fi" : "publishedstatehandler",
+        "sv" : "publishedstatehandler",
+        "en" : "publishedstatehandler",
+        "bundlename" : "publishedstatehandler",
+        "bundleinstancename" : "publishedstatehandler",
+        "metadata" : {
+            "Import-Bundle" : {
+                "publishedstatehandler" : {
+                    "bundlePath" : "/Oskari/packages/framework/bundle/"
+                 }
+            },
+            "Require-Bundle-Instance" : []
+        },
+        "instanceProps" : {}
+    }' WHERE bundle_id = (SELECT id FROM portti_bundle WHERE name = 'publishedstatehandler') 
+    AND  view_id=(SELECT id FROM portti_view WHERE type='PUBLISH');
