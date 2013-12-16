38ce24c8f2d7059850d63af2cab4b9bf7ce36f51
Vesa Luusua <vesa.luusua@nls.fi>
2013-12-16 17:43:56 +0200

    remove measureline & measurearea from default view (logged-in-users)

--- content-resources/src/main/resources/sql/01-create-default-view.sql
+++ content-resources/src/main/resources/sql/01-create-default-view.sql
@@ -228,6 +228,10 @@ UPDATE portti_view_bundle_seq set config = '{
         "viewtools": {
             "print" : false,
             "link" : false
+        },
+        "basictools": {
+            "measureline" : false,
+            "measurearea" : false
         }
     }' WHERE bundle_id = (SELECT id FROM portti_bundle WHERE name = 'toolbar')
              AND view_id=(SELECT id FROM portti_view WHERE type='DEFAULT');
---
+++ content-resources/src/main/resources/sql/upgrade/1.17/update_toolbar_config_default.sql
@@ -0,0 +1,24 @@
+-------------------------------------------------
+-- from 01-create-default-view.sql line:227
+-------------------------------------------------
+-- UPDATE portti_view_bundle_seq set config = '{
+--        "viewtools": {
+--            "print" : false,
+--            "link" : false
+--        },
+--        "basictools": {
+--            "measureline" : false,
+--            "measurearea" : false
+--        }
+--    }' WHERE bundle_id = (SELECT id FROM portti_bundle WHERE name = 'toolbar')
+--             AND view_id=(SELECT id FROM portti_view WHERE type='DEFAULT');
+
+-- update just basictools: measureline & area
+-- from toolbar confs for logged in user
+UPDATE portti_view_bundle_seq set config = '{
+    "basictools": {
+        "measureline" : false,
+        "measurearea" : false
+    }
+}' WHERE bundle_id = (SELECT id FROM portti_bundle WHERE name = 'toolbar')
+    AND view_id=(SELECT id FROM portti_view WHERE type='DEFAULT');
