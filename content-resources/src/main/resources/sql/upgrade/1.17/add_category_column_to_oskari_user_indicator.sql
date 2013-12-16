2dcd90597ae3418d6874b38da12694feed0dabe5
mrLite <aku.peltonen@nls.fi>
2013-12-16 16:16:48 +0200

    Added create user indicator sql

--- content-resources/src/main/resources/sql/upgrade/1.17/add_category_column_to_oskari_user_indicator.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE oskari_user_indicator ADD COLUMN category CHARACTER VARYING(100);
-
--- update every user indicator to be of category 'KUNTA'
-UPDATE oskari_user_indicator SET category = 'KUNTA';
