9098c0174eaf3ee8ef6ca3d311c4ed1fe4cbdc18
Sami Mäkinen <sami.makinen@nls.fi>
2014-01-20 14:50:20 +0200

    Modified mapoptions to update and reverted zoom param adjustment hack (not needed anymore)

--- control-base/src/main/java/fi/nls/oskari/control/view/modifier/param/ZoomParamHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/view/modifier/param/ZoomParamHandler.java
@@ -16,20 +16,8 @@ public class ZoomParamHandler extends ParamHandler {
 
     private static final Logger log = LogFactory.getLogger(ZoomParamHandler.class);
     private static final String PARAM_ZOOM = "zoomLevel";
-    private static final String PARAM_ZOOM_LEVELS = "zoomLevels";
     private static final String KEY_ZOOM = "zoom";
 
-    private static final String PROP_ZOOM_ADJUST = "actionhandler.GetAppSetup.ZoomParamHandler.zoomAdjust";
-
-    private static int zoomAdjust = 0;
-
-    @Override
-    public void init() {
-        super.init();
-        zoomAdjust = ConversionHelper.getInt(PropertyUtil.getOptional(PROP_ZOOM_ADJUST), 0);
-    }
-
-    
     public boolean handleParam(final ModifierParams params) throws ModifierException {
         if(params.getParamValue() == null) {
             return false;
@@ -38,14 +26,6 @@ public class ZoomParamHandler extends ParamHandler {
         try {
             final JSONObject state = getBundleState(params.getConfig(), BUNDLE_MAPFULL);
             int zoom = ConversionHelper.getInt(PropertyUtil.getOptional(params.getParamValue()), 0);
-
-            final String legacyLinkFixParam = params.getActionParams().getHttpParam(PARAM_ZOOM_LEVELS);
-            if(legacyLinkFixParam == null) {
-                // old maplinks don't have zoomLevels parameter
-                // adjust zoomlevel since the number of zoom levels have changed
-                zoom = zoom + zoomAdjust;
-            }
-
             state.put(KEY_ZOOM, zoom);
             return true;
         } catch (JSONException je) {
