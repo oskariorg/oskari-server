8396f1fafbf6964cb37125d487f28efa0b41527f
Sami Mäkinen <sami.makinen@nls.fi>
2014-03-11 12:57:08 +0200

    Dummy implementation for GetMetadataSearchOptions

--- control-base/src/main/java/fi/nls/oskari/control/data/GetCurrentUserHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/data/GetCurrentUserHandler.java
@@ -1,26 +1,13 @@
 package fi.nls.oskari.control.data;
 
 
-import fi.mml.portti.domain.permissions.WFSLayerPermissionsStore;
-import fi.mml.portti.service.db.permissions.PermissionsService;
-import fi.mml.portti.service.db.permissions.PermissionsServiceIbatisImpl;
 import fi.nls.oskari.annotation.OskariActionRoute;
 import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
-import fi.nls.oskari.util.ConversionHelper;
 import fi.nls.oskari.util.ResponseHelper;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -32,17 +19,9 @@ public class GetCurrentUserHandler extends ActionHandler {
     private static final Logger log = LogFactory.getLogger(GetCurrentUserHandler.class);
     private static final String KEY_UID = "currentUserUid";
 
-
     @Override
     public void handleAction(ActionParameters params) throws ActionException {
-
-        String result = "{}";
-
-        //String jsessionid = params.getRequest().getSession().getId();
-
-        params.getUser().getUuid();
-        params.getResponse().setHeader("currentUserUid", params.getUser().getUuid());
-        ResponseHelper.writeResponse(params, result);
-        return;
+        params.getResponse().setHeader(KEY_UID, params.getUser().getUuid());
+        ResponseHelper.writeResponse(params, "{}");
     }
 }
