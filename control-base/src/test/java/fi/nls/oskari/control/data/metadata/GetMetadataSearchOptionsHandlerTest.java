c302be7c22fbfadbb863a7cf979f8227e28579d0
Sami Mäkinen <sami.makinen@nls.fi>
2014-03-13 11:10:38 +0200

    Removed unused copy/paste method

--- control-base/src/test/java/fi/nls/oskari/control/data/metadata/GetMetadataSearchOptionsHandlerTest.java
+++ control-base/src/test/java/fi/nls/oskari/control/data/metadata/GetMetadataSearchOptionsHandlerTest.java
@@ -90,30 +90,4 @@ public class GetMetadataSearchOptionsHandlerTest extends JSONActionRouteTest {
         return new ByteArrayInputStream(resource.getBytes());
     }
 
-    public static String readString(InputStream is, final String charset)
-            throws IOException {
-        /*
-         * To convert the InputStream to String we use the Reader.read(char[]
-         * buffer) method. We iterate until the Reader return -1 which means
-         * there's no more data to read. We use the StringWriter class to
-         * produce the string.
-         */
-        if (is == null) {
-            return "";
-        }
-        final Writer writer = new StringWriter();
-        final char[] buffer = new char[1024];
-        try {
-            final Reader reader = new BufferedReader(new InputStreamReader(is,
-                    charset));
-            int n;
-            while ((n = reader.read(buffer)) != -1) {
-                writer.write(buffer, 0, n);
-            }
-        } finally {
-            is.close();
-        }
-        return writer.toString();
-    }
-
 }
