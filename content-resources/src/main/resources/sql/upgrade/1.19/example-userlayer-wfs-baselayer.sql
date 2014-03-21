a30b264c6bb80441ff353fa95b341ba5bdc51488
Tapio Keisteri <tapio.keisteri@nls.fi>
2014-03-21 14:14:15 +0200

    [AH-33] upgrade sql

--- content-resources/src/main/resources/sql/upgrade/1.19/example-userlayer-wfs-baselayer.sql
+++ content-resources/src/main/resources/sql/upgrade/1.19/example-userlayer-wfs-baselayer.sql
@@ -17,8 +17,8 @@ INSERT INTO portti_wfs_layer (maplayer_id, layer_name, url, username, password, 
   selection_sld_style_id, get_map_tiles, get_feature_info, tile_request, wms_layer_id, srs_name, feature_element, feature_namespace_uri, geometry_namespace_uri,
    get_highlight_image, wps_params, tile_buffer, schema_status, custom_parser, test_location, test_zoom)
     VALUES ((select id from oskari_maplayer where name = 'oskari:vuser_layer_data'),
-     'oskari:vuser_layer_data', 'http://demo.paikkatietoikkuna.fi/geoserver/oskari/ows', 'liferay', 'liferay', 'geometry', '3.1.1', false, '1.1.0', 2000, 'oskari', NULL, NULL, '{}',
-      '[]', '{}',
+     'oskari:vuser_layer_data', 'http://demo.paikkatietoikkuna.fi/dataset/analysis/service/ows', 'liferay', 'liferay', 'geometry', '3.1.1', false, '1.1.0', 2000, 'oskari', NULL, NULL, '{}',
+      '{}', '{}',
        '2d', NULL, false, true, false, NULL, 'EPSG:3067', 'vuser_layer_data', 'http://www.oskari.org', '', true, '{}', '{}', '', false, '[]', 9);
 
 
