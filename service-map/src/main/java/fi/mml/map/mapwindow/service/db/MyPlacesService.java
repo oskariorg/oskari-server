9458fb50b39bb2ae2db59e7fb43b1a64dd5e075f
Sami Mäkinen <sami.makinen@nls.fi>
2014-03-31 18:33:07 +0300

    Drawing myplaces as guest is now allowed for published draw layers

--- service-map/src/main/java/fi/mml/map/mapwindow/service/db/MyPlacesService.java
+++ service-map/src/main/java/fi/mml/map/mapwindow/service/db/MyPlacesService.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import fi.nls.oskari.domain.User;
 import fi.nls.oskari.domain.map.MyPlaceCategory;
+import fi.nls.oskari.permission.domain.Resource;
 import fi.nls.oskari.service.db.BaseService;
 import org.json.JSONObject;
 
 public interface MyPlacesService extends BaseService<MyPlaceCategory>{
+
+    public static final String RESOURCE_TYPE_MYPLACES = "myplaces";
+    public final static String PERMISSION_TYPE_DRAW = "DRAW";
 
     public List<MyPlaceCategory> getMyPlaceLayersById(List<Long> idList);
     public int updatePublisherName(final long id, final String uuid, final String name);
@@ -21,5 +25,8 @@ public interface MyPlacesService extends BaseService<MyPlaceCategory>{
     public boolean canInsert(final User user, final long categoryId);
     public boolean canModifyPlace(final User user, final long placeId);
     public boolean canModifyCategory(final User user, final long categoryId);
+    public boolean canModifyCategory(final User user, final String layerId);
+    public Resource getResource(final long categoryId);
+    public Resource getResource(final String myplacesLayerId);
 
 }
