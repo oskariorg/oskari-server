4f25d1b2f0b0bb8d5dc64a193c159bebd53ac245
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-04 11:25:16 +0300

    Add table to map oskari users to roles

--- content-resources/src/main/resources/sql/PostgreSQL/createUserTables.sql
+++ content-resources/src/main/resources/sql/PostgreSQL/createUserTables.sql
@@ -1,5 +1,6 @@
-DROP TABLE IF EXISTS oskari_users;
+DROP TABLE IF EXISTS oskari_role_oskari_user;
 DROP TABLE IF EXISTS oskari_roles;
+DROP TABLE IF EXISTS oskari_users;
 
 CREATE TABLE oskari_users (
   id serial NOT NULL,
@@ -15,5 +16,13 @@ CREATE TABLE oskari_roles (
   id serial NOT NULL,
   name character varying(25) NOT NULL,
   is_guest boolean default FALSE,
+  CONSTRAINT oskari_roles_pkey PRIMARY KEY (id),
   UNIQUE (name)
+);
+
+CREATE TABLE oskari_role_oskari_user (
+  id serial NOT NULL,
+  user_name character varying(25) REFERENCES oskari_users(user_name),
+  role_id integer REFERENCES oskari_roles(id),
+  CONSTRAINT oskari_role_oskari_user_pkey PRIMARY KEY (id)
 );
