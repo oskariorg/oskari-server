8ba3c32c8c989f264d31391f16492282a9278a2d
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-04 10:23:25 +0300

    Create JAAS users in dedicated scripts. Leave JAAS roles table empty.

--- content-resources/src/main/resources/sql/PostgreSQL/exampleJaasUsers.sql
+++ content-resources/src/main/resources/sql/PostgreSQL/exampleJaasUsers.sql
@@ -1,4 +1,2 @@
 INSERT INTO oskari_jaas_users(login, password) VALUES('user', 'MD5:ee11cbb19052e40b07aac0ca060c23ee');
 INSERT INTO oskari_jaas_users(login, password) VALUES('admin', 'MD5:f75b872a23cd32775f7c3a2e4f974cda');
-
-INSERT INTO oskari_jaas_roles(login, role) VALUES('admin', 'admin');
