f0591056bee4dd3e9498269bda20a577cffb9694
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-08 12:44:21 +0300

    Add configuration classes required for JNDI support

--- standalone-jetty/src/main/java/fi/nls/oskari/jetty/JettyRunner.java
+++ standalone-jetty/src/main/java/fi/nls/oskari/jetty/JettyRunner.java
@@ -1,14 +1,16 @@
 package fi.nls.oskari.jetty;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyRunner {
     public static void main(String[] args) throws Exception {
         System.out.println("JettyRunner.main");
 
         Server server = new Server(2373);
-        ServletContextHandler servletContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        WebAppContext servletContext = new WebAppContext();
+        servletContext.setConfigurationClasses(new String[]{"org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration"});
+        servletContext.setResourceBase("src/main/webapp");
         servletContext.setContextPath("/");
         servletContext.addServlet(DebugServlet.class, "/");
         server.setHandler(servletContext);
