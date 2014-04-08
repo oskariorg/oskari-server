71f4538fb01dd1e4c292b5d526b77c236da9e0e6
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-08 12:54:37 +0300

    Output OskariPool environment entry for debugging purposes

--- standalone-jetty/src/main/java/fi/nls/oskari/jetty/DebugServlet.java
+++ standalone-jetty/src/main/java/fi/nls/oskari/jetty/DebugServlet.java
@@ -1,5 +1,7 @@
 package fi.nls.oskari.jetty;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,5 +14,18 @@ public class DebugServlet extends HttpServlet {
         httpServletResponse.setContentType("text/html;charset=utf-8");
         httpServletResponse.setStatus(HttpServletResponse.SC_OK);
         httpServletResponse.getWriter().println("<h1>DebugServlet in Oskari Standalone Jetty Server</h1>");
+
+        try {
+            Integer entry = lookupJNDIEnvironmentEntry();
+            httpServletResponse.getWriter().println("<h2>JNDI Environment Entry: " + Integer.toString(entry) + "</h2>");
+        } catch (NamingException e) {
+            httpServletResponse.getWriter().println("<h2>JNDI Environment Entry undefined.</h2>");
+            e.printStackTrace();
+        }
+    }
+
+    private Integer lookupJNDIEnvironmentEntry() throws NamingException {
+        InitialContext ic = new InitialContext();
+        return (Integer)ic.lookup("java:comp/env/jdbc/OskariPool");
     }
 }
