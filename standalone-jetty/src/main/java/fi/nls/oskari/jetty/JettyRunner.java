cb2591052bb337eb2bdcb541cfcc427fc0659e5a
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-04-11 16:06:46 +0300

    Support system properties in database username and password

--- standalone-jetty/src/main/java/fi/nls/oskari/jetty/JettyRunner.java
+++ standalone-jetty/src/main/java/fi/nls/oskari/jetty/JettyRunner.java
@@ -12,16 +12,24 @@ public class JettyRunner {
     public static void main(String[] args) throws Exception {
         addProperties("/standalone.properties");
 
+        String username = fromSystemPropertiesOrPropertyUtil("db.username", "db.jndi.username");
+        String password = fromSystemPropertiesOrPropertyUtil("db.password", "db.jndi.password");
+
         Server server = JettyLauncher.launch(
                 PropertyUtil.getOptional("oskari.server.port", 2373),
                 PropertyUtil.get("oskari.client.version"),
                 PropertyUtil.get("db.jndi.driverClassName", "org.postgresql.Driver"),
                 PropertyUtil.get("db.jndi.url", "jdbc:postgresql://localhost:5432/oskaridb"),
-                PropertyUtil.get("db.jndi.username"),
-                PropertyUtil.get("db.jndi.password"),
+                username,
+                password,
                 PropertyUtil.get("db.jndi.name", "jdbc/OskariPool"));
         server.start();
         server.join();
+    }
+
+    private static String fromSystemPropertiesOrPropertyUtil(String systemPropertyKey, String propertyUtilKey) {
+        String systemProperty = System.getProperty(systemPropertyKey);
+        return systemProperty != null ? systemProperty : PropertyUtil.get(propertyUtilKey);
     }
 
     private static void addProperties(String propertiesFile) throws IOException, DuplicateException {
