6e56b1333c993f66ee15f793570107e2f7a920be
Sami Mäkinen <sami.makinen@nls.fi>
2014-06-23 18:00:45 +0300

    Refactored SaveOrganization to use ActionConstants and RequestHelper

--- control-base/src/main/java/fi/nls/oskari/control/layer/SaveOrganizationHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/SaveOrganizationHandler.java
@@ -7,11 +7,15 @@ import fi.nls.oskari.domain.map.LayerGroup;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.map.layer.LayerGroupService;
+import fi.nls.oskari.util.RequestHelper;
 import fi.nls.oskari.util.ResponseHelper;
 import fi.nls.oskari.util.ServiceFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
+import java.util.Map;
+
+import static fi.nls.oskari.control.ActionConstants.*;
 
 /**
  * Admin insert/update of class layer or class sub layer
@@ -22,21 +26,16 @@ public class SaveOrganizationHandler extends ActionHandler {
     private static final Logger log = LogFactory.getLogger(SaveOrganizationHandler.class);
 
     private final LayerGroupService layerGroupService = ServiceFactory.getLayerGroupService();
-    private final PermissionsService permissionsService = ServiceFactory.getPermissionsService();
-
-    private static final String PARAM_GROUP_ID = "id";
-    private static final String NAME_PREFIX = "name_";
-
 
     @Override
     public void handleAction(ActionParameters params) throws ActionException {
 
         final HttpServletRequest request = params.getRequest();
         try {
-            final int groupId = params.getHttpParam(PARAM_GROUP_ID, -1);
+            final int groupId = params.getHttpParam(PARAM_ID, -1);
             final LayerGroup group = new LayerGroup();
             group.setId(groupId);
-            handleLocalizations(group, NAME_PREFIX, request);
+            handleLocalizations(group, PARAM_NAME_PREFIX, request);
             if (group.getLocale() == null) {
                 throw new ActionParamsException("Missing names for group!");
             }
@@ -54,9 +53,6 @@ public class SaveOrganizationHandler extends ActionHandler {
                 final int id = layerGroupService.insert(group);
                 group.setId(id);
                 ResponseHelper.writeResponse(params, group.getAsJSON());
-
-                //final String[] externalIds = params.getHttpParam("viewPermissions", "").split(",");
-                //addPermissionsForAdmin(lc, params.getUser(), externalIds);
             } else {
                 throw new ActionDeniedException("Unauthorized user tried to update layer group - id=" + groupId);
             }
@@ -67,13 +63,9 @@ public class SaveOrganizationHandler extends ActionHandler {
     }
 
     private void handleLocalizations(final LayerGroup lc, final String nameprefix, final HttpServletRequest request) {
-        final int prefixLength = nameprefix.length();
-        final Enumeration<String> paramNames = request.getParameterNames();
-        while (paramNames.hasMoreElements()) {
-            final String nextName = paramNames.nextElement();
-            if (nextName.indexOf(nameprefix) == 0) {
-                lc.setName(nextName.substring(prefixLength), request.getParameter(nextName));
-            }
+        final Map<String, String> parsed = RequestHelper.parsePrefixedParamsMap(request, nameprefix);
+        for(Map.Entry<String, String> entry : parsed.entrySet()) {
+            lc.setName(entry.getKey(), entry.getValue());
         }
     }
 }
