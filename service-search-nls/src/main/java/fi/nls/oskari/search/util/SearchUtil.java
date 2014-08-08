eb1f506bb062d3e0fe0bf3b7a20099b9b7f28ebd
Tapio Keisteri <tapio.keisteri@nls.fi>
2014-08-08 10:05:30 +0300

    [AH-1568] dont use search.locationtype.url when it is undefined

--- service-search-nls/src/main/java/fi/nls/oskari/search/util/SearchUtil.java
+++ service-search-nls/src/main/java/fi/nls/oskari/search/util/SearchUtil.java
@@ -223,6 +223,7 @@ public class SearchUtil {
 	}
 	
 	public static URL getLocationTypeUrl() throws Exception {
+        if(PropertyUtil.getOptional(LOCATION_TYPE_URL_PROPERTY) == null) return null;
 		final URL villagesUrl = new URL(PropertyUtil.get(LOCATION_TYPE_URL_PROPERTY));
 		return villagesUrl;
 	}
@@ -519,49 +520,51 @@ public class SearchUtil {
         }
         try {
 
-			final URL locationTypeUrl = getLocationTypeUrl();
-            log.debug("locationtypeURL: ",locationTypeUrl );
-			InputStreamReader isr = new InputStreamReader(locationTypeUrl.openStream(), "UTF-8");
+            final URL locationTypeUrl = getLocationTypeUrl();
+            if (locationTypeUrl != null) {
+                log.debug("locationtypeURL: ", locationTypeUrl);
+                InputStreamReader isr = new InputStreamReader(locationTypeUrl.openStream(), "UTF-8");
 
-            
-            BufferedReader reader = new BufferedReader(isr);
-            StringBuilder readXML = new StringBuilder();
-            
-            String inputLine;
-            while ((inputLine = reader.readLine()) != null) {
-            	readXML.append(inputLine);
+
+                BufferedReader reader = new BufferedReader(isr);
+                StringBuilder readXML = new StringBuilder();
+
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null) {
+                    readXML.append(inputLine);
+                }
+                isr.close();
+
+                XMLTokener xmlTokener = new XMLTokener(readXML.toString().replace(':', '_'));
+                while (xmlTokener.more()) {
+
+                    String nextContent = xmlTokener.nextContent().toString();
+
+                    if (!"<".equals(nextContent) && nextContent.startsWith("xsd_enumeration value")) {
+
+                        String[] code = nextContent.split("\"");
+
+                        while (xmlTokener.more()) {
+
+                            String content = xmlTokener.nextContent().toString();
+
+                            if (!"<".equals(content) && content.startsWith("xsd_documentation xml_lang")) {
+                                String[] languageAndName = content.split("\"");
+
+                                locationTypeCache.put(code[1] + "_" + languageAndName[1], languageAndName[2].substring(1));
+
+                            } else if ("/xsd_annotation>".equals(content)) {
+                                break;
+                            }
+                        }
+                    }
+                }
             }
-            isr.close();
-            
-            XMLTokener xmlTokener = new XMLTokener(readXML.toString().replace(':', '_'));
-            while (xmlTokener.more()) {
-            	
-            	String nextContent = xmlTokener.nextContent().toString();
-            	
-            	if (!"<".equals(nextContent) && nextContent.startsWith("xsd_enumeration value")) {
-            		
-            		String[] code =  nextContent.split("\"");
-            		
-            		 while (xmlTokener.more()) {
-            			 
-            			 String content = xmlTokener.nextContent().toString();
-            			 
-            			 if (!"<".equals(content) && content.startsWith("xsd_documentation xml_lang")) {
-            				 String[] languageAndName = content.split("\"");
-            				 
-            				 locationTypeCache.put(code[1]+"_"+languageAndName[1], languageAndName[2].substring(1));
-            				 
-            			 }else if ("/xsd_annotation>".equals(content)) {
-            				 break;
-            			 }
-            		 }
-            	}
-            }      
-          
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to update location type", e);
-		}
-	}
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to update location type", e);
+        }
+    }
 	
 	private static QName getQName(final String attribute, final String prefix) {
 		return new QName(nameSpaceUri.get(prefix), attribute, prefix);
