a8b77ed4770eb4b0e1e4f4f3a71bb38291dc8910
Sami Mäkinen <sami.makinen@nls.fi>
2014-08-19 17:09:31 +0300

    Transforming indicator value region codes to match geoserver material

--- control-statistics/src/main/java/fi/nls/oskari/control/statistics/StatisticalIndicatorValuesHandler.java
+++ control-statistics/src/main/java/fi/nls/oskari/control/statistics/StatisticalIndicatorValuesHandler.java
@@ -5,8 +5,10 @@ import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.control.sotka.requests.SotkaRequest;
+import fi.nls.oskari.integration.sotka.SotkaRegionParser;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.util.ConversionHelper;
 import fi.nls.oskari.util.JSONHelper;
 import fi.nls.oskari.util.ResponseHelper;
 import org.json.JSONArray;
@@ -20,9 +22,14 @@ import static fi.nls.oskari.control.statistics.util.Constants.*;
 @OskariActionRoute("StatisticalIndicatorValues")
 public class StatisticalIndicatorValuesHandler extends ActionHandler {
     private final static Logger log = LogFactory.getLogger(StatisticalIndicatorValuesHandler.class);
+    private SotkaRegionParser sotkaParser = null;
+
+    public void init() {
+        sotkaParser = new SotkaRegionParser();
+    }
 
     public void handleAction(ActionParameters params) throws ActionException {
-
+        // FIXME: now always proxies to SotkaNet
         final int datasourceId = params.getRequiredParamInt(PARAM_DATASOURCE);
         final int indicatorId = params.getRequiredParamInt(PARAM_ID);
         final String optionsStr = params.getHttpParam(PARAM_OPTIONS);
@@ -38,7 +45,19 @@ public class StatisticalIndicatorValuesHandler extends ActionHandler {
         req.setIndicator(indicatorId);
         req.setYears(new String[]{options.optString("year")});
         final String data = req.getData();
-        return JSONHelper.createJSONArray(data);
+        return modifyRegionMap(JSONHelper.createJSONArray(data));
+    }
+
+    private JSONArray modifyRegionMap(final JSONArray values) {
+        for(int i = 0; i < values.length(); ++i) {
+            JSONObject obj = values.optJSONObject(i);
+            // override region codes to match geoserver material
+            final String code = sotkaParser.getCode(obj.optInt("region"));
+            if(code != null) {
+                JSONHelper.putValue(obj, "region", code);
+            }
+        }
+        return values;
     }
 
 }
