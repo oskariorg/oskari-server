ba3f896d3485af5843bcad2359a0f9c5512c34c7
Mikael Blomberg <mikael.blomberg@nls.fi>
2014-08-22 12:54:25 +0300

    ASDI modifications

--- content-resources/src/main/resources/sql/PostgreSQL/asdi-JAAS-users.sql
+++ content-resources/src/main/resources/sql/PostgreSQL/asdi-JAAS-users.sql
@@ -1,2 +1,2 @@
 INSERT INTO oskari_jaas_users(login, password) VALUES('Arcticsdi', 'MD5:0e18bfbcda676047e748e13016a08335');
-INSERT INTO oskari_jaas_users(login, password) VALUES('admin', 'MD5:f75b872a23cd32775f7c3a2e4f974cda');
+INSERT INTO oskari_jaas_users(login, password) VALUES('admin', 'MD5:1160debc0ac680cb371190f507fffceb');
