20958f22f213ae570aa64b7d6265d10a65b64cbb
Sami Mäkinen <sami.makinen@nls.fi>
2014-10-06 14:42:38 +0300

    Added correct mapOptions for guest view

--- content-resources/src/main/resources/sql/views/06-guest-view.sql
+++ content-resources/src/main/resources/sql/views/06-guest-view.sql
@@ -184,6 +184,7 @@ UPDATE portti_view_bundle_seq set startup = '{
 UPDATE portti_view_bundle_seq set config = '{
     "globalMapAjaxUrl": "[REPLACED BY HANDLER]",
     "imageLocation": "/Oskari/resources",
+    "mapOptions" : {"srsName":"EPSG:3067","maxExtent":{"bottom":6291456,"left":-548576,"right":1548576,"top":8388608},"resolutions":[2048,1024,512,256,128,64,32,16,8,4,2,1,0.5,0.25]},
     "plugins" : [
        { "id" : "Oskari.mapframework.bundle.mapmodule.plugin.LayersPlugin" },
        { "id" : "Oskari.mapframework.mapmodule.WmsLayerPlugin" },
