1004543f13ed033df7440cf582fda8b1f0fe0850
Tapio Keisteri <tapio.keisteri@nls.fi>
2014-10-28 14:10:58 +0200

    [AH-1984] oskari_resource table update and changed permission key

---
+++ content-resources/src/main/resources/sql/upgrade/1.25/03_update_oskari_resource.sql
@@ -0,0 +1,36 @@
+﻿
+-- oskari_resource update for wfs layer resource mappings
+-- new resource mapping is  type+url+name
+
+update oskari_resource as a
+set resource_mapping = b.type || '+' || b.url || '+' || b.name from oskari_maplayer as b 
+where b.type='wfslayer' and a.resource_type='maplayer' and substring(resource_mapping from position('+' in resource_mapping)+1)=b.name 
+ and substring(resource_mapping from 1 for position('+' in resource_mapping)-1)=b.url ;
+
+-- oskari_resource update for wms layers resource mappings
+
+update oskari_resource as a
+set resource_mapping = b.type || '+' || b.url || '+' || b.name from oskari_maplayer as b 
+where b.type='wmslayer' and a.resource_type='maplayer' and substring(resource_mapping from position('+' in resource_mapping)+1)=b.name 
+ and substring(resource_mapping from 1 for position('+' in resource_mapping)-1)=b.url ;
+
+-- oskari_resource update for wmts layers resource mappings
+
+update oskari_resource as a
+set resource_mapping = b.type || '+' || b.url || '+' || b.name from oskari_maplayer as b 
+where b.type='wmtslayer' and a.resource_type='maplayer' and substring(resource_mapping from position('+' in resource_mapping)+1)=b.name 
+ and substring(resource_mapping from 1 for position('+' in resource_mapping)-1)=b.url ;
+
+ -- oskari_resource update for collection layers resource mappings
+
+update oskari_resource as a
+set resource_mapping = b.type || '+' || b.url || '+' || b.name from oskari_maplayer as b 
+where b.type='collection' and a.resource_type='maplayer' and substring(resource_mapping from position('+' in resource_mapping)+1)=b.name 
+ and substring(resource_mapping from 1 for position('+' in resource_mapping)-1)=b.url ;
+
+ -- oskari_resource update for statslayer layers resource mappings
+
+update oskari_resource as a
+set resource_mapping = b.type || '+' || b.url || '+' || b.name from oskari_maplayer as b 
+where b.type='statslayer' and a.resource_type='maplayer' and substring(resource_mapping from position('+' in resource_mapping)+1)=b.name 
+ and substring(resource_mapping from 1 for position('+' in resource_mapping)-1)=b.url ;
