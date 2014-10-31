8fe287c835d6c983f4b8b4d644ce1da8c796004e
Sami Mäkinen <sami.makinen@nls.fi>
2014-10-31 11:35:53 +0200

    Locales has not been a field in servlet setup - removing it from upgrade sql

--- content-resources/src/main/resources/sql/upgrade/1.25/04_drop_columns_portti_wfs_layer.sql
+++ content-resources/src/main/resources/sql/upgrade/1.25/04_drop_columns_portti_wfs_layer.sql
@@ -1,7 +1,6 @@
 ﻿-- drop overlapping values in portti_wfs_layer table
 -- execute 1st 01_update_oskari_maplayer.sql
 ALTER TABLE portti_wfs_layer
-   DROP COLUMN  locales,
    DROP COLUMN  url,
    DROP COLUMN   username,
    DROP COLUMN   password,
