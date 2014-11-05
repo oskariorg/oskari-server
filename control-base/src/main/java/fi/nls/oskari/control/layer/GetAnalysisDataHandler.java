29b1fe0e09e975ce4a8ed29f387c02d72e281d79
Sami Mäkinen <sami.makinen@nls.fi>
2014-11-05 11:18:28 +0200

    Cleaning up

--- control-base/src/main/java/fi/nls/oskari/control/layer/GetAnalysisDataHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/GetAnalysisDataHandler.java
@@ -1,7 +1,5 @@
 package fi.nls.oskari.control.layer;
 
-import fi.mml.portti.service.db.permissions.PermissionsService;
-import fi.mml.portti.service.db.permissions.PermissionsServiceIbatisImpl;
 import fi.nls.oskari.analysis.AnalysisHelper;
 import fi.nls.oskari.annotation.OskariActionRoute;
 import fi.nls.oskari.control.ActionException;
@@ -32,7 +30,6 @@ public class GetAnalysisDataHandler extends ActionHandler {
 
     private static final Logger log = LogFactory.getLogger(GetAnalysisDataHandler.class);
     private static final AnalysisDbService analysisService = new AnalysisDbServiceIbatisImpl();
-    private static PermissionsService permissionsService = new PermissionsServiceIbatisImpl();
 
     private static final String ANALYSE_ID = "analyse_id";
     private static final String JSKEY_ANALYSISDATA = "analysisdata";
@@ -42,7 +39,6 @@ public class GetAnalysisDataHandler extends ActionHandler {
 
         // only for logged in users!
         params.requireLoggedInUser();
-
 
         final long id = ConversionHelper.getLong(params.getHttpParam(ANALYSE_ID), -1);
         if (id == -1) {
@@ -58,9 +54,9 @@ public class GetAnalysisDataHandler extends ActionHandler {
         if (select_items != null) {
 
             final List<HashMap<String, Object>> list = analysisService.getAnalysisDataByIdUid(id, user.getUuid(), select_items);
-            JSONArray rows = new JSONArray();
+            final JSONArray rows = new JSONArray();
             for (HashMap<String, Object> analysisData : list) {
-                JSONObject row = convertMapToJSON(analysisData);
+                final JSONObject row = convertMapToJSON(analysisData);
                 if(row != null) {
                     rows.put(row);
                 }
@@ -72,19 +68,25 @@ public class GetAnalysisDataHandler extends ActionHandler {
         ResponseHelper.writeResponse(params, response);
     }
 
+    /**
+     * Converts any stringified JSON in Map values to proper JSON object values
+     * and returns the Map as a JSONObject
+     * @param analysisData
+     * @return
+     */
     private JSONObject convertMapToJSON(HashMap<String, Object> analysisData) {
         if(analysisData == null) {
             return null;
         }
 
         for (Map.Entry<String, Object> entry : analysisData.entrySet()) {
-            Object value = entry.getValue();
+            final Object value = entry.getValue();
             if(value.toString().indexOf("{") == -1) {
                 // not json, skip to next one
                 continue;
             }
             // try to map as JSON
-            JSONObject modifiedValue = JSONHelper.createJSONObject(value.toString());
+            final JSONObject modifiedValue = JSONHelper.createJSONObject(value.toString());
             if(modifiedValue != null) {
                 entry.setValue(modifiedValue);
             }
