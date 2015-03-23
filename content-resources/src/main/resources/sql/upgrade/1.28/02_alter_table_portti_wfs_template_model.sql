32c7efab73b28bb2a2fa928b2db5146573774f16
Tapio Keisteri <tapio.keisteri@nls.fi>
2015-03-23 16:53:00 +0200

    [AH-2418] 1.28 upgrade script

---
+++ content-resources/src/main/resources/sql/upgrade/1.28/02_alter_table_portti_wfs_template_model.sql
@@ -0,0 +1,5 @@
+﻿-- add parse_config column to portti_wfs_template_model table
+-- this column is used for WFS path parser configuration setup
+
+ALTER TABLE portti_wfs_template_model
+   ADD parse_config text;
