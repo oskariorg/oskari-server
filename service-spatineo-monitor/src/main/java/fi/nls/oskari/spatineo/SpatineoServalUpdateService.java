71d459bdca4bf282ba95b228831672e2a244d3cd
Sami Mäkinen <sami.makinen@nls.fi>
2015-08-25 17:10:44 +0300

    Spatineo update should now get a datasource correctly

--- service-spatineo-monitor/src/main/java/fi/nls/oskari/spatineo/SpatineoServalUpdateService.java
+++ service-spatineo-monitor/src/main/java/fi/nls/oskari/spatineo/SpatineoServalUpdateService.java
@@ -1,7 +1,6 @@
 package fi.nls.oskari.spatineo;
 
 import com.google.common.collect.Lists;
-import fi.nls.oskari.db.DBHandler;
 import fi.nls.oskari.db.DatasourceHelper;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
@@ -11,6 +10,7 @@ import fi.nls.oskari.spatineo.dao.WmsServiceStatusDao;
 import fi.nls.oskari.spatineo.dto.OskariMapLayerDto;
 import fi.nls.oskari.spatineo.dto.PorttiBackendStatusDto;
 import fi.nls.oskari.spatineo.dto.SpatineoResponseDto;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -20,7 +20,7 @@ import java.util.List;
  */
 public class SpatineoServalUpdateService {
 
-    private static final Logger log = LogFactory.getLogger(SpatineoServalUpdateService.class);
+    private static final Logger LOG = LogFactory.getLogger(SpatineoServalUpdateService.class);
 
     public static final String SERVAL_URL = "https://monitor.spatineo.com/api/public/availability-1.0";
 
@@ -31,29 +31,31 @@ public class SpatineoServalUpdateService {
      * Executed every 5 minutes or so by the Quartz scheduler, inside the servlet-map JVM.
      */
     public static void scheduledServiceCall() throws Exception {
-        log.info("Starting the Spatineo Serval update service call...");
+        LOG.info("Starting the Spatineo Serval update service call...");
         final DatasourceHelper helper = DatasourceHelper.getInstance();
 
-        final DataSource dataSource = helper.getDataSource();
+        final DataSource dataSource = helper.createDataSource();
         final OskariDao oskariDao = new OskariDao(dataSource);
         final WmsServiceStatusDao serviceStatusDao = new WmsServiceStatusDao(dataSource);
         final SpatineoServalDao spatineoDao = new SpatineoServalDao(SERVAL_URL);
         serviceStatusDao.truncateStatusTable();
         for (final List<OskariMapLayerDto> layers : Lists.partition(oskariDao.findWmsMapLayerData(), CHUNK_SIZE)) {
-            log.debug("checking status for layers", layers);
+            LOG.debug("checking status for layers", layers);
             final SpatineoResponseDto spatineoResponse = spatineoDao.checkServiceStatus(layers);
-            log.debug("Spatineo response was", spatineoResponse);
+            LOG.debug("Spatineo response was", spatineoResponse);
             for (int i = 0; i < layers.size(); i++) {
                 final OskariMapLayerDto l = layers.get(i);
                 final SpatineoResponseDto.Result s = spatineoResponse.result.get(i);
                 if (null != l && null != s) {
                     serviceStatusDao.insertStatus(new PorttiBackendStatusDto(l.id, s.status, s.statusMessage, s.infoUrl));
                 } else {
-                    log.error("PROBLEM with the Spatineo request for the layers:", layers);
+                    LOG.error("PROBLEM with the Spatineo request for the layers:", layers);
                 }
             }
         }
-        log.info("Done with the Spatineo Serval update service call");
+        // clean up the created datasource
+        ((BasicDataSource)dataSource).close();
+        LOG.info("Done with the Spatineo Serval update service call");
     }
 
 }
