8999d61158493568e6300a761173223f497e877a
Sami Mäkinen <sami.makinen@nls.fi>
2015-09-02 14:58:31 +0300

    ConnectionInfo for DatasourceHelper

--- service-base/src/main/java/fi/nls/oskari/db/ConnectionInfo.java
+++ service-base/src/main/java/fi/nls/oskari/db/ConnectionInfo.java
@@ -1,212 +1,51 @@
 package fi.nls.oskari.db;
 
-import fi.nls.oskari.log.LogFactory;
-import fi.nls.oskari.log.Logger;
-import fi.nls.oskari.util.PropertyUtil;
-import org.apache.commons.dbcp2.BasicDataSource;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-import java.io.StringWriter;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import fi.nls.oskari.util.ConversionHelper;
 
 /**
  * Created by SMAKINEN on 11.6.2015.
  */
 public class ConnectionInfo {
 
-    private static final Logger LOGGER = LogFactory.getLogger(ConnectionInfo.class);
-    private static final String DEFAULT_DATASOURCE_NAME = "jdbc/OskariPool";
-    private static final String MSG_CHECKING_POOL = " - checking existance of database pool: %s";
-    private static final String PREFIX_DB = "db.";
-    private static final ConnectionInfo INSTANCE = new ConnectionInfo();
+    public String driver;
+    public String url;
+    public String user;
+    public String pass;
 
-    private List<BasicDataSource> localDataSources = new ArrayList<BasicDataSource>();
-    private final static String JNDI_PREFIX = "java:comp/env/";
-    private Context context;
+    private static final int DEFAULT_PORT = 5432;
 
-    private ConnectionInfo() {
-        // use getInstance()
-    }
-
-    public String getOskariDataSourceName() {
-        return getOskariDataSourceName(null);
-    }
-
-
-
-    public static ConnectionInfo getInstance() {
-        return INSTANCE;
-    }
-    /**
-     * Finds a datasource name property matching module (db[.module].jndi.name)
-     * @param prefix module name like myplaces, analysis etc null means "core" oskari
-     * @return defaults to jdbc/OskariPool if not configured
-     */
-    public String getOskariDataSourceName(final String prefix) {
-        final String poolToken = (prefix == null) ? "" : prefix + ".";
-        return PropertyUtil.get(PREFIX_DB + poolToken + "jndi.name", DEFAULT_DATASOURCE_NAME);
-    }
-
-    /**
-     * Returns the default datasource (jdbc/OskariPool) from context.
-     */
-    public DataSource getDataSource() {
-        return getDataSource(getContext(), getOskariDataSourceName());
-    }
-    /**
-     * Returns a datasource matching the name from context.
-     * @param name for example jdbc/OskariPool
-     */
-    public DataSource getDataSource(final String name) {
-        return getDataSource(getContext(), name);
-    }
-    /**
-     * Returns a datasource matching the name from the given context.
-     * @param name for example jdbc/OskariPool
-     */
-    public DataSource getDataSource(final Context ctx, final String name) {
-        if(ctx == null) {
+    public String getHost() {
+        String[] parts = getUrlToParts();
+        if(parts == null || parts.length == 0) {
             return null;
         }
-        try {
-            return (DataSource) ctx.lookup(JNDI_PREFIX + name);
-        } catch (Exception ex) {
-            LOGGER.info("Couldn't find pool with name '" + name + "': " + ex.getMessage());
+        int separator = parts[0].indexOf(':');
+        return parts[0].substring(0, separator);
+    }
+
+    public int getPort() {
+        String[] parts = getUrlToParts();
+        if(parts == null || parts.length == 0) {
+            return DEFAULT_PORT;
         }
-        return null;
+        int separator = parts[0].indexOf(':');
+        return ConversionHelper.getInt(parts[0].substring(separator + 1), DEFAULT_PORT);
     }
-
-
-    public boolean checkDataSource(final Context ctx) {
-        return checkDataSource(ctx, null);
-    }
-
-    /**
-     * Ensures the datasource matching the module (prefix) is present in the given context.
-     * Creates the datasource and binds it to context if not present.
-     * @param ctx
-     * @param prefix module like myplaces, analysis
-     * @return
-     */
-    public boolean checkDataSource(final Context ctx, final String prefix) {
-        final String poolToken = (prefix == null) ? "" : prefix + ".";
-        final String poolName = getOskariDataSourceName(prefix);
-
-        LOGGER.info(String.format(MSG_CHECKING_POOL, poolName));
-        final DataSource ds = getDataSource(ctx, poolName);
-        boolean success = ds != null;
-        if(success) {
-            // using container provided datasource rather than one created by us
-            LOGGER.info("Found JNDI dataSource with name: " + poolName +
-                    ". Using it instead of properties configuration db." + poolToken + "url");
-        } else {
-            LOGGER.info(" - creating a DataSource with defaults based on configured properties");
-            final DataSource dataSource = createDataSource(null);
-            addDataSource(ctx, poolName, dataSource);
-            LOGGER.info(String.format(MSG_CHECKING_POOL, poolName));
-            success = (getDataSource(ctx, poolName) != null);
+    public String getDBName() {
+        String[] parts = getUrlToParts();
+        if(parts == null || parts.length < 2) {
+            return null;
         }
-        return success;
+        return parts[1];
     }
 
-    public BasicDataSource createDataSource() {
-        return createDataSource(null);
-    }
-
-    /**
-     * Creates the datasource for the module (prefix)
-     * @param prefix for example myplaces, analysis
-     * @return
-     */
-    public BasicDataSource createDataSource(final String prefix) {
-        final String poolToken = (prefix == null) ? "" : prefix + ".";
-        final BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(PropertyUtil.get("db.jndi.driverClassName", "org.postgresql.Driver"));
-        dataSource.setUrl(PropertyUtil.get(PREFIX_DB + poolToken + "url", "jdbc:postgresql://localhost:5432/oskaridb"));
-        dataSource.setUsername(PropertyUtil.get(PREFIX_DB + poolToken + "username", ""));
-        dataSource.setPassword(PropertyUtil.get(PREFIX_DB + poolToken + "password", ""));
-        dataSource.setTimeBetweenEvictionRunsMillis(-1);
-        dataSource.setTestOnBorrow(true);
-        dataSource.setValidationQuery("SELECT 1");
-        dataSource.setValidationQueryTimeout(100);
-
-        localDataSources.add(dataSource);
-        return dataSource;
-    }
-
-    public Map<String, String> getPropsForDS(final String prefix) {
-        Map<String, String> props = new HashMap<>(4);
-        final String poolToken = (prefix == null) ? "" : prefix + ".";
-        PropertyUtil.get("db.jndi.driverClassName", "org.postgresql.Driver"));
-        dataSource.setUrl(PropertyUtil.get(PREFIX_DB + poolToken + "url", "jdbc:postgresql://localhost:5432/oskaridb"));
-        dataSource.setUsername(PropertyUtil.get(PREFIX_DB + poolToken + "username", ""));
-        dataSource.setPassword(PropertyUtil.get(PREFIX_DB + poolToken + "password", ""));
-
-    }
-
-    private void addDataSource(final Context ctx, final String name, final DataSource ds) {
-        if(ctx == null) {
-            return;
+    private String[] getUrlToParts() {
+        if(url == null) {
+            return null;
         }
-        try {
-            constructContext(ctx, "comp", "env", "jdbc");
-            ctx.bind(JNDI_PREFIX + name, ds);
-        } catch (Exception ex) {
-            LOGGER.error(ex, "Couldn't add pool with name '" + name +"': ", ex.getMessage());
-        }
+        String data = url.substring(url.indexOf("://") + 3);
+        String[] parts = data.split("/");
+        return parts;
     }
-
-    /**
-     * Constructs the context path if not available yet.
-     * @param ctx
-     * @param path
-     */
-    private void constructContext(final Context ctx, final String... path) {
-        StringWriter current = new StringWriter();
-        current.append("java:");
-        for (String key : path) {
-            try {
-                current.append("/" + key);
-                ctx.createSubcontext(current.toString());
-            } catch (Exception ignored) {
-                LOGGER.ignore("Ignore context creation fail", ignored);
-            }
-        }
-    }
-
-    /**
-     * Creates an InitialContext if not created yet.
-     */
-    public Context getContext() {
-        if(context == null) {
-            try {
-                context = new InitialContext();
-            } catch (Exception ex) {
-                LOGGER.error("Couldn't get context: ", ex.getMessage());
-            }
-        }
-        return context;
-    }
-
-    /**
-     * Clean up created resources
-     */
-    public void teardown() {
-        // clean up created datasources
-        for(BasicDataSource ds : localDataSources) {
-            try {
-                ds.close();
-                LOGGER.debug("Closed locally created data source");
-            } catch (final SQLException e) {
-                LOGGER.error(e, "Failed to close locally created data source");
-            }
-        }
-    }
-
 }
