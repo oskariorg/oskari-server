93abef443f405bc36e60962a93447ff86ded5c5b
Sami Mäkinen <sami.makinen@nls.fi>
2015-09-24 11:07:23 +0300

    Removed unnecessary import

--- service-map/src/main/java/fi/nls/oskari/map/myplaces/service/GeoServerProxyService.java
+++ service-map/src/main/java/fi/nls/oskari/map/myplaces/service/GeoServerProxyService.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.util.zip.GZIPInputStream;
 
 public class GeoServerProxyService {
 
