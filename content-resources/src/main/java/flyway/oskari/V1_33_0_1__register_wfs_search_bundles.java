bf16e09e2fece8c33bffa7028d29345eb885bf2f
Sami Mäkinen <sami.makinen@nls.fi>
2015-10-02 13:44:18 +0300

    Added flyway script for wfs search channels

---
+++ content-resources/src/main/java/flyway/oskari/V1_33_0_1__register_wfs_search_bundles.java
@@ -0,0 +1,30 @@
+package flyway.oskari;
+
+import fi.nls.oskari.db.BundleHelper;
+import fi.nls.oskari.domain.map.view.Bundle;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+
+import java.sql.Connection;
+
+/**
+ * Created by SMAKINEN on 25.8.2015.
+ */
+public class V1_33_0_1__register_wfs_search_bundles implements JdbcMigration {
+
+    private static final String NAMESPACE = "tampere";
+    private static final String ADMIN = "admin-wfs-search-channel";
+    private static final String SEARCH = "search-from-channels";
+
+    public void migrate(Connection connection) {
+        // BundleHelper checks if these bundles are already registered
+        Bundle admin = new Bundle();
+        admin.setName(ADMIN);
+        admin.setStartup(BundleHelper.getDefaultBundleStartup(NAMESPACE, ADMIN, "WFS Search channel admin"));
+        BundleHelper.registerBundle(admin);
+
+        Bundle search = new Bundle();
+        search.setName(SEARCH);
+        search.setStartup(BundleHelper.getDefaultBundleStartup(NAMESPACE, SEARCH, "SearchFromChannelsBundle"));
+        BundleHelper.registerBundle(search);
+    }
+}
