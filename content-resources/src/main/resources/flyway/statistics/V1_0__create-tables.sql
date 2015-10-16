efb93bbcc899f3e51e8225c09427e00b50d18122
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2015-10-16 14:40:37 +0300

    Typo fixed.

--- content-resources/src/main/resources/flyway/statistics/V1_0__create-tables.sql
+++ content-resources/src/main/resources/flyway/statistics/V1_0__create-tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS oskari_statistical_datasource_plugins
   class_name        CHARACTER VARYING(256) NOT NULL,
   localized_name_id CHARACTER VARYING(256) NOT NULL,
   -- This is just for guaranteeing uniqueness, this table will never have more than a few rows, all fetched at once.
-  CONSTRAINT categories_pkey PRIMARY KEY (class_name)
+  CONSTRAINT oskari_statistical_datasource_plugins_pkey PRIMARY KEY (class_name)
 );
 
 INSERT INTO oskari_statistical_datasource_plugins (class_name, localized_name_id) VALUES (
