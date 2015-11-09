275ca98da2fbc1d7ad71988617d44688b8f5b1d7
Sami Mäkinen <sami.makinen@nls.fi>
2015-11-09 15:47:40 +0200

    Default to adaptable config with infobox_ol3

--- content-resources/src/main/java/flyway/oskari/V1_34_1__register_initial_ol3_bundles.java
+++ content-resources/src/main/java/flyway/oskari/V1_34_1__register_initial_ol3_bundles.java
@@ -3,6 +3,7 @@ package flyway.oskari;
 import fi.nls.oskari.db.BundleHelper;
 import fi.nls.oskari.domain.map.view.Bundle;
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+import org.json.JSONException;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -16,11 +17,12 @@ public class V1_34_1__register_initial_ol3_bundles implements JdbcMigration {
     private static final String BUNDLE_ID_INFOBOX = "infobox_ol3";
     private static final String BUNDLE_ID_TOOLBAR = "toolbar_ol3";
 
-    public void migrate(Connection connection) throws SQLException {
+    public void migrate(Connection connection) throws SQLException, JSONException {
         // BundleHelper checks if these bundles are already registered
         Bundle bundle_infobox = new Bundle();
         bundle_infobox.setName(BUNDLE_ID_INFOBOX);
         bundle_infobox.setStartup(BundleHelper.getBundleStartup(PATH, BUNDLE_ID_INFOBOX, "Infobox OL3"));
+        bundle_infobox.getConfigJSON().putOpt("adaptable", true);
         BundleHelper.registerBundle(bundle_infobox, connection);
 
         Bundle bundle_toolbar = new Bundle();
