4a1e74939c7d3af6e75d197c80635c3f9db583a0
Sami Mäkinen <sami.makinen@nls.fi>
2015-11-09 17:28:28 +0200

    Tuning the ol3 publish template

--- content-resources/src/main/java/flyway/oskari/V1_34_0__register_drawtools_bundles.java
+++ content-resources/src/main/java/flyway/oskari/V1_34_0__register_drawtools_bundles.java
@@ -12,20 +12,14 @@ import java.sql.SQLException;
  */
 public class V1_34_0__register_drawtools_bundles implements JdbcMigration {
 
-    private static final String PATH = "/Oskari/packages/mapping/";
-    private static final String BUNDLE_ID_OL2 = "drawtools_ol2";
-    private static final String BUNDLE_ID_OL3 = "drawtools_ol3";
+    private static final String PATH = "/Oskari/packages/mapping/ol2/";
+    private static final String BUNDLE_ID = "drawtools";
 
     public void migrate(Connection connection) throws SQLException {
         // BundleHelper checks if these bundles are already registered
-        Bundle bundle_ol2 = new Bundle();
-        bundle_ol2.setName(BUNDLE_ID_OL2);
-        bundle_ol2.setStartup(BundleHelper.getBundleStartup(PATH, BUNDLE_ID_OL2, "DrawTools OL2"));
-        BundleHelper.registerBundle(bundle_ol2, connection);
-
-        Bundle bundle_ol3 = new Bundle();
-        bundle_ol3.setName(BUNDLE_ID_OL3);
-        bundle_ol3.setStartup(BundleHelper.getBundleStartup(PATH, BUNDLE_ID_OL3, "DrawTools OL3"));
-        BundleHelper.registerBundle(bundle_ol3, connection);
+        Bundle bundle = new Bundle();
+        bundle.setName(BUNDLE_ID);
+        bundle.setStartup(BundleHelper.getBundleStartup(PATH, BUNDLE_ID, "DrawTools"));
+        BundleHelper.registerBundle(bundle, connection);
     }
 }
