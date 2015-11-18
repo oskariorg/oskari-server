21277d5c8c738c69ab71559233dc4c22cf513d36
Anil Maharjan <anil.maharjan@luke.fi>
2015-11-18 15:14:44 +0200

    Sending email to recipient is added.

--- control-users/src/main/java/fi/nls/oskari/control/users/PasswordResetHandler.java
+++ control-users/src/main/java/fi/nls/oskari/control/users/PasswordResetHandler.java
@@ -2,7 +2,15 @@ package fi.nls.oskari.control.users;
 
 import java.sql.Timestamp;
 import java.util.Calendar;
+import java.util.Properties;
 import java.util.UUID;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,11 +21,14 @@ import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.control.users.model.Email;
 import fi.nls.oskari.control.users.service.IbatisEmailService;
+import fi.nls.oskari.util.PropertyUtil;
 import fi.nls.oskari.util.ResponseHelper;
 
 @OskariActionRoute("UserPasswordReset")
 public class PasswordResetHandler extends ActionHandler {
 
+	private String requestEmail = "";
+	
     @Override
     public void handleAction(ActionParameters params) throws ActionException {
         // TODO should only handle POST requests (but best to do that last, it is easier to develop/debug with GET requests)
@@ -26,17 +37,19 @@ public class PasswordResetHandler extends ActionHandler {
         // Return SUCCESS status. Do this even if nothing was sent because client should not be allowed to know whether the
         // provided email address exists in the database.
     	
-    	String requestEmail = params.getRequest().getParameter("email");
+    	requestEmail = params.getRequest().getParameter("email");
+    	String uuid = UUID.randomUUID().toString();
     	
     	Email email = new Email();
     	email.setEmail(requestEmail);
     	email.setScreenname("Test");
-    	email.setUuid(UUID.randomUUID().toString());
+    	email.setUuid(uuid);
     	email.setExpiryTimestamp(createExpiryTime());
     	
     	IbatisEmailService emailService = new IbatisEmailService();
     	emailService.addEmail(email);
     	
+    	sendEmail(requestEmail, uuid);
         JSONObject result = new JSONObject();
         try {
             result.put("status", "SUCCESS");
@@ -55,5 +68,37 @@ public class PasswordResetHandler extends ActionHandler {
         return expiryTime;
     }
 
+    /**
+     * While sending email smtp host and sender should be added to oskari-ext.properties
+     * e.g: oskari.email.sender=abc@def.com
+     * 		oskari.email.host=smtp.domain.com
+     * @param to Receiver's email address
+     * @param uuid Token number to be sent with email.
+     */
+    private void sendEmail(String to, String uuid){
+    	String from;
+    	Properties properties;
+    	
+    	try {
+    		from = PropertyUtil.get("oskari.email.sender");
+        	properties = System.getProperties();
+        	properties.setProperty("mail.smtp.host", PropertyUtil.get("oskari.email.host"));
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		return;
+    	}
+    	
+    	Session session = Session.getDefaultInstance(properties);
+    	 try {
+    		 MimeMessage mimeMessage = new MimeMessage(session);
+    		 mimeMessage.setFrom(new InternetAddress(from));
+    		 mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+    		 mimeMessage.setSubject("TEST");
+    		 mimeMessage.setContent("<h1>Token : " + uuid + " </h1>", "text/html" );
+             Transport.send(mimeMessage);
+          } catch (MessagingException ex) {
+             ex.printStackTrace();
+          }
+    }
 
 }
