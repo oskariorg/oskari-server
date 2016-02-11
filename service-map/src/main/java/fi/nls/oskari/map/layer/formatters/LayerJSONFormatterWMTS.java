5b9f2aef7ecb967304d99ac71edb958330cd4838
kessu <tapio.keisteri@nls.fi>
2016-02-11 12:12:54 +0200

    [AH-3003] supported wmts matrix id fix

--- service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterWMTS.java
+++ service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterWMTS.java
@@ -34,7 +34,10 @@ public class LayerJSONFormatterWMTS extends LayerJSONFormatter {
         final JSONObject layerJson = getBaseJSON(layer, lang, isSecure);
 
         // Use capabities in 1st hand for to get matrix id
-
+        String mid = LayerJSONFormatterWMTS.getTileMatrixSetId(layer.getCapabilities(), layer.getSrs_name());
+        if(mid != null){
+            layer.setTileMatrixSetId(mid);
+        }
         JSONHelper.putValue(layerJson, "tileMatrixSetId", layer.getTileMatrixSetId());
 
         // TODO: parse tileMatrixSetData for styles and set default style name from the one where isDefault = true
