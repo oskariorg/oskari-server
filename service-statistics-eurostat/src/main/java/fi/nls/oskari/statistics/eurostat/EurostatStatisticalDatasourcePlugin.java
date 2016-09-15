f939eae0bd9459f78dd4da0d4989384606cbd367
Sami Mäkinen <sami.makinen@nls.fi>
2016-09-15 15:42:07 +0300

    Initial Maven module for Eurostat adapter

---
+++ service-statistics-eurostat/src/main/java/fi/nls/oskari/statistics/eurostat/EurostatStatisticalDatasourcePlugin.java
@@ -0,0 +1,43 @@
+package fi.nls.oskari.statistics.eurostat;
+
+import fi.nls.oskari.control.statistics.plugins.StatisticalDatasourcePlugin;
+import fi.nls.oskari.control.statistics.plugins.StatisticalIndicator;
+import fi.nls.oskari.control.statistics.plugins.db.DatasourceLayer;
+import fi.nls.oskari.control.statistics.plugins.db.StatisticalDatasource;
+import fi.nls.oskari.domain.User;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EurostatStatisticalDatasourcePlugin implements StatisticalDatasourcePlugin {
+    private final static Logger LOG = LogFactory.getLogger(EurostatStatisticalDatasourcePlugin.class);
+
+    /**
+     * Maps the SotkaNET layer identifiers to Oskari layers.
+     */
+    private Map<String, Long> layerMappings;
+
+    @Override
+    public List<? extends StatisticalIndicator> getIndicators(User user) {
+        throw new RuntimeException("not implemented");
+    }
+
+    @Override
+    public void init(StatisticalDatasource source) {
+        final List<DatasourceLayer> layerRows = source.getLayers();
+        layerMappings = new HashMap<>();
+
+        for (DatasourceLayer row : layerRows) {
+            layerMappings.put(row.getSourceProperty().toLowerCase(), row.getMaplayerId());
+        }
+        LOG.debug("Eurostat layer mappings: ", layerMappings);
+    }
+
+    @Override
+    public boolean canCache() {
+        return true;
+    }
+}
