0e05df1e0e69dcd0164698fd720a37d56f4e8c3e
Zhe Zhang <zhe.zhang@nls.fi>
2016-11-02 15:05:44 +0200

    Caching enablers

--- service-statistics-eurostat/src/main/java/fi/nls/oskari/statistics/eurostat/EurostatConfig.java
+++ service-statistics-eurostat/src/main/java/fi/nls/oskari/statistics/eurostat/EurostatConfig.java
@@ -5,16 +5,21 @@ import org.json.JSONObject;
 
 public class EurostatConfig {
 
+    private long datasourceId;
     private String url;
 
 
-    EurostatConfig(JSONObject json) {
+    EurostatConfig(JSONObject json, long id) {
+        datasourceId = id;
         url = json.optString("url");
 
     }
 
     public String getUrl() {
         return url;
+    }
+    public long getId() {
+        return datasourceId;
     }
 
 
