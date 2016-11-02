03428f96b7d994d2356511368045a5fbca6e3e0b
Zhe Zhang <zhe.zhang@nls.fi>
2016-11-02 15:06:10 +0200

    Only including indicators that have the GEO dimension

--- service-statistics-eurostat/src/main/java/fi/nls/oskari/statistics/eurostat/EurostatIndicatorsParser.java
+++ service-statistics-eurostat/src/main/java/fi/nls/oskari/statistics/eurostat/EurostatIndicatorsParser.java
@@ -1,5 +1,6 @@
 package fi.nls.oskari.statistics.eurostat;
 
+import fi.nls.oskari.cache.JedisManager;
 import fi.nls.oskari.control.statistics.plugins.StatisticalIndicatorSelector;
 import fi.nls.oskari.control.statistics.plugins.StatisticalIndicatorSelectors;
 import fi.nls.oskari.control.statistics.plugins.db.DatasourceLayer;
@@ -16,9 +17,11 @@ import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,34 +42,33 @@ public class EurostatIndicatorsParser {
 
     }
 
-
-    public void setMetadata(EurostatIndicator indicator, String dataStructureID) throws Exception {
-
+    public boolean setMetadata(EurostatIndicator indicator, String dataStructureID) throws Exception {
+// here should return boolean
         XMLStreamReader reader = null;
+        boolean hasGEO = false;
 
-
-        try (InputStreamReader inputReader = new InputStreamReader(IOHelper.getConnection(this.getURL("/SDMX/diss-web/rest/datastructure/ESTAT/") + dataStructureID).getInputStream())) {
+        try (InputStreamReader inputReader =
+                     new InputStreamReader(
+                             getMetadata(this.getURL("/SDMX/diss-web/rest/datastructure/ESTAT/"), dataStructureID))) {
 
             reader = XMLInputFactory.newInstance().createXMLStreamReader(inputReader);
             StAXOMBuilder builder = new StAXOMBuilder(reader);
             OMElement eleMeta = builder.getDocumentElement();
-
             AXIOMXPath xpath_Codelist = XmlHelper.buildXPath("/mes:Structure/mes:Structures/str:Codelists/str:Codelist", NAMESPACE_CTX);
             AXIOMXPath xpath_codeID = XmlHelper.buildXPath("str:Code", this.NAMESPACE_CTX);
             AXIOMXPath xpath_codeName = XmlHelper.buildXPath("com:Name", this.NAMESPACE_CTX);
-
             AXIOMXPath dimensionPath = XmlHelper.buildXPath("/mes:Structure/mes:Structures/str:DataStructures/str:DataStructure/str:DataStructureComponents/str:DimensionList/str:Dimension", NAMESPACE_CTX);
-
-
-
             List<OMElement> codelists = xpath_Codelist.selectNodes(eleMeta);// here we have all the codelist indicators
             List<OMElement> dimension = dimensionPath.selectNodes(eleMeta);
             final StatisticalIndicatorSelectors selectors = new StatisticalIndicatorSelectors();
             indicator.setSelectors(selectors);
 
-            for (OMElement dimension1 : dimension){
+            for (OMElement dimension1 : dimension) {
                 String idDimension = dimension1.getAttributeValue(QName.valueOf("id"));// idDimension is { FREQ, UNIT, GEO}
-                if (idDimension.equals("GEO")){
+                if (idDimension.equals("GEO")) {
+                    // we need to know that this indicator has a region dimension ("GEO")
+                    hasGEO = true;
+                    // but we don't pass it as a parameter for the frontend -> skip to next one
                     continue;
                 }
                 StatisticalIndicatorSelector selector = new StatisticalIndicatorSelector(idDimension);
@@ -74,7 +76,7 @@ public class EurostatIndicatorsParser {
                 OMElement localRepresentationOme = XmlHelper.getChild(dimension1, "LocalRepresentation");
                 OMElement enumerationOme = XmlHelper.getChild(localRepresentationOme, "Enumeration");
                 OMElement refOme = XmlHelper.getChild(enumerationOme, "Ref");
-                String refId= refOme.getAttributeValue(QName.valueOf("id")); // { refId is CL_FREQ, CL_UNIT}
+                String refId = refOme.getAttributeValue(QName.valueOf("id")); // { refId is CL_FREQ, CL_UNIT}
 
                 for (OMElement codelistElem : codelists) {
                     String id = codelistElem.getAttributeValue(QName.valueOf("id"));// Codelist ID = CL_FREQ
@@ -90,9 +92,7 @@ public class EurostatIndicatorsParser {
                         for (OMElement selectValue : codeName) {
                             String selectValues = selectValue.getText();                 // selectedValues = codeName "Daily, weekly "
                             selector.addAllowedValue(nameID, selectValues);
-
                         }
-
                     }
                     selectors.addSelector(selector);
                 }
@@ -108,7 +108,10 @@ public class EurostatIndicatorsParser {
                 } catch (Exception ignore) {
                 }
             }
+
+
         }
+        return hasGEO;
     }
 
     private void populateTimeDimension(StatisticalIndicatorSelectors selectors) {
@@ -126,8 +129,6 @@ public class EurostatIndicatorsParser {
     public String getURL(final String pUrl) throws IOException {
         return config.getUrl() + pUrl;
     }
-
-
 
     public List<EurostatIndicator> parse(List<DatasourceLayer> layers) {
 
@@ -156,14 +157,24 @@ public class EurostatIndicatorsParser {
             AXIOMXPath xpath_names = XmlHelper.buildXPath("com:Name", NAMESPACE_CTX);
 
             List<OMElement> indicatorsElement = xpath_indicator.selectNodes(ele);
-            int count = 0;
-            for (OMElement indicator : indicatorsElement) { // str:Dataflow
+            int count= 0;
+            for (OMElement indicator : indicatorsElement) {
+                // str:Dataflow
                 count++;
-                if (count > 550 && count<600) {
+                if (count < 550 || count>600) {
+                    // at this range there should be indicators with NUTS areas as regions
+                    continue;
+                }
+                if (list.size() > 10) {
+                    // have 10 indicators with geo:
+                    // break so we don't need to wait all day for the rest to load
+                    break;
+                }
                 EurostatIndicator item = new EurostatIndicator();
                 String id = indicator.getAttributeValue(QName.valueOf("id"));
                 item.setId(id);// itemId = nama_gdp_c
-                list.add(item);
+
+
                 List<OMElement> names = xpath_names.selectNodes(indicator);
                 for (OMElement name : names) {                                     // com:Name item (language)="en", item(indicatorName)= "sold production..."
                     String language = XmlHelper.getAttributeValue(name, "lang");
@@ -175,12 +186,13 @@ public class EurostatIndicatorsParser {
                 OMElement struct = XmlHelper.getChild(indicator, "Structure");
                 OMElement ref = XmlHelper.getChild(struct, "Ref");
                 String DSDid = ref.getAttributeValue(QName.valueOf("id"));
-                    setMetadata(item, DSDid);
-
+                boolean indicatorHasRegion = setMetadata(item, DSDid);
+                if (indicatorHasRegion) {
+                    list.add(item);
+                }
                 for (DatasourceLayer layer : layers) {
                     item.addLayer(new EurostatStatisticalIndicatorLayer(layer.getMaplayerId(), item.getId(), config.getUrl()));
                 }
-            }
             }
 
         } catch (java.lang.Exception e) {
@@ -196,4 +208,20 @@ public class EurostatIndicatorsParser {
         return list;
     }
 
+    private InputStream getMetadata(String pUrl, String dataStructureID) {
+        final String url = pUrl + dataStructureID;
+        final String cacheKey = "stats:" + config.getId() + ":metadata:" + url;
+        try {
+            String metadata = JedisManager.get(cacheKey);
+            if(metadata == null) {
+                metadata = IOHelper.getURL(url);
+                JedisManager.setex(cacheKey, JedisManager.EXPIRY_TIME_DAY, metadata);
+            }
+            return new ByteArrayInputStream(metadata.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException ex) {
+            LOG.error(ex, "Error getting indicator metadata from Pxweb datasource:", url);
+        }
+        return null;
+
+    }
 }
