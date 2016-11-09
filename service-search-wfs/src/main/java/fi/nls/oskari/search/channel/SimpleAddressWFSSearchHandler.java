a067a1b5b407ace2ea6c9b5e6d46ec701170b780
Sami Mäkinen <sami.makinen@nls.fi>
2016-11-09 09:16:17 +0200

    XML-escaping and warning for missing parameters on address handler

--- service-search-wfs/src/main/java/fi/nls/oskari/search/channel/SimpleAddressWFSSearchHandler.java
+++ service-search-wfs/src/main/java/fi/nls/oskari/search/channel/SimpleAddressWFSSearchHandler.java
@@ -6,6 +6,7 @@ import fi.nls.oskari.domain.SelectItem;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.wfs.WFSSearchChannelsConfiguration;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.json.JSONArray;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class SimpleAddressWFSSearchHandler extends WFSChannelHandler {
         JSONArray params = config.getParamsForSearch();
         if(params.length() < 2) {
             // default to simple handler if less than 2 params or all numbers
+            log.warn("Tried to use address handler, but it needs 2 parameters. Using default handler instead.");
             return super.createFilter(sc, config);
         }
         StringBuffer filter = new StringBuffer("<Filter>");
@@ -40,15 +42,14 @@ public class SimpleAddressWFSSearchHandler extends WFSChannelHandler {
             streetNumber = lastWord;
         }
 
-        // FIXME: escape param and searchStr
         filter.append("<PropertyIsLike wildCard='*' singleChar='>' escape='!' matchCase='false'>" +
-                "<PropertyName>"+params.optString(0)+"</PropertyName><Literal>"+ streetName +
-                "*</Literal></PropertyIsLike>"
+                "<PropertyName>" +  StringEscapeUtils.escapeXml(params.optString(0))+"</PropertyName><Literal>" +
+                StringEscapeUtils.escapeXml(streetName) + "*</Literal></PropertyIsLike>"
         );
 
         filter.append("<PropertyIsLike wildCard='*' singleChar='>' escape='!' matchCase='false'>" +
-                "<PropertyName>"+params.optString(1)+"</PropertyName><Literal>"+ streetNumber +
-                "*</Literal></PropertyIsLike>"
+                "<PropertyName>" + StringEscapeUtils.escapeXml(params.optString(1)) + "</PropertyName><Literal>" +
+                StringEscapeUtils.escapeXml(streetNumber) + "*</Literal></PropertyIsLike>"
         );
 
         filter.append("</And>");
