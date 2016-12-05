c7554670e05f4bfd1c7be0c58c4273f1c0aed84e
Sami Mäkinen <sami.makinen@nls.fi>
2016-12-05 18:40:47 +0200

    Change mapstats path on mapfull-template

---
+++ content-resources/src/main/resources/flyway/oskari/V1_41_0__update_mapfull_template.sql
@@ -0,0 +1,76 @@
+UPDATE portti_bundle SET startup='{
+    "bundlename" : "mapfull"
+    "metadata" : {
+        "Import-Bundle" : {
+            "core-base" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "core-map" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "sandbox-base" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "sandbox-map" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "event-base" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "event-map" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "event-map-layer" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "request-base" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "request-map" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "request-map-layer" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "service-base" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "service-map" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "domain" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "mapmodule-plugin" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "mapwfs2" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "mapwmts" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "mapstats" : {
+                "bundlePath" : "/Oskari/packages/mapping/ol2/"
+            },
+            "mapuserlayers" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "mapanalysis" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+             "mapmyplaces" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "oskariui" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "mapfull" : {
+                "bundlePath" : "/Oskari/packages/framework/bundle/"
+            },
+            "ui-components": {
+                "bundlePath": "/Oskari/packages/framework/bundle/"
+            }
+        }
+    }
+}' where name = 'mapfull';
