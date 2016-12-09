9516aa9fc1767f07ce065429e0b0486c503dcd4d
Sami Mäkinen <sami.makinen@nls.fi>
2016-12-09 15:40:22 +0200

    Removed rolefetching that is already available in roleservice

--- control-users/src/main/java/fi/nls/oskari/control/users/service/UserRegistrationService.java
+++ control-users/src/main/java/fi/nls/oskari/control/users/service/UserRegistrationService.java
@@ -108,13 +108,4 @@ public class UserRegistrationService extends OskariComponent {
         throw new RuntimeException("Couldn't find email token for " + username);
     }
 
-    public Integer findUserRoleId(String name) {
-        try (SqlSession session = factory.openSession()) {
-            final EmailMapper mapper = session.getMapper(EmailMapper.class);
-            return mapper.findUserRoleId(name);
-        } catch (Exception e) {
-            LOG.warn(e, "Exception when trying to delete by email:", name);
-        }
-        throw new RuntimeException("Couldn't delete email token for " + name);
-    }
 }
