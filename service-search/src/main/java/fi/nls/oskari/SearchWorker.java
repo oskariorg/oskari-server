0c60f9a827311c808e240d4ddbb6d88c618c1dd7
Sami Mäkinen <sami.makinen@nls.fi>
2016-12-19 16:28:55 +0200

    Removed unused methods and simplified doSearch

--- service-search/src/main/java/fi/nls/oskari/SearchWorker.java
+++ service-search/src/main/java/fi/nls/oskari/SearchWorker.java
@@ -1,23 +1,15 @@
 package fi.nls.oskari;
 
 import fi.mml.portti.service.search.*;
-import fi.nls.oskari.log.LogFactory;
-import fi.nls.oskari.log.Logger;
-import fi.nls.oskari.search.channel.SearchableChannel;
 import fi.nls.oskari.util.ConversionHelper;
 import fi.nls.oskari.util.JSONHelper;
-import fi.nls.oskari.util.PropertyUtil;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 public class SearchWorker {
 
@@ -36,18 +28,6 @@ public class SearchWorker {
     public static final String STR_NULL = "null";
     /** Our service */
     private static SearchService searchService = new SearchServiceImpl();
-
-    private final static Logger log = LogFactory.getLogger(SearchWorker.class);
-
-    private static String[] defaultChannels = new String[0];
-
-    public static void init() {
-        defaultChannels = PropertyUtil.getCommaSeparatedList("search.channels.default");
-    }
-
-    public static void addChannel(String channelId, SearchableChannel searchableChannel) {
-        searchService.addChannel(channelId, searchableChannel);
-    }
 
     /**
      * Checks if search was legal
@@ -70,104 +50,33 @@ public class SearchWorker {
         }
         return STR_TRUE;
     }
-
-    /**
-     * Does actual search
-     * 
-     * @param searchString
-     * @param locale
-     */
-    public static JSONObject doSearch(final String searchString,
-            final Locale locale) {
-        String err = SearchWorker.checkLegalSearch(searchString);
-        if (err == null) {
-            throw new RuntimeException("Could not check search string");
-        }
-        if (!err.equals(STR_TRUE)) {
-            JSONObject errJson = new JSONObject();
-            try {
-                errJson.put(KEY_TOTAL_COUNT, -1);
-                errJson.put(KEY_ERROR_TEXT, err);
-            } catch (JSONException jsonex) {
-                throw new RuntimeException("Could not form error JSON");
-            }
-            return errJson;
-        }
-
-        final SearchCriteria sc = new SearchCriteria();
-        sc.setSearchString(searchString);
-
-        sc.setLocale(locale.getLanguage());
-        if(defaultChannels.length == 0) {
-            init();
-        }
-
-        for(String channelId : defaultChannels) {
-            sc.addChannel(channelId);
-        }
-
-        return doSearch(sc);
-    }
     
     public static JSONObject doSearch(final SearchCriteria sc) {
         
         Query query = searchService.doSearch(sc);
+        int maxResults = searchService.getMaxResultsCount();
+        List<SearchResultItem> items = query.getSortedResults(maxResults + 1);
 
-        List<SearchResultItem> items = new ArrayList<SearchResultItem>();
-        JSONArray methodArray = new JSONArray();
-        for(String channelId : sc.getChannels()) {
-            //log.debug("channelId = " + channelId);
-            items.addAll(query.findResult(channelId).getSearchResultItems());
-            methodArray.put(JSONHelper.createJSONObject(channelId,query.findResult(channelId).getSearchMethod()));
+        JSONObject result = new JSONObject();
+        if(items.size() > maxResults) {
+            JSONHelper.putValue(result, KEY_HAS_MORE, true);
+            items = items.subList(0, maxResults);
         }
-        Collections.sort(items);
 
-        JSONObject rootJson = new JSONObject();
         JSONArray itemArray = new JSONArray();
 
-        int maxResults = searchService.getMaxResultsCount();
         int itemCount = 0;
         for (SearchResultItem sri : items) {
-            if (itemCount >= maxResults) {
-                if (items.size() > maxResults) {
-                    try {
-                        rootJson.put(KEY_HAS_MORE, true);
-                    } catch (JSONException jsonex) {
-                        throw new RuntimeException("Could not set"
-                                + " hasMore in JSON");
-                    }
-                }
-                break;
-            }
-            if(!sri.hasNameAndLocation()) {
-                // didn't get name/location -> skip to next result
-                log.warn("Didn't get name or location from search result item:", sri);
-                continue;
-            }
-
-            itemArray.put(sri.toJSON(itemCount));
-
-            // Success
-            itemCount++;
+            itemArray.put(sri.toJSON(itemCount++));
         }
+        JSONHelper.putValue(result, KEY_TOTAL_COUNT, itemCount);
+        JSONHelper.putValue(result, KEY_LOCATIONS, itemArray);
 
-        try {
-            rootJson.put(KEY_LOCATIONS, itemArray);
-        } catch (JSONException jsonex) {
-            throw new RuntimeException("Could not set search items in JSON");
+        JSONArray methodArray = new JSONArray();
+        for(String channelId : sc.getChannels()) {
+            methodArray.put(JSONHelper.createJSONObject(channelId, query.findResult(channelId).getSearchMethod()));
         }
-
-        try {
-            rootJson.put(KEY_METHODS, methodArray);
-        } catch (JSONException jsonex) {
-            throw new RuntimeException("Could not set search method items in JSON");
-        }
-
-        try {
-            rootJson.put(KEY_TOTAL_COUNT, itemCount);
-        } catch (JSONException jsonex) {
-            throw new RuntimeException("Could not set item count in JSON");
-        }
-        return rootJson;
+        JSONHelper.putValue(result, KEY_METHODS, methodArray);
+        return result;
     }
 }
