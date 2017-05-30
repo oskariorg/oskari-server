44cbee5e644349dd8d37fda4ec4a0f2d804bd7e3
MTYNI <mikael.k.tyni@gmail.com>
2017-05-30 17:01:45 +0300

    feature/registration-improvements: -Added user profile link to configuration / My Account -Added deletion of user content: User indicator

---
+++ control-users/src/main/java/fi/nls/oskari/control/users/PersonalDataHandler.java
@@ -0,0 +1,25 @@
+package fi.nls.oskari.control.users;
+
+import fi.nls.oskari.annotation.OskariViewModifier;
+import fi.nls.oskari.control.view.modifier.bundle.BundleHandler;
+import fi.nls.oskari.util.JSONHelper;
+import fi.nls.oskari.util.PropertyUtil;
+import fi.nls.oskari.view.modifier.ModifierException;
+import fi.nls.oskari.view.modifier.ModifierParams;
+import org.json.JSONObject;
+
+@OskariViewModifier("personaldata")
+public class PersonalDataHandler extends BundleHandler {
+
+    private static final String PROFILE_URL = "changeInfoUrl";
+
+    @Override
+    public boolean modifyBundle(ModifierParams params) throws ModifierException {
+        if(!PropertyUtil.getOptional("allow.registration", false)) {
+            return false;
+        }
+        final JSONObject config = getBundleConfig(params.getConfig());
+        JSONHelper.putValue(config, PROFILE_URL, PropertyUtil.get("auth.register.url", "/user"));
+        return false;
+    }
+}
