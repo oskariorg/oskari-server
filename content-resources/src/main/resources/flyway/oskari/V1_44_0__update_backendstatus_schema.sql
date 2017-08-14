f518661ffa013ac8ba8f1a1dd9522ad5e416949f
jampukka <janne.heikkila@maanmittauslaitos.fi>
2017-08-14 15:24:06 +0300

    Remove IF EXISTS, we sort of want to fail if no such table exists, fix truncate statement

--- content-resources/src/main/resources/flyway/oskari/V1_44_0__update_backendstatus_schema.sql
+++ content-resources/src/main/resources/flyway/oskari/V1_44_0__update_backendstatus_schema.sql
@@ -5,17 +5,17 @@ DROP VIEW IF EXISTS portti_backendstatus_allknown;
 DROP VIEW IF EXISTS portti_backendalert;
 
 -- modernise the name of the table
-ALTER TABLE IF EXISTS portti_backendstatus RENAME TO oskari_backendstatus;
+ALTER TABLE portti_backendstatus RENAME TO oskari_backendstatus;
 
 -- drop columns that are not needed
-ALTER TABLE IF EXISTS oskari_backendstatus DROP COLUMN id;
-ALTER TABLE IF EXISTS oskari_backendstatus DROP COLUMN source;
-ALTER TABLE IF EXISTS oskari_backendstatus DROP COLUMN statusjson;
+ALTER TABLE oskari_backendstatus DROP COLUMN id;
+ALTER TABLE oskari_backendstatus DROP COLUMN source;
+ALTER TABLE oskari_backendstatus DROP COLUMN statusjson;
 
 -- change the type of maplayer_id to int
-TRUNCATE TABLE IF EXISTS oskari_backendstatus; -- clean the table, make sure the next operation won't fail
-ALTER TABLE IF EXISTS oskari_backendstatus ALTER maplayer_id TYPE int USING maplayer_id::int;
-ALTER TABLE IF EXISTS oskari_backendstatus ALTER maplayer_id SET NOT NULL;
-ALTER TABLE IF EXISTS oskari_backendstatus ADD FOREIGN KEY (maplayer_id) REFERENCES oskari_maplayer(id);
+TRUNCATE oskari_backendstatus; -- clean the table, make sure the next operation won't fail
+ALTER TABLE oskari_backendstatus ALTER maplayer_id TYPE int USING maplayer_id::int;
+ALTER TABLE oskari_backendstatus ALTER maplayer_id SET NOT NULL;
+ALTER TABLE oskari_backendstatus ADD FOREIGN KEY (maplayer_id) REFERENCES oskari_maplayer(id);
 
 COMMIT;
