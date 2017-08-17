795dccf9e5cb888a206168e1265177193a34c354
ollijak <ollijak@WSLT38.dimenteq.ad>
2017-08-17 14:52:14 +0300

    fix #2 for previous commit

--- service-map/src/main/java/fi/mml/map/mapwindow/util/OskariLayerWorker.java
+++ service-map/src/main/java/fi/mml/map/mapwindow/util/OskariLayerWorker.java
@@ -276,7 +276,7 @@ public class OskariLayerWorker {
             JSONHelper.putValue(permission, "publish", PUBLICATION_PERMISSION_OK);
             JSONHelper.putValue(permission, "download", DOWNLOAD_PERMISSION_OK);
         } else if (user.isGuest()) {
-          JSONHelper.putValue(permission, "publish", NO_PUBLICATION_PERMISSION);
+            JSONHelper.putValue(permission, "publish", NO_PUBLICATION_PERMISSION);
         } else {
             for (Role role : user.getRoles()) {
                 if (editAccessList != null && editAccessList.contains(layerPermissionKey + ":" + role.getId())) {
