202b44fe1ab83c3c479d4c45685f0e08b9c3d76b
MTYNI <mikael.k.tyni@gmail.com>
2017-08-22 11:59:45 +0300

    feature/mybatis-migration: -Fixed InspireThemesHandlerTest

--- control-base/src/test/java/fi/nls/oskari/control/data/InspireThemesHandlerTest.java
+++ control-base/src/test/java/fi/nls/oskari/control/data/InspireThemesHandlerTest.java
@@ -7,20 +7,20 @@ import fi.nls.oskari.control.ActionDeniedException;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.control.ActionParamsException;
 import fi.nls.oskari.domain.map.InspireTheme;
-import fi.nls.oskari.util.DuplicateException;
 import fi.nls.oskari.util.PropertyUtil;
 import fi.nls.test.control.JSONActionRouteTest;
 import fi.nls.test.util.ResourceHelper;
-import org.junit.After;
+import fi.nls.test.util.TestHelper;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -39,13 +39,7 @@ public class InspireThemesHandlerTest extends JSONActionRouteTest {
 
     @Before
     public void setUp() throws Exception {
-        Properties properties = new Properties();
-        try {
-            properties.load(InspireThemesHandlerTest.class.getResourceAsStream("test.properties"));
-            PropertyUtil.addProperties(properties);
-        } catch (DuplicateException e) {
-            //fail("Should not throw exception" + e.getStackTrace());
-        }
+        assumeTrue(TestHelper.dbAvailable());
         inspireThemeService = mock(InspireThemeServiceIbatisImpl.class);
         InspireTheme theme1 = new InspireTheme();
         theme1.setId(1);
@@ -145,8 +139,8 @@ public class InspireThemesHandlerTest extends JSONActionRouteTest {
         return map;
     }
 
-    @After
-    public void delete() {
+    @AfterClass
+    public static void delete() {
         PropertyUtil.clearProperties();
     }
 }
