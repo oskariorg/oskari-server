a3cc9be4bb07f6d88f43ac83d178abc3be176400
jampukka <janne.heikkila@maanmittauslaitos.fi>
2017-08-23 14:56:17 +0300

    Move logic that handles GetCapabilities information from SaveLayerHandler to service-map/OskariLayerCapabilitiesTools

--- service-capabilities-update/src/main/java/org/oskari/capabilities/UpdateCapabilitiesJob.java
+++ service-capabilities-update/src/main/java/org/oskari/capabilities/UpdateCapabilitiesJob.java
@@ -1,26 +1,17 @@
 package org.oskari.capabilities;
 
-import java.util.List;
 import java.util.Map;
 
-import org.json.JSONObject;
-
-import fi.mml.map.mapwindow.service.wms.WebMapService;
-import fi.mml.map.mapwindow.service.wms.WebMapServiceFactory;
 import fi.nls.oskari.annotation.Oskari;
 import fi.nls.oskari.domain.map.OskariLayer;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.map.layer.OskariLayerService;
 import fi.nls.oskari.map.layer.OskariLayerServiceIbatisImpl;
-import fi.nls.oskari.map.layer.formatters.LayerJSONFormatterWMS;
-import fi.nls.oskari.map.layer.formatters.LayerJSONFormatterWMTS;
 import fi.nls.oskari.service.ServiceException;
 import fi.nls.oskari.service.capabilities.CapabilitiesCacheService;
 import fi.nls.oskari.service.capabilities.CapabilitiesCacheServiceMybatisImpl;
-import fi.nls.oskari.service.capabilities.OskariLayerCapabilities;
-import fi.nls.oskari.wmts.WMTSCapabilitiesParser;
-import fi.nls.oskari.wmts.domain.WMTSCapabilities;
+import fi.nls.oskari.service.capabilities.OskariLayerCapabilitiesHelper;
 import fi.nls.oskari.worker.ScheduledJob;
 
 /**
@@ -30,84 +21,68 @@ import fi.nls.oskari.worker.ScheduledJob;
 public class UpdateCapabilitiesJob extends ScheduledJob {
 
     private static final Logger LOG = LogFactory.getLogger(UpdateCapabilitiesJob.class);
-    
+
     private OskariLayerService layerService;
     private CapabilitiesCacheService capabilitiesService;
-    
+
     public UpdateCapabilitiesJob() {
         this(new OskariLayerServiceIbatisImpl(), new CapabilitiesCacheServiceMybatisImpl());
     }
-    
+
     public UpdateCapabilitiesJob(OskariLayerService layerService, CapabilitiesCacheService capabilitiesService) {
         this.layerService = layerService;
         this.capabilitiesService = capabilitiesService;
     }
-    
+
     @Override
     public void execute(Map<String, Object> params) {
-        updateCapabilities();
-    }
-    
-    protected void updateCapabilities() {
         for (OskariLayer layer : layerService.findAll()) {
             updateCapabilities(layer);
         }
     }
-    
+
     protected void updateCapabilities(OskariLayer layer) {
         if (layer == null) {
             return;
         }
-
-        switch (layer.getType()) {
-        case OskariLayer.TYPE_WMS:
-        case OskariLayer.TYPE_WMTS:
-            JSONObject capabilitiesJSON = getCapabilitiesJSON(layer);
-            if (capabilitiesJSON != null) {
-                layer.setCapabilities(capabilitiesJSON);
-            }
-            break;
-        default:
-            LOG.info("Skipping layer with type: ", layer.getType());
-            break;
-        }
-    }
-
-    protected JSONObject getCapabilitiesJSON(OskariLayer layer) {
         try {
-            OskariLayerCapabilities capabilities = capabilitiesService.getCapabilities(layer, true);
-            // flush cache, otherwise only db is updated but code retains the old cached version
-            WebMapServiceFactory.flushCache(layer.getId());
-
             switch (layer.getType()) {
             case OskariLayer.TYPE_WMS:
-                return getCapabilitiesJSON_WMS(layer, capabilities);
+                updateCapabilitiesWMS(layer);
+                break;
             case OskariLayer.TYPE_WMTS:
-                return getCapabilitiesJSON_WMTS(layer, capabilities);
+                updateCapabilitiesWMTS(layer);
+                break;
+            case OskariLayer.TYPE_WFS:
+                updateCapabilitiesWFS(layer);
+                break;
             default:
-                return null;
+                LOG.info("Can't update capabilities of type:", layer.getType(),
+                        "layer id:", layer.getId(), "skipping!");
+                return;
             }
+            layerService.update(layer);
         } catch (ServiceException e) {
-            return null;
+            LOG.warn(e, "Failed to update capabilities of layer id:", layer.getId());
         }
     }
 
-    private JSONObject getCapabilitiesJSON_WMS(OskariLayer layer, OskariLayerCapabilities capabilities) {
-        WebMapService wms = WebMapServiceFactory.createFromXML(layer.getName(), capabilities.getData());
-        if (wms == null) {
-            LOG.warn("Failed to parse WMS capabilities, layer: ", layer.getName());
-        }
-        return LayerJSONFormatterWMS.createCapabilitiesJSON(wms);
+    private void updateCapabilitiesWMS(OskariLayer layer) throws ServiceException {
+        OskariLayerCapabilitiesHelper.setPropertiesFromCapabilitiesWMS(
+                capabilitiesService.getCapabilities(layer, true), layer);
     }
 
-    private static JSONObject getCapabilitiesJSON_WMTS(OskariLayer layer, OskariLayerCapabilities capabilities) {
+    private void updateCapabilitiesWMTS(OskariLayer layer) throws ServiceException {
         try {
-            WMTSCapabilities caps = new WMTSCapabilitiesParser().parseCapabilities(capabilities.getData());
-            return LayerJSONFormatterWMTS.createCapabilitiesJSON(caps, caps.getLayer(layer.getName()));
+            OskariLayerCapabilitiesHelper.setPropertiesFromCapabilitiesWMTS(
+                    capabilitiesService.getCapabilities(layer, true), layer, null);
         } catch (Exception e) {
             LOG.warn(e, "Failed to parse WMTS capabilities, layer: ", layer.getName());
-            return null;
         }
+    }
+
+    private void updateCapabilitiesWFS(OskariLayer layer) throws ServiceException {
+        OskariLayerCapabilitiesHelper.setPropertiesFromCapabilitiesWFS(layer);
     }
 
 }
---
+++ service-map/src/main/java/fi/nls/oskari/service/capabilities/OskariLayerCapabilitiesHelper.java
@@ -0,0 +1,95 @@
+package fi.nls.oskari.service.capabilities;
+
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import fi.mml.map.mapwindow.service.wms.WebMapService;
+import fi.mml.map.mapwindow.service.wms.WebMapServiceFactory;
+import fi.nls.oskari.domain.map.OskariLayer;
+import fi.nls.oskari.map.layer.formatters.LayerJSONFormatterWMS;
+import fi.nls.oskari.map.layer.formatters.LayerJSONFormatterWMTS;
+import fi.nls.oskari.service.ServiceException;
+import fi.nls.oskari.util.JSONHelper;
+import fi.nls.oskari.wfs.GetGtWFSCapabilities;
+import fi.nls.oskari.wmts.WMTSCapabilitiesParser;
+import fi.nls.oskari.wmts.domain.ResourceUrl;
+import fi.nls.oskari.wmts.domain.WMTSCapabilities;
+import fi.nls.oskari.wmts.domain.WMTSCapabilitiesLayer;
+
+public class OskariLayerCapabilitiesHelper {
+
+    private static final String KEY_STYLES = "styles";
+    private static final String KEY_NAME = "name";
+
+    public static void setPropertiesFromCapabilitiesWMS(OskariLayerCapabilities capabilities, OskariLayer ml)
+            throws ServiceException {
+        // flush cache, otherwise only db is updated but code retains the old cached version
+        WebMapServiceFactory.flushCache(ml.getId());
+        // parse capabilities
+        WebMapService wms = WebMapServiceFactory.createFromXML(ml.getName(), capabilities.getData());
+        if (wms == null) {
+            throw new ServiceException("Couldn't parse capabilities for service!");
+        }
+        JSONObject caps = LayerJSONFormatterWMS.createCapabilitiesJSON(wms);
+        ml.setCapabilities(caps);
+        //TODO: similiar parsing for WMS GetCapabilities for admin layerselector  and this
+        // Parsing is processed twice:
+        // 1st with geotools parsing for admin layerselector (styles are not parsered correct in all cases)
+        // 2nd in this class
+        // Fix default style, if no legendimage setup
+        String style = getDefaultStyle(ml, caps);
+        if (style != null) {
+            ml.setStyle(style);
+        }
+
+        ml.setSupportedCRSs(LayerJSONFormatterWMS.getCRSs(wms));
+    }
+
+    private static String getDefaultStyle(OskariLayer ml, final JSONObject caps) {
+        String style = null;
+        if (ml.getId() == -1 && ml.getLegendImage() == null && caps.has(KEY_STYLES)) {
+            // Take 1st style name for default - geotools parsing is not always correct
+            JSONArray styles = JSONHelper.getJSONArray(caps, KEY_STYLES);
+            JSONObject jstyle = JSONHelper.getJSONObject(styles, 0);
+            if (jstyle != null) {
+                style = JSONHelper.getStringFromJSON(jstyle, KEY_NAME, null);
+                return style;
+            }
+        }
+        return style;
+    }
+
+    public static void setPropertiesFromCapabilitiesWMTS(OskariLayerCapabilities capabilities,
+            OskariLayer ml, String crs) throws Exception {
+        // parse capabilities
+        WMTSCapabilities caps = new WMTSCapabilitiesParser().parseCapabilities(capabilities.getData());
+        if (caps == null) {
+            throw new ServiceException("Couldn't parse capabilities for service!");
+        }
+        WMTSCapabilitiesLayer layer = caps.getLayer(ml.getName());
+        ResourceUrl resUrl = layer.getResourceUrlByType("tile");
+        JSONObject options = ml.getOptions();
+        if(resUrl != null) {
+            JSONHelper.putValue(options, "requestEncoding", "REST");
+            JSONHelper.putValue(options, "format", resUrl.getFormat());
+            JSONHelper.putValue(options, "urlTemplate", resUrl.getTemplate());
+        }
+
+        JSONObject jscaps = LayerJSONFormatterWMTS.createCapabilitiesJSON(caps, layer);
+        ml.setCapabilities(jscaps);
+
+        crs = crs != null ? crs : ml.getSrs_name();
+        ml.setTileMatrixSetId(LayerJSONFormatterWMTS.getTileMatrixSetId(jscaps, crs));
+
+        ml.setSupportedCRSs(LayerJSONFormatterWMTS.getCRSs(caps, layer));
+    }
+
+    public static void setPropertiesFromCapabilitiesWFS(OskariLayer ml) {
+        Map<String, Object> capa = GetGtWFSCapabilities.getGtDataStoreCapabilities(
+                ml.getUrl(), ml.getVersion(), ml.getUsername(), ml.getPassword(), ml.getSrs_name());
+        ml.setSupportedCRSs(GetGtWFSCapabilities.parseProjections(capa, ml.getVersion(), ml.getName()));
+    }
+
+}
