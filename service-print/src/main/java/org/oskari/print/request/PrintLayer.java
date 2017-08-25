1a7e02f89a3f0688e0f4702e54419e0f6e0927e2
jampukka <janne.heikkila@maanmittauslaitos.fi>
2017-08-25 15:58:01 +0300

    Foo

---
+++ service-print/src/main/java/org/oskari/print/request/PrintLayer.java
@@ -0,0 +1,114 @@
+package org.oskari.print.request;
+
+public class PrintLayer {
+
+    private String id;
+    private String type;
+    private String url;
+    private String name;
+    private String style;
+    private String srsName;
+    private String version;
+    private String tileMatrixSet;
+    private int opacity;
+    private String username;
+    private String password;
+    private Tile[] tiles;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTileMatrixSet() {
+        return tileMatrixSet;
+    }
+
+    public void setTileMatrixSet(String tileMatrixSet) {
+        this.tileMatrixSet = tileMatrixSet;
+    }
+
+    public int getOpacity() {
+        return opacity;
+    }
+
+    public void setOpacity(int opacity) {
+        this.opacity = opacity;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(Tile[] tiles) {
+        this.tiles = tiles;
+    }
+
+    public String getSrsName() {
+        return srsName;
+    }
+
+    public void setSrsName(String srsName) {
+        this.srsName = srsName;
+    }
+
+}
