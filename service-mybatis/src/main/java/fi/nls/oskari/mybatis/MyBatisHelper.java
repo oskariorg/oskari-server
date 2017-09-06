e49af21864251b4388926fcc3f486991cf4fedb2
Sami Mäkinen <zakarfin@gmail.com>
2017-09-06 17:07:23 +0300

    Moved more code to MyBatisHelper with Sonar fixes

--- service-mybatis/src/main/java/fi/nls/oskari/mybatis/MyBatisHelper.java
+++ service-mybatis/src/main/java/fi/nls/oskari/mybatis/MyBatisHelper.java
@@ -2,6 +2,7 @@ package fi.nls.oskari.mybatis;
 
 import javax.sql.DataSource;
 
+import fi.nls.oskari.service.ServiceRuntimeException;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,13 +13,30 @@ import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 public class MyBatisHelper {
 
     public static SqlSessionFactory initMyBatis(DataSource ds, Class<?>... mappers) {
+        return build(getConfig(ds, mappers));
+    }
+
+    public static Configuration getConfig(DataSource ds, Class<?>... mappers) {
+        if(ds == null) {
+            throw new ServiceRuntimeException("Tried initializing MyBatis without a datasource");
+        }
         final TransactionFactory transactionFactory = new JdbcTransactionFactory();
         final Environment environment = new Environment("development", transactionFactory, ds);
         final Configuration configuration = new Configuration(environment);
+        configuration.setLazyLoadingEnabled(true);
         for (Class<?> mapper : mappers) {
             configuration.addMapper(mapper);
         }
-        return new SqlSessionFactoryBuilder().build(configuration);
+        return configuration;
     }
 
+    public static void attachAliases(Configuration config, Class<?>... aliases) {
+        for (Class<?> alias : aliases) {
+            config.getTypeAliasRegistry().registerAlias(alias);
+        }
+    }
+
+    public static SqlSessionFactory build(Configuration config) {
+        return new SqlSessionFactoryBuilder().build(config);
+    }
 }
