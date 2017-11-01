5957d5ea8177b3edb55f4843e0d5d334e82e7fca
mtyni <mikael.k.tyni@gmail.com>
2017-11-01 15:32:46 +0200

    Added first version of features insert, with hard coded geometry.

--- control-base/src/main/java/fi/nls/oskari/util/GeoServerRequestBuilder.java
+++ control-base/src/main/java/fi/nls/oskari/util/GeoServerRequestBuilder.java
@@ -1,12 +1,21 @@
 package fi.nls.oskari.util;
 
-import fi.nls.oskari.control.data.MyPlacesLayersHandler.ModifyOperationType;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import org.apache.axiom.om.*;
+import org.geotools.data.DataUtilities;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.gml.producer.FeatureTransformer;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.geotools.geometry.jts.WKTReader2;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +24,7 @@ public class GeoServerRequestBuilder {
     private static final Logger log = LogFactory.getLogger(GeoServerRequestBuilder.class);
 
     private static final String VERSION_1_1_0 = "1.1.0";
+    private static final String VERSION_1_0_0 = "1.0.0";
 
     private static OMFactory factory = null;
     private static OMNamespace xmlSchemaInstance = null;
@@ -33,12 +43,16 @@ public class GeoServerRequestBuilder {
             "dot_shape", "stroke_linejoin", "fill_pattern", "stroke_linecap", "stroke_dasharray", "border_linejoin",
             "border_dasharray");
 
+
+    private static final List<String> FEATURES_LIST = Arrays.asList("name", "place_desc", "attention_text", "link",
+            "image_url", "category_id", "feature");
+
     public OMElement buildLayersGet(String uuid) {
 
         OMElement root = null;
 
         try {
-            root = buildWFSRootNode("GetFeature");
+            root = buildWFSRootNode("GetFeature", VERSION_1_1_0);
 
             OMElement query = factory.createOMElement("Query", wfsNameSpace);
             OMAttribute typeName = factory.createOMAttribute("typeName", null, "feature:categories");
@@ -79,7 +93,7 @@ public class GeoServerRequestBuilder {
         OMElement root = null;
 
         try {
-            root = buildWFSRootNode("Transaction");
+            root = buildWFSRootNode("Transaction", VERSION_1_1_0);
 
             OMElement transaction = factory.createOMElement("Insert", wfsNameSpace);
             OMNamespace feature = factory.createOMNamespace("http://www.oskari.org", "feature");
@@ -108,7 +122,7 @@ public class GeoServerRequestBuilder {
         OMElement root = null;
 
         try {
-            root = buildWFSRootNode("Transaction");
+            root = buildWFSRootNode("Transaction", VERSION_1_1_0);
 
             OMNamespace feature = factory.createOMNamespace("http://www.oskari.org", "feature");
             OMElement transaction = factory.createOMElement("Update", feature);
@@ -137,7 +151,7 @@ public class GeoServerRequestBuilder {
         OMElement root = null;
 
         try {
-            root = buildWFSRootNode("Transaction");
+            root = buildWFSRootNode("Transaction", VERSION_1_1_0);
 
             OMNamespace feature = factory.createOMNamespace("http://www.oskari.org", "feature");
             OMElement transaction = factory.createOMElement("Delete", feature);
@@ -157,19 +171,19 @@ public class GeoServerRequestBuilder {
 
     }
 
-    private OMElement buildWFSRootNode (String wfsType ) throws Exception {
+    private OMElement buildWFSRootNode (String wfsType, String version) throws Exception {
         OMElement root = factory.createOMElement(wfsType, wfsNameSpace);
         OMAttribute schemaLocation = factory.createOMAttribute("schemaLocation",
                 xmlSchemaInstance,
                 "http://www.opengis.net/wfs http://schemas.opengis.net/wfs/"
                         + VERSION_1_1_0 + "/wfs.xsd");
 
-        OMAttribute version = factory.createOMAttribute("version", null, VERSION_1_1_0);
-        OMAttribute service = factory.createOMAttribute("service", null, "WFS");
+        OMAttribute versionElement = factory.createOMAttribute("version", null, version);
+        OMAttribute serviceElement = factory.createOMAttribute("service", null, "WFS");
 
         root.addAttribute(schemaLocation);
-        root.addAttribute(version);
-        root.addAttribute(service);
+        root.addAttribute(versionElement);
+        root.addAttribute(serviceElement);
 
         return root;
     }
@@ -228,8 +242,60 @@ public class GeoServerRequestBuilder {
         return null;
     }
 
-    public OMElement buildFeaturesInsert(String payload) {
-        return null;
+    public OMElement buildFeaturesInsert(String payload) throws Exception {
+
+        OMElement root = null;
+
+        try {
+            root = buildWFSRootNode("Transaction", VERSION_1_0_0);
+
+            OMElement transaction = factory.createOMElement("Insert", wfsNameSpace);
+            OMNamespace feature = factory.createOMNamespace("http://www.oskari.org", "feature");
+
+            OMElement myPlaces = factory.createOMElement("my_places", feature);
+            myPlaces.addChild(getGeometry());
+
+            JSONArray jsonArray = new JSONObject(payload).getJSONArray("features");
+            for (int i = 0; i < jsonArray.length(); ++i) {
+                for (String property : FEATURES_LIST) {
+                    myPlaces.addChild(getElement(jsonArray.getJSONObject(i).getJSONObject("properties"), property, feature));
+                }
+            }
+
+            transaction.addChild(myPlaces);
+            root.addChild(transaction);
+        } catch (Exception e) {
+            log.error(e, "Failed to create payload - root: ", root);
+            throw new RuntimeException(e.getMessage());
+        }
+        return root;
+    }
+
+    private OMElement getGeometry() throws Exception {
+
+        SimpleFeatureType GEOMETRY_TYPE = DataUtilities.createType("Location", "geom:Point,name:String");
+
+        DefaultFeatureCollection collection = new DefaultFeatureCollection();
+        WKTReader2 wkt = new WKTReader2();
+        collection.add(SimpleFeatureBuilder.build(GEOMETRY_TYPE, new Object[] { wkt.read("POINT (1 2)"),
+                "name1" }, null));
+
+        FeatureTransformer transform = new FeatureTransformer();
+        transform.setEncoding(StandardCharsets.UTF_8);
+        transform.setGmlPrefixing(true);
+
+        // define feature information
+        transform.getFeatureTypeNamespaces().declareDefaultNamespace("feature", "http://www.opengis.net/wfs");
+        transform.addSchemaLocation("schemaLocation", "http://www.opengis.net/wfs http://schemas.opengis.net/wfs/"
+                + VERSION_1_0_0 + "/wfs.xsd");
+        transform.setSrsName("EPSG:3076");
+
+        ByteArrayOutputStream xmlOutput = new ByteArrayOutputStream();
+        transform.transform(collection, xmlOutput);
+
+        InputStream xmlInput = new ByteArrayInputStream(xmlOutput.toByteArray());
+        OMElement root = OMXMLBuilderFactory.createOMBuilder(xmlInput).getDocumentElement();
+        return root;
     }
 
     public OMElement buildFeaturesUpdate(String payload) {
