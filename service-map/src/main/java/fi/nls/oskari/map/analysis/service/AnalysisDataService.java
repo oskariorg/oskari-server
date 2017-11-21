c6ad24236e4262ec69f9cd1818e3f0ab8e9bf6fa
jampukka <janne.heikkila@nls.fi>
2017-11-21 09:12:24 +0200

    revert back

--- service-map/src/main/java/fi/nls/oskari/map/analysis/service/AnalysisDataService.java
+++ service-map/src/main/java/fi/nls/oskari/map/analysis/service/AnalysisDataService.java
@@ -136,6 +136,10 @@ public class AnalysisDataService {
         return analysis;
     }
 
+    public Analysis getAnalysisById(long id) {
+        return analysisService.getAnalysisById(id);
+    }
+
     /**
      * Merge analyses to one new analyse
      *
@@ -167,7 +171,7 @@ public class AnalysisDataService {
 
         try {
             // Insert analysis row - use old for seed
-            analysis = analysisService.getAnalysisById(ids.get(0));
+            analysis = getAnalysisById(ids.get(0));
             // --------------------
             analysis.setAnalyse_json(json.toString());
             analysis.setLayer_id(analysislayer.getId());
