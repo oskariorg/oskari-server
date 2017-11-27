f3706a74ce6b52d09b0ac5290c05b1eae910f733
jampukka <janne.heikkila@nls.fi>
2017-11-27 10:29:58 +0200

    Use property instead of log.isDebugEnabled(), change static fields to final, whitespace fixes

--- service-wfs/src/main/java/fi/nls/oskari/wfs/util/HttpHelper.java
+++ service-wfs/src/main/java/fi/nls/oskari/wfs/util/HttpHelper.java
@@ -18,14 +18,20 @@ public class HttpHelper {
 
     private static final Logger log = LogFactory.getLogger(HttpHelper.class);
 
-    private static int CONNECTION_TIMEOUT_MS = 3000;
-    private static int READ_TIMEOUT_MS = 60000;
+    private static final int CONNECTION_TIMEOUT_MS_DEFAULT = 3000;
+    private static final int READ_TIMEOUT_MS_DEFAULT = 60000;
+    private static final boolean LOG_RESPONSES_DEFAULT = false;
+
+    private static final int CONNECTION_TIMEOUT_MS;
+    private static final int READ_TIMEOUT_MS;
+    private static final boolean LOG_RESPONSES;
 
     static {
-        CONNECTION_TIMEOUT_MS = PropertyUtil.getOptional("oskari.connection.timeout", CONNECTION_TIMEOUT_MS);
-        READ_TIMEOUT_MS = PropertyUtil.getOptional("oskari.read.timeout", READ_TIMEOUT_MS);
+        CONNECTION_TIMEOUT_MS = PropertyUtil.getOptional("oskari.connection.timeout", CONNECTION_TIMEOUT_MS_DEFAULT);
+        READ_TIMEOUT_MS = PropertyUtil.getOptional("oskari.read.timeout", READ_TIMEOUT_MS_DEFAULT);
+        LOG_RESPONSES = PropertyUtil.getOptional("transport.response.debug", LOG_RESPONSES_DEFAULT);
     }
-    
+
     /**
      * Basic HTTP GET method
      * 
@@ -33,29 +39,30 @@ public class HttpHelper {
      * @return response body
      */
     public static String getRequest(String url, String cookies) {
-		HttpRequest request;
-		String response = null;
-		try {
-			request = HttpRequest.get(url)
-					.acceptGzipEncoding().uncompress(true)
-					.trustAllCerts()
-					.trustAllHosts();
+        HttpRequest request;
+        String response = null;
+        try {
+            request = HttpRequest.get(url)
+                    .acceptGzipEncoding().uncompress(true)
+                    .trustAllCerts()
+                    .trustAllHosts();
             if(cookies != null) {
                 request.getConnection().setRequestProperty("Cookie", cookies);
             }
             if(request.ok() || request.code() == 304) {
-				response = request.body();
-                log.debug(response);
+                response = request.body();
+                if (LOG_RESPONSES) {
+                    log.debug(response);
+                }
+            } else {
+                handleHTTPError("GET", url, request.code(), false);
             }
-			else {
-				handleHTTPError("GET", url, request.code(), false);
-			}
-		} catch (HttpRequestException e) {
-			handleHTTPRequestFail(url, e, false);
-		} catch (Exception e) {
-			handleHTTPRequestFail(url, e, false);
-		}
-		return response;
+        } catch (HttpRequestException e) {
+            handleHTTPRequestFail(url, e, false);
+        } catch (Exception e) {
+            handleHTTPRequestFail(url, e, false);
+        }
+        return response;
     }
     /**
      * Basic HTTP GET method
@@ -77,9 +84,9 @@ public class HttpHelper {
             }
             if(request.ok() || request.code() == 304)
                 if(key != null) headerValue = request.header(key);
-            else {
-                handleHTTPError("GET", url, request.code(), false);
-            }
+                else {
+                    handleHTTPError("GET", url, request.code(), false);
+                }
         } catch (HttpRequestException e) {
             handleHTTPRequestFail(url, e, false);
         } catch (Exception e) {
@@ -87,6 +94,7 @@ public class HttpHelper {
         }
         return headerValue;
     }
+
     /**
      * HTTP GET method with optional basic authentication and contentType definition
      * 
@@ -97,11 +105,11 @@ public class HttpHelper {
      * @return response body
      */
     public static BufferedInputStream getRequestStream(String url, String contentType, String username, String password) {
-		HttpRequest request = getRequest(url, contentType, username, password);
-		if(request != null) {
-			return request.buffer();
-		}
-		return null;
+        HttpRequest request = getRequest(url, contentType, username, password);
+        if(request != null) {
+            return request.buffer();
+        }
+        return null;
     }
 
     /**
@@ -114,13 +122,13 @@ public class HttpHelper {
      * @return response body
      */
     public static Reader getRequestReader(String url, String contentType, String username, String password) {
-		HttpRequest request = getRequest(url, contentType, username, password);
-		if(request != null) {
-			return request.bufferedReader();
-		}
-		return null;
+        HttpRequest request = getRequest(url, contentType, username, password);
+        if(request != null) {
+            return request.bufferedReader();
+        }
+        return null;
     }
-    
+
     /**
      * HTTP GET method with optional basic authentication and contentType definition
      * 
@@ -131,40 +139,40 @@ public class HttpHelper {
      * @return response body
      */
     public static HttpRequest getRequest(String url, String contentType, String username, String password) {
-		HttpRequest request;
-		try {
+        HttpRequest request;
+        try {
 
-			HttpRequest.keepAlive(false);
-			if(username != null && !username.equals("") && !username.equals("null")) {
-				request = HttpRequest.get(url)
-						.basic(username, password)
-						.accept(contentType)
-						.connectTimeout(CONNECTION_TIMEOUT_MS)
+            HttpRequest.keepAlive(false);
+            if(username != null && !username.equals("") && !username.equals("null")) {
+                request = HttpRequest.get(url)
+                        .basic(username, password)
+                        .accept(contentType)
+                        .connectTimeout(CONNECTION_TIMEOUT_MS)
                         .readTimeout(READ_TIMEOUT_MS)
-						.acceptGzipEncoding().uncompress(true)
-						.trustAllCerts()
-						.trustAllHosts();
-			} else {
-				request = HttpRequest.get(url)
-						.contentType(contentType)
-						.connectTimeout(CONNECTION_TIMEOUT_MS)
+                        .acceptGzipEncoding().uncompress(true)
+                        .trustAllCerts()
+                        .trustAllHosts();
+            } else {
+                request = HttpRequest.get(url)
+                        .contentType(contentType)
+                        .connectTimeout(CONNECTION_TIMEOUT_MS)
                         .readTimeout(READ_TIMEOUT_MS)
-						.acceptGzipEncoding().uncompress(true)
-						.trustAllCerts()
-						.trustAllHosts();
-			}
-			if(request.ok() || request.code() == 304)
-				return request;
-			else {
-				handleHTTPError("GET", url, request.code(), false);
-			}
-			
-		} catch (HttpRequestException e) {
-			handleHTTPRequestFail(url, e, false);
-		} catch (Exception e) {
-			handleHTTPRequestFail(url, e, false);
-		}
-		return null;
+                        .acceptGzipEncoding().uncompress(true)
+                        .trustAllCerts()
+                        .trustAllHosts();
+            }
+            if(request.ok() || request.code() == 304)
+                return request;
+            else {
+                handleHTTPError("GET", url, request.code(), false);
+            }
+
+        } catch (HttpRequestException e) {
+            handleHTTPRequestFail(url, e, false);
+        } catch (Exception e) {
+            handleHTTPRequestFail(url, e, false);
+        }
+        return null;
     }
     /**
      * HTTP POST method with optional basic authentication and contentType definition
@@ -176,10 +184,10 @@ public class HttpHelper {
      * @return response body
      */
     public static Reader postRequestReader(String url, String contentType, String data, String username,
-                                                   String password) {
+            String password) {
         return postRequestReader(url, contentType, data, username, password, false);
     }
-    
+
     /**
      * HTTP POST method with optional basic authentication and contentType definition
      * 
@@ -191,51 +199,51 @@ public class HttpHelper {
      * @return response body
      */
     public static Reader postRequestReader(String url, String contentType, String data, String username,
-                                                   String password, boolean throwException) {
-		HttpRequest request;
-		Reader response = null;
-		try {
-			
-			HttpRequest.keepAlive(false);
-			if(username != null && !username.equals("") && !username.equals("null")) {
-				request = HttpRequest.post(url)
-						.basic(username, password)
-						.contentType(contentType)
+            String password, boolean throwException) {
+        HttpRequest request;
+        Reader response = null;
+        try {
+
+            HttpRequest.keepAlive(false);
+            if(username != null && !username.equals("") && !username.equals("null")) {
+                request = HttpRequest.post(url)
+                        .basic(username, password)
+                        .contentType(contentType)
                         .connectTimeout(CONNECTION_TIMEOUT_MS)
                         .readTimeout(READ_TIMEOUT_MS)
-						.acceptGzipEncoding().uncompress(true)
-						.trustAllCerts()
-						.trustAllHosts()
-						.send(data);
-			} else {
-				request = HttpRequest.post(url)
-						.contentType(contentType)
+                        .acceptGzipEncoding().uncompress(true)
+                        .trustAllCerts()
+                        .trustAllHosts()
+                        .send(data);
+            } else {
+                request = HttpRequest.post(url)
+                        .contentType(contentType)
                         .connectTimeout(CONNECTION_TIMEOUT_MS)
                         .readTimeout(READ_TIMEOUT_MS)
-						.acceptGzipEncoding().uncompress(true)
-						.trustAllCerts()
-						.trustAllHosts()
-						.send(data);
-			}
-			if(request.ok() || request.code() == 304) {
-			    // default charset is UTF-8
-			    log.debug("request charset:", request.charset());
-			    response = request.bufferedReader();
-			    if (log.isDebugEnabled()) {
-			        response = new DebugLoggingReader(response);
-			    }
-			} else {
-			    handleHTTPError("POST", url, request.code(), throwException);
-			}
-			
-		} catch (HttpRequestException e) {
-			handleHTTPRequestFail(url, e, throwException);
-		} catch (Exception e) {
-			handleHTTPRequestFail(url, e, throwException);
-		}
-		return response;
+                        .acceptGzipEncoding().uncompress(true)
+                        .trustAllCerts()
+                        .trustAllHosts()
+                        .send(data);
+            }
+            if(request.ok() || request.code() == 304) {
+                // default charset is UTF-8
+                log.debug("request charset:", request.charset());
+                response = request.bufferedReader();
+                if (LOG_RESPONSES) {
+                    response = new DebugLoggingReader(response);
+                }
+            } else {
+                handleHTTPError("POST", url, request.code(), throwException);
+            }
+
+        } catch (HttpRequestException e) {
+            handleHTTPRequestFail(url, e, throwException);
+        } catch (Exception e) {
+            handleHTTPRequestFail(url, e, throwException);
+        }
+        return response;
     }
-    
+
     /**
      * Handles HTTP error logging for HTTP request methods
      * 
@@ -266,4 +274,5 @@ public class HttpHelper {
                     e, WFSExceptionHelper.ERROR_COMMON_PROCESS_REQUEST_FAILURE);
         }
     }
+
 }
