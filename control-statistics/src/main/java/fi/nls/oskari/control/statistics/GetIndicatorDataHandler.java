94e107443be8a50a82bfb4bad3f211adba33dac3
jampukka <janne.heikkila@nls.fi>
2017-11-29 13:11:47 +0200

    Remove unused import, remove outdated javadoc

--- control-statistics/src/main/java/fi/nls/oskari/control/statistics/GetIndicatorDataHandler.java
+++ control-statistics/src/main/java/fi/nls/oskari/control/statistics/GetIndicatorDataHandler.java
@@ -18,15 +18,9 @@ import org.json.JSONObject;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
 
 /**
- * This interface gives the data for one indicator to the frontend for showing it on the map and on the table.
- * 
- * - action_route=GetIndicatorData&plugin_id=plugin_id&indicator_id=indicator_id&layer_id=layer_id&selectors=URL_ENCODED_JSON
- * 
- * For example SotkaNET requires selectors for year and gender. This means that selectors parameter content
- * could be for example: selectors=%7B%22gender%22%3A%20%22male%22%2C%20%22year%22%3A%20%222005%22%7D
+ * This ActionHandler retrieves data for an indicator for the frontend
  * 
  * Response is in JSON, and contains the indicator data.
  */
