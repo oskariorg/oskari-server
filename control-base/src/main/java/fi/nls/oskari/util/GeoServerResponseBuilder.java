53c4b00c9a7cbe3d5b4d7c6c93007be388ada3cc
okauppinen <olli.kauppinen@nls.fi>
2017-12-01 11:02:18 +0200

    refactored and added transaction response to responseMap

--- control-base/src/main/java/fi/nls/oskari/util/GeoServerResponseBuilder.java
+++ control-base/src/main/java/fi/nls/oskari/util/GeoServerResponseBuilder.java
@@ -3,18 +3,26 @@ package fi.nls.oskari.util;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.service.ServiceRuntimeException;
+import net.opengis.ows10.ExceptionReportType;
 import net.opengis.wfs.InsertResultsType;
 import net.opengis.wfs.InsertedFeatureType;
 import net.opengis.wfs.TransactionResponseType;
+import net.opengis.wfs.TransactionResultsType;
+import net.opengis.wfs.TransactionSummaryType;
+
 import org.apache.commons.io.IOUtils;
+import org.eclipse.emf.ecore.EObject;
 import org.geotools.GML;
 import org.geotools.GML.Version;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.wfs.WFSConfiguration;
 import org.geotools.xml.Parser;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.opengis.feature.simple.SimpleFeature;
+
+import net.opengis.wfs.WfsFactory;
 
 import java.io.InputStream;
 import java.util.*;
@@ -24,46 +32,60 @@ import static org.geotools.GML.Version.GML3;
 
 public class GeoServerResponseBuilder {
 
-    private static final Logger log = LogFactory.getLogger(GeoServerRequestBuilder.class);
+    private static final Logger log = LogFactory.getLogger(GeoServerResponseBuilder.class);
 
     private static final List<String> LAYERS_GET_LIST = Arrays.asList("category_name", "default", "stroke_width",
             "stroke_color", "fill_color", "uuid", "dot_color", "dot_size", "border_width", "border_color",
             "dot_shape", "stroke_linejoin", "fill_pattern", "stroke_linecap", "stroke_dasharray", "border_linejoin",
             "border_dasharray");
+    private static final String FID_LIST = "fidList";
+    private static final String RESPONSE_UPDATED = "updated";
+    private static final String RESPONSE_INSERTED = "inserted";
+    private static final String RESPONSE_DELETED = "deleted";
 
-    private static WFSConfiguration configuration_v1_1 = new org.geotools.wfs.v1_1.WFSConfiguration();
-    private static WFSConfiguration configuration_v1_0 = new org.geotools.wfs.v1_0.WFSConfiguration();
+    //WFS1.1 & JSON/GeoJSON response
+    public JSONArray buildLayersGet(String response) throws Exception {
+        JSONObject json = new JSONObject(response);
+        return json.getJSONArray("features");
 
-    public JSONObject buildLayersGet(String response) throws Exception {
-        return new JSONObject(parseLayersGet(response, LAYERS_GET_LIST));
+    }
+    //WFS1.1
+    public JSONArray buildLayersInsert(String response) throws Exception {
+        Map <String, Object> responseMap = parseTransactionResponse(response);
+        return (JSONArray) responseMap.get(FID_LIST);
     }
 
-    public JSONObject buildLayersInsert(String response) throws Exception {
-        return new JSONObject(parseTransactionResponse(response,configuration_v1_1));
+    //WFS1.1
+    public int buildLayersUpdate(String response) throws Exception {
+        Map <String, Object> responseMap = parseTransactionResponse(response);
+        return (int) responseMap.get(RESPONSE_UPDATED);
     }
-
-    public JSONObject buildLayersUpdate(String response) throws Exception {
-        return new JSONObject(parseTransactionResponse(response,configuration_v1_1));
+    //WFS1.1
+    public int buildLayersDelete(String response) throws Exception {
+        Map <String, Object> responseMap = parseTransactionResponse(response);
+        return (int) responseMap.get(RESPONSE_DELETED);
     }
-
-    public JSONObject buildLayersDelete(String response) throws Exception {
-        return new JSONObject(parseTransactionResponse(response,configuration_v1_1));
+    //WFS1.0 JSON/GeoJSON response
+    public JSONArray buildFeaturesGet(String response) throws Exception {
+        JSONObject json = new JSONObject(response);
+        return json.getJSONArray("features");
     }
-
-    public JSONObject buildFeaturesGet(String response) throws Exception {
-        return null;
+    //WFS1.1
+    public JSONArray buildFeaturesInsert(String response) throws Exception {
+        Map <String, Object> responseMap = parseTransactionResponse(response);
+        return (JSONArray) responseMap.get(FID_LIST);
     }
+    //WFS1.1
+    public int buildFeaturesUpdate(String response) throws Exception {
+        Map <String, Object> responseMap = parseTransactionResponse(response);
+        return (int) responseMap.get(RESPONSE_UPDATED);
 
-    public JSONObject buildFeaturesInsert(String response) throws Exception {
-        return new JSONObject(parseTransactionResponse(response,configuration_v1_0));
     }
+    //WFS1.1
+    public int buildFeaturesDelete(String response) throws Exception {
+        Map <String, Object> responseMap = parseTransactionResponse(response);
+        return (int) responseMap.get(RESPONSE_DELETED);
 
-    public JSONObject buildFeaturesUpdate(String response) throws Exception {
-        return new JSONObject(parseTransactionResponse(response,configuration_v1_0));
-    }
-
-    public JSONObject buildFeaturesDelete(String response) throws Exception {
-        return new JSONObject(parseTransactionResponse(response,configuration_v1_0));
     }
 
     private static SimpleFeatureCollection getFeatureCollection(InputStream inputStream, Version configuration) {
@@ -74,7 +96,7 @@ public class GeoServerResponseBuilder {
             throw new ServiceRuntimeException("Couldn't parse response to feature collection", ex);
         }
     }
-
+    //WFS1.1 & GML
     public static Map<String, Object> parseLayersGet(String response, List<String> propertyList) throws Exception {
         List featuresList = new ArrayList();
 
@@ -96,29 +118,34 @@ public class GeoServerResponseBuilder {
         return result;
     }
 
-
-    public static Map<String, Object> parseTransactionResponse(String response, WFSConfiguration configuration) throws Exception {
-
-        List fidList = new ArrayList();
-
+    //WFS 1.1.0
+    public static Map<String, Object> parseTransactionResponse(String response) throws Exception {
+        final WFSConfiguration configuration = new org.geotools.wfs.v1_1.WFSConfiguration();
+        JSONArray fidList = new JSONArray();
         InputStream inputStream = IOUtils.toInputStream(response, "UTF-8");
         Parser parser = new Parser(configuration);
         Object parsedResponse = parser.parse(inputStream);
+        Map <String, Object> responseMap = new HashMap<String, Object>();
 
         if (parsedResponse instanceof TransactionResponseType) {
             TransactionResponseType transactionResponse = (TransactionResponseType) parsedResponse;
+            TransactionSummaryType summary = transactionResponse.getTransactionSummary();
+            responseMap.put(RESPONSE_DELETED, summary.getTotalDeleted().intValue());
+            responseMap.put(RESPONSE_INSERTED, summary.getTotalInserted().intValue());
+            responseMap.put(RESPONSE_UPDATED, summary.getTotalUpdated().intValue());
+            //TransactionResultsType result = transactionResponse.getTransactionResults();
             InsertResultsType insertResults = transactionResponse.getInsertResults();
             for (int i = 0; i < insertResults.getFeature().size(); ++i) {
                 String fid = ((InsertedFeatureType) insertResults.getFeature().get(i)).getFeatureId().get(0).toString();
-                Map fidMap = new HashMap();
-                fidMap.put("category_id", fid);
-                fidList.add(fidMap);
+                fidList.put(fid);
             }
-
+            responseMap.put(FID_LIST, fidList);
+        } else if (parsedResponse instanceof ExceptionReportType){
+            ExceptionReportType excReport = (ExceptionReportType) parsedResponse;
+            for (Object e : excReport.getException()){
+                log.warn("e:" + e.toString());
+            }
         }
-
-        Map result = new HashMap();
-        result.put("fids", fidList);
-        return result;
+        return responseMap;
     }
 }
