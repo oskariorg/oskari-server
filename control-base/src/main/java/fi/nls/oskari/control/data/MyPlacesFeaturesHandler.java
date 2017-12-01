88524aa4f8f2462c8ba87b8d97425a2af73fea24
okauppinen <olli.kauppinen@nls.fi>
2017-12-01 10:43:11 +0200

    refactored and added preprocess require logged in user

--- control-base/src/main/java/fi/nls/oskari/control/data/MyPlacesFeaturesHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/data/MyPlacesFeaturesHandler.java
@@ -8,24 +8,79 @@ import fi.nls.oskari.myplaces.MyPlacesService;
 import fi.nls.oskari.myplaces.MyPlacesServiceMybatisImpl;
 import fi.nls.oskari.service.OskariComponentManager;
 import fi.nls.oskari.util.*;
+
+import org.apache.axiom.om.OMElement;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
-@OskariActionRoute("MyPlacesFeature")
+@OskariActionRoute("MyPlacesFeatures")
 public class MyPlacesFeaturesHandler extends RestActionHandler {
 
     private final static Logger log = LogFactory.getLogger(MyPlacesFeaturesHandler.class);
+    private static final String PARAM_FEATURES = "features";
+    private static final String PARAM_LAYER_ID = "layerId";
+    private static final String PARAM_FEATURE_ID = "featureId";
+    private static final String JSKEY_FEATURES = "features";
+    private static final String JSKEY_IDLIST = "idList";
+    private static final String JSKEY_SUCCESS = "success";
+    private static final String JSKEY_UPDATED = "updated";
+    private static final String JSKEY_DELETED = "deleted";
 
-    private MyPlacesService service = new MyPlacesServiceMybatisImpl();
+    private GeoServerRequestBuilder requestBuilder;
+    private GeoServerResponseBuilder responseBuilder;
 
-    enum ModifyOperationType { INSERT, UPDATE, DELETE }
+    @Override
+    public void init() {
+        super.init();
+        requestBuilder =  new GeoServerRequestBuilder();
+        responseBuilder = new GeoServerResponseBuilder();
+    }
+
+    @Override
+    public void preProcess(ActionParameters params) throws ActionException {
+        super.preProcess(params);
+        //only for logged in user
+        params.requireLoggedInUser();
+    }
 
     public void handleGet(ActionParameters params) throws ActionException {
-        //checkCredentials(params);
-        GeoServerRequestBuilder requestBuilder = new GeoServerRequestBuilder();
-        GeoServerResponseBuilder responseBuilder = new GeoServerResponseBuilder();
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
         try {
-            ResponseHelper.writeResponse(params, GeoServerHelper.sendRequest(requestBuilder.buildFeaturesGet("fdsa-fdsa-fdsa-fdsa-fdsa")));//TODO params.getUser().getUuid()))));
+            OMElement request;
+            String layerId = params.getHttpParam(PARAM_LAYER_ID);
+            String featureIds = params.getHttpParam(PARAM_FEATURE_ID);
+            if (featureIds != null){
+                log.debug("Requested GetFeatures by feature ids: ", featureIds);
+                request = requestBuilder.buildFeaturesGetByIds(params.getUser(), featureIds);
+            }else if (layerId != null){
+                log.debug("Requested GetFeatures by layer ids: ", layerId);
+                request = requestBuilder.buildFeaturesGetByLayer(params.getUser(), layerId);
+            }else{
+                log.debug("Requested GetFeatures by uuid: ", params.getUser().getUuid());
+                request = requestBuilder.buildFeaturesGet(params.getUser());
+            }
+            String response = GeoServerHelper.sendRequest(request);
+            JSONArray features = responseBuilder.buildFeaturesGet(response);
+            JSONHelper.putValue(responseJson, JSKEY_FEATURES, features);
+            ResponseHelper.writeResponse(params, responseJson);
+        }
+        catch (Exception e) {
+            throw new ActionException(e.getMessage());
+        }
+    }
+
+    public void handlePost(ActionParameters params) throws ActionException {
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
+        try {
+            String jsonString = params.getHttpParam(PARAM_FEATURES);
+            OMElement request = requestBuilder.buildFeaturesInsert(params.getUser(), jsonString);
+            String response = GeoServerHelper.sendRequest(request);
+            JSONArray idList = responseBuilder.buildFeaturesInsert(response);
+            JSONHelper.putValue(responseJson, JSKEY_IDLIST, idList);
+            ResponseHelper.writeResponse(params, responseJson);
         }
         catch (Exception e) {
             throw new ActionException(e.getMessage());
@@ -33,59 +88,37 @@ public class MyPlacesFeaturesHandler extends RestActionHandler {
     }
 
     public void handlePut(ActionParameters params) throws ActionException {
-        handleModifyRequest(params, ModifyOperationType.INSERT);
-    }
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
 
-    public void handlePost(ActionParameters params) throws ActionException {
-        handleModifyRequest(params, ModifyOperationType.UPDATE);
-    }
-
-    public void handleDelete(ActionParameters params) throws ActionException {
-        handleModifyRequest(params, ModifyOperationType.DELETE);
-    }
-
-    private void handleModifyRequest(ActionParameters params, ModifyOperationType operation) throws ActionException {
-        //checkCredentials(params);
-
-        String jsonString = readPayload(params);
-        /*JSONObject json = null;
-        Integer categoryId = null;
         try {
-            json = new JSONObject(jsonString);
-            categoryId = json.getInt("categoryId");
-        }
-        catch (Exception e) {
-            throw new ActionParamsException("Invalid input, expected JSON");
-        }
-        if (!service.canModifyCategory(params.getUser(), categoryId)) {
-            throw new ActionDeniedException("Not allowed");
-        }*/
-        GeoServerRequestBuilder requestBuilder = new GeoServerRequestBuilder();
-        GeoServerResponseBuilder responseBuilder = new GeoServerResponseBuilder();
-        try {
-            switch (operation) {
-                case INSERT:
-                    ResponseHelper.writeResponse(params, responseBuilder.buildFeaturesInsert(
-                            GeoServerHelper.sendRequest(requestBuilder.buildFeaturesInsert(jsonString))));
-                    break;
-                case UPDATE:
-                    ResponseHelper.writeResponse(params, responseBuilder.buildFeaturesUpdate(
-                            GeoServerHelper.sendRequest(requestBuilder.buildFeaturesUpdate(jsonString))));
-                    break;
-                case DELETE:
-                    ResponseHelper.writeResponse(params, responseBuilder.buildFeaturesDelete(
-                            GeoServerHelper.sendRequest(requestBuilder.buildFeaturesDelete(jsonString))));
-                    break;
-            }
+            String jsonString = params.getHttpParam(PARAM_FEATURES);
+            OMElement request = requestBuilder.buildFeaturesUpdate(params.getUser(), jsonString);
+            String response = GeoServerHelper.sendRequest(request);
+            int updated = responseBuilder.buildFeaturesUpdate(response);
+            JSONHelper.putValue(responseJson, JSKEY_SUCCESS, true); //TODO add checking
+            JSONHelper.putValue(responseJson, JSKEY_UPDATED, updated);
+            ResponseHelper.writeResponse(params, responseJson);
         }
         catch (Exception e) {
             throw new ActionException(e.getMessage());
         }
     }
 
-    private void checkCredentials(ActionParameters params) throws ActionException {
-        if(params.getUser().isGuest()) {
-            throw new ActionDeniedException("Session expired");
+    public void handleDelete(ActionParameters params) throws ActionException {
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
+        try {
+            String jsonString = params.getHttpParam(PARAM_FEATURES);
+            OMElement request = requestBuilder.buildFeaturesDelete(params.getUser(), jsonString);
+            String response = GeoServerHelper.sendRequest(request);
+            int deleted = responseBuilder.buildFeaturesDelete(response);
+            JSONHelper.putValue(responseJson, JSKEY_SUCCESS, true); //TODO add checking
+            JSONHelper.putValue(responseJson, JSKEY_DELETED, deleted);
+            ResponseHelper.writeResponse(params, responseJson);
+        }
+        catch (Exception e) {
+            throw new ActionException(e.getMessage(), e);
         }
     }
 
