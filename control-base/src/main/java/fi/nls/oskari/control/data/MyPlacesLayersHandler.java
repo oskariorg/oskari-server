987120de4b87f2fbb7a91e09b01aae0aa1575a8e
okauppinen <olli.kauppinen@nls.fi>
2017-12-01 10:47:40 +0200

    refactored, added preprosessing, added createDefaultCategory functionality

--- control-base/src/main/java/fi/nls/oskari/control/data/MyPlacesLayersHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/data/MyPlacesLayersHandler.java
@@ -3,93 +3,147 @@ package fi.nls.oskari.control.data;
 
 import fi.nls.oskari.annotation.OskariActionRoute;
 import fi.nls.oskari.control.*;
+import fi.nls.oskari.domain.User;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.myplaces.MyPlacesService;
 import fi.nls.oskari.myplaces.MyPlacesServiceMybatisImpl;
+import fi.nls.oskari.service.OskariComponentManager;
 import fi.nls.oskari.util.*;
+
+import java.io.InputStream;
+
+import org.apache.axiom.om.OMElement;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-@OskariActionRoute("MyPlacesLayer")
+@OskariActionRoute("MyPlacesLayers")
 public class MyPlacesLayersHandler extends RestActionHandler {
 
     private final static Logger log = LogFactory.getLogger(MyPlacesLayersHandler.class);
+    private static final String PARAM_LAYERS = "layers";
+    private static final String JSKEY_MYPLACESLAYERS = "myplaceslayers";
+    private static final String JSKEY_IDLIST = "idList";
+    private static final String JSKEY_SUCCESS = "success";
+    private static final String JSKEY_UPDATED = "updated";
+    private static final String JSKEY_DELETED = "deleted";
+    private static final String DEFAULT_CATEGORY = "default_category.json";
 
-    private MyPlacesService service = new MyPlacesServiceMybatisImpl();
+    private GeoServerRequestBuilder requestBuilder;
+    private GeoServerResponseBuilder responseBuilder;
 
-    public enum ModifyOperationType { INSERT, UPDATE, DELETE }
+    @Override
+    public void init() {
+        super.init();
+        requestBuilder =  new GeoServerRequestBuilder();
+        responseBuilder = new GeoServerResponseBuilder();
+    }
 
+    @Override
+    public void preProcess(ActionParameters params) throws ActionException {
+        super.preProcess(params);
+        //only for logged in user
+        params.requireLoggedInUser();
+    }
+
+    @Override
     public void handleGet(ActionParameters params) throws ActionException {
-        //checkCredentials(params);
-        GeoServerRequestBuilder requestBuilder = new GeoServerRequestBuilder();
-        GeoServerResponseBuilder responseBuilder = new GeoServerResponseBuilder();
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
+
         try {
-            ResponseHelper.writeResponse(params, responseBuilder.buildLayersGet(
-                    GeoServerHelper.sendRequest(requestBuilder.buildLayersGet("fdsa-fdsa-fdsa-fdsa-fdsa"))));//;params.getUser().getUuid()))));
-        }
-        catch (Exception e) {
-            throw new ActionException(e.getMessage());
-        }
-    }
-
-    public void handlePut(ActionParameters params) throws ActionException {
-        handleModifyRequest(params, ModifyOperationType.INSERT);
-    }
-
-    public void handlePost(ActionParameters params) throws ActionException {
-        handleModifyRequest(params, ModifyOperationType.UPDATE);
-    }
-
-    public void handleDelete(ActionParameters params) throws ActionException {
-        handleModifyRequest(params, ModifyOperationType.DELETE);
-    }
-
-    private void handleModifyRequest(ActionParameters params, ModifyOperationType operation) throws ActionException {
-        //checkCredentials(params);
-
-        String jsonString = readPayload(params);
-        /*JSONArray json = null;
-        Integer categoryId = null;
-        try { //TODO: Loop through array and get category_name
-            json = new JSONObject(jsonString).getJSONArray("categories");
-            categoryId = json.getInt("category_name");
-        }
-        catch (Exception e) {
-            throw new ActionParamsException("Invalid input, expected JSON");
-        }
-        if (!service.canModifyCategory(params.getUser(), categoryId)) {
-            throw new ActionDeniedException("Not allowed");
-        }*/
-        GeoServerRequestBuilder requestBuilder = new GeoServerRequestBuilder();
-        GeoServerResponseBuilder responseBuilder = new GeoServerResponseBuilder();
-        try {
-            switch (operation) {
-                case INSERT:
-                    ResponseHelper.writeResponse(params, responseBuilder.buildLayersInsert(
-                            GeoServerHelper.sendRequest(requestBuilder.buildLayersInsert(jsonString))));
-                    break;
-                case UPDATE:
-                    ResponseHelper.writeResponse(params, responseBuilder.buildLayersUpdate(
-                            GeoServerHelper.sendRequest(requestBuilder.buildLayersUpdate(jsonString))));
-                    break;
-                case DELETE:
-                    ResponseHelper.writeResponse(params, responseBuilder.buildLayersDelete(
-                            GeoServerHelper.sendRequest(requestBuilder.buildLayersDelete(jsonString))));
-                    break;
+            OMElement request = requestBuilder.buildLayersGet(params.getUser());
+            String response = GeoServerHelper.sendRequest(request);
+            JSONArray layers = responseBuilder.buildLayersGet(response);
+            // if user have no layers, create default
+            if (layers.length() == 0){
+                createDefaultCategory(params.getUser());
+                response = GeoServerHelper.sendRequest(request);
+                layers = responseBuilder.buildLayersGet(response);
             }
+            JSONHelper.putValue(responseJson, JSKEY_MYPLACESLAYERS, layers);
+            ResponseHelper.writeResponse(params, responseJson);
         }
         catch (Exception e) {
             throw new ActionException(e.getMessage());
         }
     }
 
-    private void checkCredentials(ActionParameters params) throws ActionException {
-        if(params.getUser().isGuest()) {
-            throw new ActionDeniedException("Session expired");
+    @Override
+    public void handlePost(ActionParameters params) throws ActionException {
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
+        try {
+            String jsonString = params.getHttpParam(PARAM_LAYERS);
+            OMElement request = requestBuilder.buildLayersInsert(params.getUser(), jsonString);
+            String response = GeoServerHelper.sendRequest(request);
+            JSONArray idList = responseBuilder.buildLayersInsert(response);
+            JSONHelper.putValue(responseJson, JSKEY_IDLIST, idList);
+            ResponseHelper.writeResponse(params, responseJson);
+        }
+        catch (Exception e) {
+            log.error(e);
+            throw new ActionException(e.getMessage());
+        }
+    }
+    @Override
+    public void handlePut(ActionParameters params) throws ActionException {
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
+        try {
+            String jsonString = params.getHttpParam(PARAM_LAYERS);
+
+            OMElement request = requestBuilder.buildLayersUpdate(params.getUser(), jsonString);
+            String response = GeoServerHelper.sendRequest(request);
+            int updated = responseBuilder.buildLayersUpdate(response);
+            JSONHelper.putValue(responseJson, JSKEY_SUCCESS, true); //TODO add checking
+            JSONHelper.putValue(responseJson, JSKEY_UPDATED, updated);
+            ResponseHelper.writeResponse(params, responseJson);
+        }
+        catch (Exception e) {
+            if(e instanceof JSONException) {
+                throw new ActionException("JSON processing error", e);
+            } else if (e instanceof ActionDeniedException){
+                throw (ActionException) e;
+            }
+            throw new ActionException(e.getMessage(), e);
+        }
+    }
+    @Override
+    public void handleDelete(ActionParameters params) throws ActionException {
+        //params.requireLoggedInUser();
+        JSONObject responseJson = new JSONObject();
+        try {
+            String jsonString = params.getHttpParam(PARAM_LAYERS);
+            OMElement request = requestBuilder.buildLayersDelete(params.getUser(),jsonString);
+            String response = GeoServerHelper.sendRequest(request);
+            int deleted = responseBuilder.buildLayersDelete(response);
+            JSONHelper.putValue(responseJson, JSKEY_SUCCESS, true); //TODO add checking
+            JSONHelper.putValue(responseJson, JSKEY_DELETED, deleted);
+            ResponseHelper.writeResponse(params, responseJson);
+        }
+        catch (Exception e) {
+            throw new ActionException(e.getMessage());
         }
     }
 
+
+    private void createDefaultCategory (User user) throws ActionException{
+        try {
+            InputStream inputStream = getClass().getResourceAsStream(DEFAULT_CATEGORY);
+            String jsonString = IOHelper.readString(inputStream);
+            OMElement request = requestBuilder.buildLayersInsert(user, jsonString);
+            String response = GeoServerHelper.sendRequest(request);
+            JSONArray idList = responseBuilder.buildLayersInsert(response);
+            log.info("Created default category for user: ", user.getUuid(), " with layer id: ", idList.toString());
+        }
+        catch (Exception e) {
+            throw new ActionException(e.getMessage());
+        }
+    }
+
+    //TODO: move to util
     private String readPayload(ActionParameters params) throws ActionException {
         try {
             return IOHelper.readString(params.getRequest().getInputStream());
