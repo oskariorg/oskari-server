92c2f6cece13b9e4cca6b9f33501cdb0f1234f72
okauppinen <olli.kauppinen@nls.fi>
2017-12-11 10:00:21 +0200

    changed to use correct geojson payload

--- control-base/src/test/java/fi/nls/oskari/control/data/MyPlacesFeaturesHandlerTest.java
+++ control-base/src/test/java/fi/nls/oskari/control/data/MyPlacesFeaturesHandlerTest.java
@@ -4,21 +4,25 @@ package fi.nls.oskari.control.data;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.util.GeoServerRequestBuilder;
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import org.apache.axiom.om.OMElement;
 
 public class MyPlacesFeaturesHandlerTest {
 
     @Test
     public void MyPlacesFeaturesHandler() throws Exception {
         try {
-
             InputStream inputStream = getClass().getResourceAsStream("insert_payload.json");
-            String payload = IOUtils.toString(inputStream, "UTF-8");
+            String uuid = "fdsa-fdsa-fdsa-fdsa-fdsa";
+            JSONObject payload = new JSONObject (IOUtils.toString(inputStream, "UTF-8"));
+            JSONArray features  = payload.getJSONArray("features");
             GeoServerRequestBuilder handler = new GeoServerRequestBuilder();
-            handler.buildFeaturesInsert(payload);
+            OMElement request = handler.buildFeaturesInsert(uuid, features);
         }
         catch (Exception e) {
             e.printStackTrace();
