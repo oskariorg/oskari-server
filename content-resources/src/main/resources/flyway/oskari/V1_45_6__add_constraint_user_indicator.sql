d09f14bca9a99764d50db66951fbb561ac9ad214
Sami Mäkinen <sami.makinen@nls.fi>
2017-12-22 16:13:28 +0200

    Add constraint to user for user_indicator

--- content-resources/src/main/resources/flyway/oskari/V1_45_6__add_constraint_user_indicator.sql
+++ content-resources/src/main/resources/flyway/oskari/V1_45_6__add_constraint_user_indicator.sql
@@ -1,1 +1,5 @@
 ALTER TABLE oskari_user_indicator ADD PRIMARY KEY (id);
+
+ALTER TABLE oskari_user_indicator ADD CONSTRAINT oskari_user_indicator_user_fk
+ FOREIGN KEY (user_id) REFERENCES oskari_users (id) SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
