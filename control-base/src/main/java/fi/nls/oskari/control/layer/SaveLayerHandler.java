b3c657fe5d3251f170011caccbc2094dee14974c
jampukka <janne.heikkila@nls.fi>
2018-01-22 16:45:06 +0200

    Remove supportedCRSs from OskariMapLayer

--- control-base/src/main/java/fi/nls/oskari/control/layer/SaveLayerHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/SaveLayerHandler.java
@@ -24,6 +24,7 @@ import fi.nls.oskari.service.ServiceException;
 import fi.nls.oskari.service.capabilities.CapabilitiesCacheService;
 import fi.nls.oskari.service.capabilities.OskariLayerCapabilitiesHelper;
 import fi.nls.oskari.util.*;
+import fi.nls.oskari.wfs.GetGtWFSCapabilities;
 import fi.nls.oskari.wfs.WFSLayerConfigurationService;
 import fi.nls.oskari.wfs.util.WFSParserConfigs;
 import fi.nls.oskari.wmts.WMTSCapabilitiesParser;
@@ -333,8 +334,6 @@ public class SaveLayerHandler extends ActionHandler {
 
         ml.setRealtime(ConversionHelper.getBoolean(params.getHttpParam("realtime"), ml.getRealtime()));
         ml.setRefreshRate(ConversionHelper.getInt(params.getHttpParam("refreshRate"), ml.getRefreshRate()));
-        //Default supported crs for unknown crs layers
-        ml.setSupportedCRSs(new HashSet<String>(Arrays.asList(ml.getSrs_name())));
 
         if(OskariLayer.TYPE_WMS.equals(ml.getType())) {
             return handleWMSSpecific(params, ml);
@@ -531,7 +530,13 @@ public class SaveLayerHandler extends ActionHandler {
             ml.setAttributes(attributes);
         }
         // Get supported projections
-        OskariLayerCapabilitiesHelper.setPropertiesFromCapabilitiesWFS(ml);
+        Map<String, Object> capa = GetGtWFSCapabilities.getGtDataStoreCapabilities(
+                ml.getUrl(), ml.getVersion(), ml.getUsername(), ml.getPassword(), ml.getSrs_name());
+        Set<String> crss = GetGtWFSCapabilities.parseProjections(capa, ml.getName());
+        JSONObject capabilities = new JSONObject();
+        JSONHelper.put(capabilities, "srs", new JSONArray(crss));
+        ml.setCapabilities(capabilities);
+        ml.setCapabilitiesLastUpdated(new Date());
     }
 
 
