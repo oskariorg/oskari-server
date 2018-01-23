5f7aa5a2ef5219ebea334a1ec9ac40ddfc5ed8ca
Sami Mäkinen <sami.makinen@nls.fi>
2018-01-23 15:54:01 +0200

    Add download-basket to personal views as well as default view

--- content-resources/src/main/java/flyway/sample/V1_0_12__add_download_basket_to_views.java
+++ content-resources/src/main/java/flyway/sample/V1_0_12__add_download_basket_to_views.java
@@ -1,48 +1,34 @@
 package flyway.sample;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
-
 import fi.nls.oskari.util.FlywayHelper;
 import fi.nls.oskari.util.PropertyUtil;
-import fi.nls.oskari.map.view.ViewService;
-import fi.nls.oskari.map.view.ViewServiceIbatisImpl;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * Migration is skipped by default!
+ *
+ * Add "flyway.sample.1_0_12.skip=false" in oskari-ext.properties to add
+ * download-basket for all default and user views.
+ *
+ */
 public class V1_0_12__add_download_basket_to_views implements JdbcMigration {
 
-	private static final ViewService VIEW_SERVICE = new ViewServiceIbatisImpl();
-	private static final String DOWNLOAD_BASKET = "download-basket";
+    private static final String BUNDLE_ID = "download-basket";
 
-	public void migrate(Connection connection) throws Exception {
-        if(PropertyUtil.getOptional("flyway.sample.1_0_12.skip", true)) {
+    public void migrate(Connection connection) throws Exception {
+        if (PropertyUtil.getOptional("flyway.sample.1_0_12.skip", true)) {
             return;
         }
-		long viewId = VIEW_SERVICE.getDefaultViewId();
-		makeInsert(viewId,connection);
-	}
 
-	private void makeInsert(long viewId, Connection connection) throws Exception {
-
-		final PreparedStatement statement = connection.prepareStatement("INSERT INTO portti_view_bundle_seq"
-				+ "(view_id, bundle_id, seqno, config, state, startup, bundleinstance) " + "VALUES (" + "?, "
-				+ "(SELECT id FROM portti_bundle WHERE name=?), "
-				+ "(SELECT max(seqno)+1 FROM portti_view_bundle_seq WHERE view_id=?), " + "?, ?, "
-				+ "(SELECT startup FROM portti_bundle WHERE name=?), " + "?)");
-
-		statement.setLong(1, viewId);
-		statement.setString(2, DOWNLOAD_BASKET);
-		statement.setLong(3, viewId);
-		statement.setString(4, "{}");
-		statement.setString(5, "{}");
-		statement.setString(6, DOWNLOAD_BASKET);
-		statement.setString(7, DOWNLOAD_BASKET);
-
-		try {
-			statement.execute();
-		} finally {
-			statement.close();
-		}
-	}
+        final List<Long> views = FlywayHelper.getUserAndDefaultViewIds(connection);
+        for (Long viewId : views) {
+            if (FlywayHelper.viewContainsBundle(connection, BUNDLE_ID, viewId)) {
+                continue;
+            }
+            FlywayHelper.addBundleWithDefaults(connection, viewId, BUNDLE_ID);
+        }
+    }
 }
