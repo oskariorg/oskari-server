36218c8ac2edd83956f489e93d1434bf242d06c4
jampukka <janne.heikkila@nls.fi>
2018-02-15 09:09:13 +0200

    Update logging statements

--- service-print/src/main/java/org/oskari/print/loader/CommandLoadImageFromURL.java
+++ service-print/src/main/java/org/oskari/print/loader/CommandLoadImageFromURL.java
@@ -17,6 +17,7 @@ public class CommandLoadImageFromURL extends CommandLoadImageBase {
 
     private static final Logger LOG = LogFactory.getLogger(CommandLoadImageFromURL.class);
     private static final int RETRY_COUNT = 3;
+    private static final int SLEEP_BETWEEN_RETRIES_MS = 50;
     
     private final String uri;
 
@@ -27,17 +28,20 @@ public class CommandLoadImageFromURL extends CommandLoadImageBase {
 
     @Override
     public BufferedImage run() throws Exception {
-        LOG.info(uri);
+        LOG.info("Loading image from:", uri);
         URL url = new URL(uri);
+        IOException toLog = null;
         for (int i = 0; i < RETRY_COUNT - 1; i++) {
             try {
                 return ImageIO.read(url);
             } catch (IOException e) {
-                LOG.warn(e);
-                // Sleep for a tiny moment between retries
-                Thread.sleep(50);
+                toLog = e;
+                // Sleep for a moment between retries
+                Thread.sleep(SLEEP_BETWEEN_RETRIES_MS);
             }
         }
+        // Only log the last failed attempt
+        LOG.warn(toLog, "Failed to load image from:", uri);
         return ImageIO.read(url);
     }
 
