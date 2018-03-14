e49d72861d0279c2c868c488f99843c6b264939a
jampukka <janne.heikkila@nls.fi>
2018-03-14 21:44:06 +0200

    Remove unused import

--- content-resources/src/main/java/flyway/oskari/V1_45_4__migrate_thematic_maps.java
+++ content-resources/src/main/java/flyway/oskari/V1_45_4__migrate_thematic_maps.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.TreeSet;
 
 import fi.nls.oskari.util.ConversionHelper;
 import fi.nls.oskari.util.PropertyUtil;
