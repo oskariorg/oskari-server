a0b96861b12601285c7c257b4259100d966df530
jampukka <janne.heikkila@nls.fi>
2018-03-29 12:44:50 +0300

    Separate the logic between CreateUserLayerHandler and UserLayerDataService

--- control-userlayer/src/main/java/org/oskari/control/userlayer/CreateUserLayerHandler.java
+++ control-userlayer/src/main/java/org/oskari/control/userlayer/CreateUserLayerHandler.java
@@ -24,11 +24,14 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.referencing.CRS;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.oskari.map.userlayer.input.FeatureCollectionParser;
 import org.oskari.map.userlayer.input.FeatureCollectionParsers;
 import org.oskari.map.userlayer.service.UserLayerDataService;
+import org.oskari.map.userlayer.service.UserLayerDbService;
+import org.oskari.map.userlayer.service.UserLayerDbServiceMybatisImpl;
 
 import fi.mml.map.mapwindow.util.OskariLayerWorker;
 import fi.nls.oskari.annotation.OskariActionRoute;
@@ -37,6 +40,8 @@ import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.control.ActionParamsException;
 import fi.nls.oskari.domain.map.userlayer.UserLayer;
+import fi.nls.oskari.domain.map.userlayer.UserLayerData;
+import fi.nls.oskari.domain.map.userlayer.UserLayerStyle;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.service.ServiceException;
@@ -70,6 +75,10 @@ public class CreateUserLayerHandler extends ActionHandler {
     private static final int MAX_FILES_IN_ZIP = 10;
 
     private static final String PARAM_SOURCE_EPSG_KEY = "sourceEpsg";
+    private static final String KEY_NAME = "layer-name";
+    private static final String KEY_DESC = "layer-desc";
+    private static final String KEY_SOURCE = "layer-source";
+    private static final String KEY_STYLE = "layer-style";
 
     private static final int KB = 1024 * 1024;
     private static final int MB = 1024 * KB;
@@ -77,10 +86,22 @@ public class CreateUserLayerHandler extends ActionHandler {
     // Store files smaller than 128kb in memory instead of writing them to disk
     private static final int MAX_SIZE_MEMORY = 128 * KB;
 
-    private final UserLayerDataService userlayerService = new UserLayerDataService();
     private final DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory(MAX_SIZE_MEMORY, null);
     private final String targetEPSG = PropertyUtil.get(PROPERTY_TARGET_EPSG, "EPSG:4326");
     private final int userlayerMaxFileSize = PropertyUtil.getOptional(PROPERTY_USERLAYER_MAX_FILE_SIZE_MB, 10) * MB;
+
+    private UserLayerDbService userLayerService;
+
+    public void setUserLayerService(UserLayerDbService userLayerService) {
+        this.userLayerService = userLayerService;
+    }
+
+    @Override
+    public void init() {
+        if (userLayerService == null) {
+            userLayerService = new UserLayerDbServiceMybatisImpl();
+        }
+    }
 
     @Override
     public void handleAction(ActionParameters params) throws ActionException {
@@ -107,12 +128,8 @@ public class CreateUserLayerHandler extends ActionHandler {
             fileItems.forEach(FileItem::delete);
         }
 
-        try {
-            UserLayer ulayer = userlayerService.storeUserData(fc, params.getUser(), formParams);
-            writeResponse(params, ulayer);
-        } catch (ServiceException e) {
-            throw new ActionException("unable_to_store_data", e);
-        }
+        UserLayer userLayer = store(fc, params.getUser().getUuid(), formParams);
+        writeResponse(params, userLayer);
     }
 
     private CoordinateReferenceSystem decodeCRS(String epsg) throws ActionParamsException {
@@ -286,8 +303,45 @@ public class CreateUserLayerHandler extends ActionHandler {
         return FeatureCollectionParsers.getByFileExt(ext);
     }
 
+    private UserLayer store(SimpleFeatureCollection fc, String uuid, Map<String, String> formParams)
+            throws ActionException {
+        try {
+            UserLayer userLayer = createUserLayer(fc, uuid, formParams);
+            UserLayerStyle userLayerStyle = createUserLayerStyle(formParams);
+            List<UserLayerData> userLayerDataList = UserLayerDataService.createUserLayerData(fc, uuid);
+            userLayer.setFeatures_count(fc.size());
+            userLayer.setFeatures_skipped(fc.size() - userLayerDataList.size());
+            userLayerService.insertUserLayer(userLayer, userLayerStyle, userLayerDataList);
+            return userLayer;
+        } catch (JSONException e) {
+            throw new ActionException("Failed to encode feature as GeoJSON");
+        } catch (ServiceException e) {
+            throw new ActionException("Failed to store features to database");
+        }
+    }
+
+    private UserLayer createUserLayer(SimpleFeatureCollection fc, String uuid, Map<String, String> formParams) {
+        String name = formParams.get(KEY_NAME);
+        String desc = formParams.get(KEY_DESC);
+        String source = formParams.get(KEY_SOURCE);
+        return UserLayerDataService.createUserLayer(fc, uuid, name, desc, source);
+    }
+
+    private UserLayerStyle createUserLayerStyle(Map<String, String> formParams)
+            throws ActionParamsException {
+        try {
+            JSONObject styleObject = null;
+            if (formParams.containsKey(KEY_STYLE)) {
+                styleObject = JSONHelper.createJSONObject(formParams.get(KEY_STYLE));
+            }
+            return UserLayerDataService.createUserLayerStyle(styleObject);
+        } catch (JSONException e) {
+            throw new ActionParamsException("Invalid style json");
+        }
+    }
+
     private void writeResponse(ActionParameters params, UserLayer ulayer) throws ActionException {
-        JSONObject userLayer = userlayerService.parseUserLayer2JSON(ulayer);
+        JSONObject userLayer = UserLayerDataService.parseUserLayer2JSON(ulayer);
         JSONHelper.putValue(userLayer, "featuresCount", ulayer.getFeatures_count());
         JSONObject permissions = OskariLayerWorker.getAllowedPermissions();
         JSONHelper.putValue(userLayer, "permissions", permissions);
--- service-userlayer/src/main/java/org/oskari/map/userlayer/service/UserLayerDataService.java
+++ service-userlayer/src/main/java/org/oskari/map/userlayer/service/UserLayerDataService.java
@@ -1,6 +1,5 @@
 package org.oskari.map.userlayer.service;
 
-import fi.nls.oskari.domain.User;
 import fi.nls.oskari.domain.map.OskariLayer;
 import fi.nls.oskari.domain.map.userlayer.UserLayer;
 import fi.nls.oskari.domain.map.userlayer.UserLayerData;
@@ -11,8 +10,10 @@ import fi.nls.oskari.map.layer.OskariLayerService;
 import fi.nls.oskari.map.layer.OskariLayerServiceIbatisImpl;
 import fi.nls.oskari.map.layer.formatters.LayerJSONFormatterUSERLAYER;
 import fi.nls.oskari.service.ServiceException;
+import fi.nls.oskari.util.ConversionHelper;
 import fi.nls.oskari.util.JSONHelper;
 import fi.nls.oskari.util.PropertyUtil;
+
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.json.JSONArray;
@@ -20,7 +21,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.PropertyDescriptor;
 import org.oskari.geojson.GeoJSON;
 import org.oskari.geojson.GeoJSONWriter;
@@ -28,108 +28,81 @@ import org.oskari.geojson.GeoJSONWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-
 
 public class UserLayerDataService {
 
-    private Logger log = LogFactory.getLogger(UserLayerDataService.class);
-    private static final UserLayerDbService userLayerService = new UserLayerDbServiceMybatisImpl();
-    private OskariLayerService mapLayerService = new OskariLayerServiceIbatisImpl();
-    private final static LayerJSONFormatterUSERLAYER FORMATTER = new LayerJSONFormatterUSERLAYER();
+    private static final Logger log = LogFactory.getLogger(UserLayerDataService.class);
+    private static final OskariLayerService mapLayerService = new OskariLayerServiceIbatisImpl();
+    private static final LayerJSONFormatterUSERLAYER FORMATTER = new LayerJSONFormatterUSERLAYER();
 
     private static final String USERLAYER_LAYER_PREFIX = "userlayer_";
     private static final String USERLAYER_BASELAYER_ID = "userlayer.baselayer.id";
+
     private static final String USERLAYER_MAXFEATURES_COUNT = "userlayer.maxfeatures.count";
-    private static final String KEY_DESC = "layer-desc";
-    private static final String KEY_NAME = "layer-name";
-    private static final String KEY_SOURCE = "layer-source";
-    private static final String KEY_STYLE = "layer-style";
+    private static final int USERLAYER_MAX_FEATURES_COUNT = PropertyUtil.getOptional(USERLAYER_MAXFEATURES_COUNT, -1);
 
-    static final int USERLAYER_BASE_LAYER_ID = PropertyUtil.getOptional(USERLAYER_BASELAYER_ID, -1);
-    static final int USERLAYER_MAX_FEATURES_COUNT = PropertyUtil.getOptional(USERLAYER_MAXFEATURES_COUNT, -1);
+    private static final int USERLAYER_BASE_LAYER_ID = PropertyUtil.getOptional(USERLAYER_BASELAYER_ID, -1);
 
-    public UserLayer storeUserData(SimpleFeatureCollection fc, User user, Map<String, String> fparams) throws ServiceException {
-        try {
-            //TODO: Style insert
-            log.info("user data store start: ", fparams);
-            final UserLayer userLayer = new UserLayer();
-            final UserLayerStyle style = new UserLayerStyle();
-
-            style.setId(1);  // for default, even if style should be always valued
-            //set style from json
-            if (fparams.containsKey(KEY_STYLE)) {
-                final JSONObject stylejs = JSONHelper
-                        .createJSONObject(fparams.get(KEY_STYLE));
-                style.populateFromJSON(stylejs);
-            }
-            //set userLayer
-            SimpleFeatureType ft = fc.getSchema();
-            userLayer.setLayer_name(ft.getTypeName());
-            userLayer.setLayer_desc("");
-            userLayer.setLayer_source("");
-            userLayer.setFields(parseFields(ft));
-            userLayer.setUuid(user.getUuid());
-            // TODO: Store the bounds in WGS84
-            // ReferencedEnvelope env = fc.getBounds();
-
-            if (fparams.containsKey(KEY_NAME)) {
-                userLayer.setLayer_name(fparams.get(KEY_NAME));
-            }
-            if (fparams.containsKey(KEY_DESC)) {
-                userLayer.setLayer_desc(fparams.get(KEY_DESC));
-            }
-            if (fparams.containsKey(KEY_SOURCE)) {
-                userLayer.setLayer_source(fparams.get(KEY_SOURCE));
-            }
-
-            //get userLayerData list
-            List<UserLayerData> userLayerDataList = getUserLayerData(fc, user, userLayer);
-
-            if (userLayerDataList.isEmpty()){
-                throw new ServiceException ("no_features");
-            }
-            //insert layer, style and data in one transaction
-            int count = userLayerService.insertUserLayer(userLayer, style, userLayerDataList);
-            log.info("stored:",count, "rows from", userLayer.getFeatures_count(), "features and skipped:",userLayer.getFeatures_skipped());
-            return userLayer;
-        } catch (Exception e) {
-            log.error(e, "Unable to store user layer  data");
-            throw new ServiceException ("unable_to_store_data");
-        }
+    public static UserLayer createUserLayer(SimpleFeatureCollection fc,
+            String uuid, String name, String desc, String source) {
+        final SimpleFeatureType ft = fc.getSchema();
+        final UserLayer userLayer = new UserLayer();
+        userLayer.setUuid(uuid);
+        userLayer.setLayer_name(ConversionHelper.getString(name, ft.getTypeName()));
+        userLayer.setLayer_desc(ConversionHelper.getString(desc, ""));
+        userLayer.setLayer_source(ConversionHelper.getString(source, ""));
+        userLayer.setFields(parseFields(ft));
+        // TODO: Store the bounds in WGS84
+        // ... ReferencedEnvelope env = fc.getBounds();
+        return userLayer;
     }
 
-    private List<UserLayerData> getUserLayerData(SimpleFeatureCollection fc, User user, UserLayer userLayer) throws ServiceException{
+    private static String parseFields(SimpleFeatureType schema) {
+        // parse FeatureType schema to JSONArray to keep same order in fields as in the imported file
+        JSONArray jsfields = new JSONArray();
+        try {
+            Collection<PropertyDescriptor> types = schema.getDescriptors();
+            for (PropertyDescriptor type : types) {
+                JSONObject obj = new JSONObject();
+                obj.put("name", type.getName().getLocalPart());
+                obj.put("type", type.getType().getBinding().getSimpleName());
+                jsfields.put(obj);
+            }
+        } catch (Exception ex) {
+            log.error(ex, "Couldn't parse field schema");
+        }
+        return JSONHelper.getStringFromJSON(jsfields, "[]");
+    }
+
+    public static UserLayerStyle createUserLayerStyle(JSONObject styleObject)
+            throws JSONException {
+        final UserLayerStyle style = new UserLayerStyle();
+        style.setId(1);  // for default, even if style should be always valued
+        if (styleObject != null) {
+            style.populateFromJSON(styleObject);
+        }
+        return style;
+    }
+
+    public static List<UserLayerData> createUserLayerData(SimpleFeatureCollection fc, String uuid)
+            throws JSONException {
+        List<UserLayerData> userLayerDataList = new ArrayList<>();
         try (SimpleFeatureIterator it = fc.features()) {
-            final List<UserLayerData> userLayerDataList = new ArrayList<>();
-            final String uuid = user.getUuid();
-
-            int count = 0;
-            int noGeometry = 0;
-
             while (it.hasNext()) {
                 SimpleFeature f = it.next();
                 if (f.getDefaultGeometry() == null) {
-                    noGeometry++;
                     continue;
                 }
-                UserLayerData uld = toUserLayerData(f, uuid);
-                userLayerDataList.add(uld);
-                count++;
-                if (USERLAYER_MAX_FEATURES_COUNT != -1 && count > USERLAYER_MAX_FEATURES_COUNT) {
+                userLayerDataList.add(toUserLayerData(f, uuid));
+                if (USERLAYER_MAX_FEATURES_COUNT != -1 && userLayerDataList.size() == USERLAYER_MAX_FEATURES_COUNT) {
                     break;
                 }
             }
-            userLayer.setFeatures_count(count);
-            userLayer.setFeatures_skipped(noGeometry);
-            return userLayerDataList;
-        } catch (Exception e) {
-            log.error(e, "Failed to parse geojson features to userlayer data list");
-            throw new ServiceException("failed_to_parse_geojson");
         }
+        return userLayerDataList;
     }
 
-    private UserLayerData toUserLayerData(SimpleFeature f, String uuid) throws JSONException {
+    private static UserLayerData toUserLayerData(SimpleFeature f, String uuid) throws JSONException {
         JSONObject geoJSON = new GeoJSONWriter().writeFeature(f);
         String id = geoJSON.optString(GeoJSON.ID);
         JSONObject geometry = geoJSON.getJSONObject(GeoJSON.GEOMETRY);
@@ -145,12 +118,10 @@ public class UserLayerDataService {
         return userLayerData;
     }
 
-
     /**
      * Returns the base WFS-layer for userlayers
-     * @return
      */
-    public OskariLayer getBaseLayer() {
+    public static OskariLayer getBaseLayer() {
         if (USERLAYER_BASE_LAYER_ID == -1) {
             log.error("Userlayer baseId not defined. Please define", USERLAYER_BASELAYER_ID,
                     "property with value pointing to the baselayer in database.");
@@ -165,7 +136,7 @@ public class UserLayerDataService {
      * @param ulayer
      * @return
      */
-    public JSONObject parseUserLayer2JSON(UserLayer ulayer) {
+    public static JSONObject parseUserLayer2JSON(UserLayer ulayer) {
         return parseUserLayer2JSON(ulayer, getBaseLayer());
     }
     /**
@@ -174,7 +145,7 @@ public class UserLayerDataService {
      * @return
      * @throws ServiceException
      */
-    public JSONObject parseUserLayer2JSON(final UserLayer ulayer, final OskariLayer baseLayer) {
+    public static JSONObject parseUserLayer2JSON(final UserLayer ulayer, final OskariLayer baseLayer) {
 
         try {
             final String id = baseLayer.getExternalId();
@@ -199,21 +170,5 @@ public class UserLayerDataService {
             return null;
         }
     }
-    // parse FeatureType schema to JSONArray to keep same order in fields as in the imported file
-    public String parseFields(FeatureType schema) {
 
-        JSONArray jsfields = new JSONArray();
-        try {
-            Collection<PropertyDescriptor> types = schema.getDescriptors();
-            for (PropertyDescriptor type : types) {
-                JSONObject obj = new JSONObject();
-                obj.put("name", type.getName().getLocalPart());
-                obj.put("type", type.getType().getBinding().getSimpleName());
-                jsfields.put(obj);
-            }
-        } catch (Exception ex) {
-            log.error(ex, "Couldn't parse field schema");
-        }
-        return JSONHelper.getStringFromJSON(jsfields, "[]");
-    }
 }
