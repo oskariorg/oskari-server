a9f5c1843dd0f32d19dd867d1d53fba7ce8c45d4
jampukka <janne.heikkila@nls.fi>
2018-04-12 16:50:12 +0300

    is NOT empty

--- control-base/src/main/java/fi/nls/oskari/control/layer/GetMapLayerGroupsHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/GetMapLayerGroupsHandler.java
@@ -143,7 +143,7 @@ public class GetMapLayerGroupsHandler extends ActionHandler {
             }
 
             List<OskariLayerGroupLink> groupLinks = linksByGroupId.get(groupId);
-            if (groupLinks != null && groupLinks.isEmpty()) {
+            if (groupLinks != null && !groupLinks.isEmpty()) {
                 List<Integer> groupsLayerIds = linksByGroupId.get(groupId).stream()
                     .filter(l -> contains(layerIds, l.getLayerId()))
                     .sorted(Comparator.comparingInt(OskariLayerGroupLink::getOrderNumber))
