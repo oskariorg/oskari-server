cecd57512d812fab484abfa81a484b7657ee8a39
Sami Mäkinen <zakarfin@gmail.com>
2018-04-13 10:03:24 +0300

    Removed unnecessary check that JSONHelper does internally

--- service-base/src/main/java/fi/nls/oskari/domain/map/view/View.java
+++ service-base/src/main/java/fi/nls/oskari/domain/map/view/View.java
@@ -277,9 +277,6 @@ public class View implements Serializable {
             return null;
         }
         JSONObject config = mapfull.getConfigJSON();
-        if(config == null) {
-            return null;
-        }
         return JSONHelper.getJSONObject(config, "mapOptions");
     }
 
