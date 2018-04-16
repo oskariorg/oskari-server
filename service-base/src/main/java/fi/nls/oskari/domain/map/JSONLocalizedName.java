0d4d250a83c03fcb62027229cc969ddeded60100
Sami Mäkinen <sami.makinen@nls.fi>
2018-04-16 17:05:32 +0300

    Looping entries instead of keys for performance

--- service-base/src/main/java/fi/nls/oskari/domain/map/JSONLocalizedName.java
+++ service-base/src/main/java/fi/nls/oskari/domain/map/JSONLocalizedName.java
@@ -2,13 +2,6 @@ package fi.nls.oskari.domain.map;
 
 import java.util.Map;
 
-/**
- * Created with IntelliJ IDEA.
- * User: TMIKKOLAINEN
- * Date: 9.9.2013
- * Time: 12:26
- * To change this template use File | Settings | File Templates.
- */
 public class JSONLocalizedName extends JSONLocalized {
     public String getName(final String language) {
         return getLocalizedValue(language, LOCALE_NAME);
@@ -23,8 +16,8 @@ public class JSONLocalizedName extends JSONLocalized {
     }
 
     public void setNames(Map<String, String> names) {
-        for (String lang : names.keySet()) {
-            setName(lang, names.get(lang));
+        for (Map.Entry<String, String> entry : names.entrySet()) {
+            setName(entry.getKey(), entry.getValue());
         }
     }
 }
