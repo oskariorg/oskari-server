7aac0e303fdf51e4842d65d7e0adf0d31c19f4ba
Sami Mäkinen <sami.makinen@nls.fi>
2018-04-16 17:07:36 +0300

    Compatibility fixes between hierarchical and classic layer admins

--- control-base/src/main/java/fi/nls/oskari/control/layer/SaveOrganizationHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/SaveOrganizationHandler.java
@@ -6,11 +6,14 @@ import fi.nls.oskari.domain.map.DataProvider;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.map.layer.DataProviderService;
+import fi.nls.oskari.util.ConversionHelper;
+import fi.nls.oskari.util.JSONHelper;
 import fi.nls.oskari.util.RequestHelper;
 import fi.nls.oskari.util.ResponseHelper;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONObject;
 import org.oskari.service.util.ServiceFactory;
 
 import java.util.Map;
@@ -32,19 +35,28 @@ public class SaveOrganizationHandler extends ActionHandler {
     public void handleAction(ActionParameters params) throws ActionException {
 
         final HttpServletRequest request = params.getRequest();
-        try {
-            final int dataProviderId = params.getHttpParam(PARAM_ID, -1);
-            final DataProvider dataProvider = new DataProvider();
-            dataProvider.setId(dataProviderId);
-            handleLocalizations(dataProvider, PARAM_NAME_PREFIX, request);
-            if (dataProvider.getLocale() == null) {
-                throw new ActionParamsException("Missing names for layer dataprovider group!");
-            }
+        final DataProvider dataProvider = new DataProvider();
+        // hierarchical admin frontend sends separate params
+        dataProvider.setId(params.getHttpParam(PARAM_ID, -1));
+        dataProvider.setNames(RequestHelper.parsePrefixedParamsMap(request, PARAM_NAME_PREFIX));
 
+        if (dataProvider.getLocale() == null) {
+            // the classical layer admin sends JSON payload
+            // locale as payload  { id: "124", locales: {en: "name en", fi: "name fi"}}
+            JSONObject payload = params.getPayLoadJSON();
+            dataProvider.setId(ConversionHelper.getInt(payload.optString("id"), -1));
+            dataProvider.setNames(JSONHelper.getObjectAsMap(payload.optJSONObject("locales")));
+        }
+
+        if (dataProvider.getLocale() == null) {
+            throw new ActionParamsException("Missing names for layer dataprovider!");
+        }
+
+        try {
             // ************** UPDATE ************************
-            if (dataProviderId != -1) {
-                if (!dataProviderService.hasPermissionToUpdate(params.getUser(), dataProviderId)) {
-                    throw new ActionDeniedException("Unauthorized user tried to update layer dataprovider group - id=" + dataProviderId);
+            if (dataProvider.getId() != -1) {
+                if (!dataProviderService.hasPermissionToUpdate(params.getUser(), dataProvider.getId())) {
+                    throw new ActionDeniedException("Unauthorized user tried to update layer dataprovider - id=" + dataProvider.getId());
                 }
                 dataProviderService.update(dataProvider);
                 ResponseHelper.writeResponse(params, dataProvider.getAsJSON());
@@ -55,18 +67,11 @@ public class SaveOrganizationHandler extends ActionHandler {
                 dataProvider.setId(id);
                 ResponseHelper.writeResponse(params, dataProvider.getAsJSON());
             } else {
-                throw new ActionDeniedException("Unauthorized user tried to update layer dataprovider group - id=" + dataProviderId);
+                throw new ActionDeniedException("Unauthorized user tried to update layer dataprovider - id=" + dataProvider.getId());
             }
 
         } catch (Exception e) {
-            throw new ActionException("Couldn't update/insert map layer dataprovider group", e);
-        }
-    }
-
-    private void handleLocalizations(final DataProvider lc, final String nameprefix, final HttpServletRequest request) {
-        final Map<String, String> parsed = RequestHelper.parsePrefixedParamsMap(request, nameprefix);
-        for(Map.Entry<String, String> entry : parsed.entrySet()) {
-            lc.setName(entry.getKey(), entry.getValue());
+            throw new ActionException("Couldn't update/insert map layer dataprovider", e);
         }
     }
 }
