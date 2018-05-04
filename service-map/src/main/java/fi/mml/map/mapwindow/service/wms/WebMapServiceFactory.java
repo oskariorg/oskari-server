42c17c49eeb361310546331733cbf7a6af1c8991
jampukka <janne.heikkila@nls.fi>
2018-05-04 09:32:09 +0300

    Rewrite WebMapServiceFactory.buildWebMapService(OskariLayer layer)

--- service-map/src/main/java/fi/mml/map/mapwindow/service/wms/WebMapServiceFactory.java
+++ service-map/src/main/java/fi/mml/map/mapwindow/service/wms/WebMapServiceFactory.java
@@ -23,55 +23,53 @@ public class WebMapServiceFactory {
     static {
         wmsCache.setExpiration(12L*60L*60L*1000L);
     }
-
-    /**
-     * Builds new WMS interface with correct version
-     * 
-     * @param layerId id of the map layer
-     * @throws ServiceException if something goes wrong when getting capabilities from cache or from service
-     * @throws WebMapServiceParseException if something goes wrong when parsing
-     * @throws LayerNotFoundInCapabilitiesException if layer is not found in capabilities
-     */
-    public static WebMapService buildWebMapService(int layerId)
-            throws ServiceException, WebMapServiceParseException, LayerNotFoundInCapabilitiesException {
+	
+	/**
+	 * Builds new WMS interface with correct version
+	 * 
+	 * @param layerId id of the map layer
+	 * 
+	 * @throws ServiceException if something goes wrong when getting capabilities from cache or from service 
+	 * @throws WebMapServiceParseException if something goes wrong when parsing
+	 * @throws LayerNotFoundInCapabilitiesException if layer is not found in capabilities
+	 */
+	public static WebMapService buildWebMapService(int layerId)
+	        throws ServiceException, WebMapServiceParseException, LayerNotFoundInCapabilitiesException {
         return buildWebMapService(LAYER_SERVICE.find(layerId));
     }
 
     public static WebMapService buildWebMapService(OskariLayer layer)
             throws ServiceException, WebMapServiceParseException, LayerNotFoundInCapabilitiesException {
         final String cacheKey = "wmsCache_" + layer.getId();
+
+        // Check own Cache<WebMapService>
         WebMapService wms = wmsCache.get(cacheKey);
-        // caching since this is called whenever a layer JSON is created!!
-        if (wms == null) {
-            try {
-                final OskariLayerCapabilities caps = CAPABILITIES_SERVICE.getCapabilities(layer);
-                final String data = caps.getData();
-                if (data == null || data.isEmpty()) {
-                    // setup empty capabilities so we don't try to parse again before cache flush
-                    WMSCapabilities emptyCaps = new WMSCapabilities();
-                    wmsCache.put(cacheKey, emptyCaps);
-                    return emptyCaps;
-                }
-                if (isVersion1_3_0(data)) {
-                    wms = new WebMapServiceV1_3_0_Impl("from DataBase", data, layer.getName());
-                } else if (isVersion1_1_1(data)) {
-                    wms = new WebMapServiceV1_1_1_Impl("from DataBase", data, layer.getName());
-                }
-                if(wms != null) {
-                    // cache the parsed value
-                    wmsCache.put(cacheKey, wms);
-                    if (caps.getId() == null) {
-                        CAPABILITIES_SERVICE.save(layer, data);
-                    }
-                }
-            } catch (ServiceException | WebMapServiceParseException ex) {
-                // setup empty capabilities so we don't try to parse again before cache flush
-                wmsCache.put(cacheKey, new WMSCapabilities());
-                throw ex;
-            }
+		if (wms != null) {
+		    return wms;
+		}
+
+		// Get Capabilities XML document from CapabilitiesCacheService
+		OskariLayerCapabilities cc = CAPABILITIES_SERVICE.getCapabilities(layer);
+		String data = cc.getData();
+		
+		try {
+		    wms = createFromXML(layer.getName(), data);
+		} catch (WebMapServiceParseException | LayerNotFoundInCapabilitiesException ex) {
+            // setup empty capabilities so we don't try to parse again before cache flush
+            wmsCache.put(cacheKey, new WMSCapabilities());
+            throw ex;
         }
+
+		if (cc.getId() == null) {
+		    // Capabilities originated from the service and was parseable, save it to DB
+            CAPABILITIES_SERVICE.save(layer, cc.getData());
+        }
+		
+        // Cache successfully parsed WebMapService
+        wmsCache.put(cacheKey, wms);
+        
         return wms;
-    }
+	}
 
     public static WebMapService createFromXML(final String layerName, final String xml)
             throws WebMapServiceParseException, LayerNotFoundInCapabilitiesException {
@@ -92,28 +90,28 @@ public class WebMapServiceFactory {
         wmsCache.flush(true);
     }
 
-    /**
-     * Returns true is data represents a WMS 1.1.1 version
-     * 
-     * @param data
-     * @return
-     */
-    public static boolean isVersion1_1_1(String data) {
+	/**
+	 * Returns true is data represents a WMS 1.1.1 version
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public static boolean isVersion1_1_1(String data) {
         return data != null &&
                 data.contains("WMT_MS_Capabilities") &&
                 data.contains("version=\"1.1.1\"");
-    }
+	}
 
-    /**
-     * Returns true is data represents a WMS 1.3.0 version
-     * 
-     * @param data
-     * @return
-     */
-    public static boolean isVersion1_3_0(String data) {
+	/**
+	 * Returns true is data represents a WMS 1.3.0 version
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public static boolean isVersion1_3_0(String data) {
         return data != null &&
                 data.contains("WMS_Capabilities") &&
                 data.contains("version=\"1.3.0\"");
-    }
+	}
 
 }
