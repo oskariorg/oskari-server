60ee97ff3dd3c429d898f22a5e8531687128cf58
Sami Mäkinen <sami.makinen@nls.fi>
2018-05-07 15:16:28 +0300

    Rename and change config from properties to DB

--- service-statistics-pxweb/src/main/java/fi/nls/oskari/control/statistics/plugins/pxweb/parser/PrefixedRegionsValueProcessor.java
+++ service-statistics-pxweb/src/main/java/fi/nls/oskari/control/statistics/plugins/pxweb/parser/PrefixedRegionsValueProcessor.java
@@ -2,42 +2,29 @@ package fi.nls.oskari.control.statistics.plugins.pxweb.parser;
 
 import fi.nls.oskari.control.statistics.data.RegionValue;
 import fi.nls.oskari.control.statistics.plugins.db.DatasourceLayer;
-import fi.nls.oskari.util.PropertyUtil;
 import org.json.JSONArray;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
-public class TKValueProcessor extends ValueProcessor {
+/**
+ * Allows statistical datasource to have a prefix in region ID. If prefix is defined for layer this
+ * processor returns values for ONLY the regions that have the prefix and removes the prefix from the returned region ID.
+ */
+public class PrefixedRegionsValueProcessor extends ValueProcessor {
 
-    private Map<String, String> regionsetType = new HashMap<>();
-
-    public TKValueProcessor() {
-        String propertiesPrefix = "stats.tk.";
-        PropertyUtil.getPropertyNamesStartingWith(propertiesPrefix).stream()
-                .forEach(propName -> {
-                    String regionType = propName.substring(propertiesPrefix.length());
-                    regionsetType.put(regionType.toLowerCase(), PropertyUtil.get(propName));
-                });
-    }
     public Optional<RegionValue> getRegionValue(JSONArray values, String regionId, int valueIndex, DatasourceLayer layer) {
         Optional<RegionValue> value = super.getRegionValue(values, regionId, valueIndex, layer);
         if(!value.isPresent()) {
             return value;
         }
         // got value, check if regionid has prefix
-        String type = layer.getConfig().optString("regionType");
-        if(type == null) {
-            return value;
-        }
-        String regionIdPrefix = regionsetType.get(type.toLowerCase());
+        String regionIdPrefix = layer.getConfig("statsRegionPrefix");
         if(regionIdPrefix == null) {
             return value;
         }
         RegionValue v = value.get();
         if(!v.getRegion().startsWith(regionIdPrefix)) {
-            // not a regino in this regionset
+            // not a region in this regionset
             return Optional.empty();
         }
         // take out the prefix
