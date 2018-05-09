1ea2272071510e8f187fc491c2092425669a0b66
Sami Mäkinen <zakarfin@gmail.com>
2018-05-09 10:13:43 +0300

    Added comment on ValueProcessor

--- service-statistics-pxweb/src/main/java/fi/nls/oskari/control/statistics/plugins/pxweb/parser/ValueProcessor.java
+++ service-statistics-pxweb/src/main/java/fi/nls/oskari/control/statistics/plugins/pxweb/parser/ValueProcessor.java
@@ -11,6 +11,16 @@ import java.util.Optional;
  * Returns the region ID/value pair if one is found.
  */
 public class ValueProcessor {
+    /**
+     * These parameters might make no sense for the functionality in the long run. For simplicity we could give the
+     * region ID and the value as parameter or for more complex handling we could pass the whole resultset.
+     * TODO: Consider this API subject to breaking changes in the future
+     * @param values The array of all the values returned by the PX-web API
+     * @param regionId Id of the region that we are currently processing
+     * @param valueIndex index for array in values-array for the region
+     * @param layer
+     * @return
+     */
     public Optional<RegionValue> getRegionValue(JSONArray values, String regionId, int valueIndex, DatasourceLayer layer) {
         Double val = values.optDouble(valueIndex);
         if (val.isNaN()) {
