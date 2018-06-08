7efa30f699b1490ff2120c8845a7614bc3b312d9
Sami Mäkinen <zakarfin@gmail.com>
2018-06-08 19:49:38 +0300

    Add comment

--- content-resources/src/main/resources/flyway/oskari/V1_47_7__set_baselayers_as_internal.sql
+++ content-resources/src/main/resources/flyway/oskari/V1_47_7__set_baselayers_as_internal.sql
@@ -1,2 +1,3 @@
--- set internal flag to true on baselayers for user generated content
+-- Set internal flag to true on baselayers for user generated content
+-- This affects existing installs - any new install will automatically setup the layers as internal through the appropriate flyway modules
 UPDATE oskari_maplayer SET internal=true where name in ('oskari:my_places', 'oskari:analysis_data', 'oskari:vuser_layer_data');
