13dcc524e4427fa2da5e4e3939cf2825bbfb599d
jampukka <janne.heikkila@maanmittauslaitos.fi>
2018-06-12 10:29:47 +0300

    Cleanup GetUserLayerStyleHandler

--- control-userlayer/src/main/java/org/oskari/control/userlayer/GetUserLayerStyleHandler.java
+++ control-userlayer/src/main/java/org/oskari/control/userlayer/GetUserLayerStyleHandler.java
@@ -3,7 +3,6 @@ package org.oskari.control.userlayer;
 import org.json.JSONObject;
 
 import fi.nls.oskari.annotation.OskariActionRoute;
-import fi.nls.oskari.control.ActionDeniedException;
 import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
@@ -20,33 +19,22 @@ import org.oskari.map.userlayer.service.UserLayerDbService;
  */
 @OskariActionRoute("GetUserLayerStyle")
 public class GetUserLayerStyleHandler extends ActionHandler {
-    private static final String PARAM_ID = "id";
+
     private UserLayerDbService userLayerDbService;
-    
+
     @Override
     public void init() {
         userLayerDbService = OskariComponentManager.getComponentOfType(UserLayerDbService.class);
     }
-    
+
     public void handleAction(ActionParameters params) throws ActionException {
-        params.requireLoggedInUser();
-
-        final long id = params.getRequiredParamLong(PARAM_ID);
-        //style id == userlayer id
-        final UserLayer userLayer = userLayerDbService.getUserLayerById(id);
-        if(userLayer == null) {
-            throw new ActionParamsException("Userlayer's style id didn't match any userlayer: " + id);
+        UserLayer layer = UserLayerHandlerHelper.getUserLayer(userLayerDbService, params);
+        UserLayerStyle style = userLayerDbService.getUserLayerStyleById(layer.getId());
+        if (style == null) {
+            throw new ActionParamsException("Unable to get style for id" + layer.getId());
         }
-        if(!userLayer.isOwnedBy(params.getUser().getUuid())) {
-            throw new ActionDeniedException("Userlayer's style belongs to another user");
-        }
+        JSONObject response = style.parseUserLayerStyle2JSON();
+        ResponseHelper.writeResponse(params, response);
+    }
 
-        final UserLayerStyle style = userLayerDbService.getUserLayerStyleById(id);
-        if (style != null){
-            JSONObject response = style.parseUserLayerStyle2JSON();
-            ResponseHelper.writeResponse(params, response);
-        }else{
-            throw new ActionParamsException("Unable to get style for id" + id);
-        }        
-    }    
 }
