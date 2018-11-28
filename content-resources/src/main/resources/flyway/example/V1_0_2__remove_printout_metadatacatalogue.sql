5606761336fe37ecf4fa585e4259d639e87f5ebf
Jan Wolski <jan@wolski.fi>
2018-11-28 15:00:16 +0200

    Rescue metadatacatalogue

--- content-resources/src/main/resources/flyway/example/V1_0_2__remove_printout_metadatacatalogue.sql
+++ content-resources/src/main/resources/flyway/example/V1_0_2__remove_printout_metadatacatalogue.sql
@@ -1,3 +1,3 @@
 DELETE FROM portti_view_bundle_seq WHERE EXISTS
-(SELECT id FROM portti_bundle WHERE portti_bundle.id = portti_view_bundle_seq.bundle_id AND name IN ('printout', 'metadatacatalogue'));
+(SELECT id FROM portti_bundle WHERE portti_bundle.id = portti_view_bundle_seq.bundle_id AND name = 'printout');
 
