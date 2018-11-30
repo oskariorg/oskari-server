d30867013af194b8c3447ac8b6157f814761bed6
okauppinen <olli.kauppinen@nls.fi>
2018-11-30 22:45:08 +0200

    add options for ServiceException

--- service-base/src/main/java/fi/nls/oskari/service/ServiceException.java
+++ service-base/src/main/java/fi/nls/oskari/service/ServiceException.java
@@ -1,15 +1,23 @@
 package fi.nls.oskari.service;
 
+import org.json.JSONObject;
+
 public class ServiceException extends Exception {
-    
-	private static final long serialVersionUID = -3303389634376753825L;
+    private JSONObject options;
+    private static final long serialVersionUID = -3303389634376753825L;
 
 	public ServiceException(final String message, final Exception e) {
         super(message, e);
     }
-
     public ServiceException(final String message) {
         super(message);
     }
 
+    public ServiceException (final String message, final JSONObject options) {
+        super(message);
+        this.options = options;
+    }
+    public JSONObject getOptions() {
+        return options;
+    }
 }
