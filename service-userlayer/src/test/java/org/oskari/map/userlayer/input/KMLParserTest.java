c2a011ecdab58859f77e279e2dfa4f63faf03ef4
okauppinen <olli.kauppinen@nls.fi>
2018-12-01 13:21:31 +0200

    update kml parser test

--- service-userlayer/src/test/java/org/oskari/map/userlayer/input/KMLParserTest.java
+++ service-userlayer/src/test/java/org/oskari/map/userlayer/input/KMLParserTest.java
@@ -4,51 +4,114 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.net.URISyntaxException;
-import java.util.List;
 
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.junit.Test;
-import org.opengis.feature.GeometryAttribute;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.oskari.map.userlayer.input.KMLParser;
 
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 
 import fi.nls.oskari.service.ServiceException;
 
 public class KMLParserTest {
 
-    @SuppressWarnings("unchecked")
     @Test
-    public void testParse() throws ServiceException, URISyntaxException {
-        File file = new File(getClass().getResource("samples.kml").toURI());
-
+    public void testParseExtendedData() throws ServiceException, URISyntaxException {
+        File file = new File(getClass().getResource("user_data.kml").toURI());
         KMLParser kml = new KMLParser();
-
         SimpleFeatureCollection fc = kml.parse(file, null, null);
-
+        SimpleFeatureType schema = fc.getSchema();
+        boolean found = false;
+        // test schema
+        assertEquals(5, schema.getAttributeCount());
+        assertEquals(Geometry.class, schema.getDescriptor(KMLParser.KML_GEOM).getType().getBinding());
+        assertEquals(String.class, schema.getDescriptor("Opening hours").getType().getBinding());
+        // test feature count
+        assertEquals(5, fc.size());
+        // loop features
         SimpleFeatureIterator it = fc.features();
-        SimpleFeature doc = it.next();
-        assertEquals("document", doc.getFeatureType().getTypeName());
-        it.close();
-
-        List<SimpleFeature> folders = (List<SimpleFeature>) doc.getAttribute("Feature");
-        assertEquals(6, folders.size());
-
-        SimpleFeature folder = folders.get(0);
-        assertEquals("Placemarks", folder.getAttribute("name"));
-        assertEquals("These are just some of the different kinds of placemarks with\n" +
-                "        which you can mark your favorite places", folder.getAttribute("description"));
-        GeometryAttribute ga = folder.getDefaultGeometryProperty();
-        assertEquals("LookAt", ga.getName().getLocalPart());
-        Point lookAt = (Point) folder.getAttribute("LookAt");
-        Coordinate c = lookAt.getCoordinate();
-        // LookAt is parsed as lon -> y, lat -> x
-        assertEquals(-122.0839597145766, c.y, 1e-6);
-        assertEquals(37.42222904525232, c.x, 1e-6);
-        assertEquals(0.0, c.z, 0.0);
+        while (it.hasNext()){
+            SimpleFeature feature = it.next();
+            assertEquals(5, feature.getAttributeCount());
+            if ("Ruskeasuo".equals(feature.getAttribute(KMLParser.KML_NAME))){
+                found = true;
+                Point p = (Point) feature.getDefaultGeometry();
+                Coordinate c = p.getCoordinate();
+                // point is parsed as lon -> y, lat -> x
+                assertEquals(60.2063017, c.y, 1e-6);
+                assertEquals(24.9021089, c.x, 1e-6);
+                assertEquals(0.0, c.z, 0.0);
+                assertEquals("Floorball arena", feature.getAttribute(KMLParser.KML_DESC));
+                assertEquals("9-20", feature.getAttribute("Opening hours"));
+                // should be empty string instead of null, to get feature which matches schema
+                assertEquals("", feature.getAttribute("Additional info"));
+            } else if ("Route".equals(feature.getAttribute(KMLParser.KML_NAME))) {
+                LineString line = (LineString) feature.getDefaultGeometry();
+                assertEquals(157, line.getCoordinates().length);
+                assertEquals("Route from the Arena center to the Codecorner", feature.getAttribute(KMLParser.KML_DESC));
+            }
+        }
+        assertEquals("Failed to parse feature Ruskeasuo", true, found);
     }
-
+    @Test
+    public void testParseSampleData() throws ServiceException, URISyntaxException {
+        File file = new File(getClass().getResource("samples.kml").toURI());
+        KMLParser kml = new KMLParser();
+        SimpleFeatureCollection fc = kml.parse(file, null, null);
+        boolean foundHidden = false;
+        boolean foundPentagon = false;
+        // test schema
+        SimpleFeatureType schema = fc.getSchema();
+        assertEquals(3, schema.getAttributeCount());
+        // test feature count
+        assertEquals(20, fc.size());
+        // loop features
+        SimpleFeatureIterator it = fc.features();
+        while (it.hasNext()){
+            SimpleFeature feature = it.next();
+            assertEquals(3, feature.getAttributeCount());
+            // hidden placemark with no geometry
+            if (feature.getDefaultGeometry() == null){
+                foundHidden = true;
+                assertEquals("Descriptive HTML", feature.getAttribute(KMLParser.KML_NAME));
+            } else if ("The Pentagon".equals(feature.getAttribute(KMLParser.KML_NAME))) {
+                foundPentagon = true;
+                assertEquals("Shouldn't have kml LookAt attribute", null, feature.getAttribute("LookAt"));
+            }
+        }
+        assertEquals(true, foundHidden);
+        assertEquals("Can't find The Pentagon. Has plane chrashed or just the test failed", true, foundPentagon);
+    }
+    //@Test PARSER DOESN'T SUPPORT TYPED EXTENDED DATA (types are defined in Schema element)
+    public void testParseTypedUserData() throws ServiceException, URISyntaxException {
+        // file is converted from shape
+        File file = new File(getClass().getResource("hki.kml").toURI());
+        KMLParser kml = new KMLParser();
+        SimpleFeatureCollection fc = kml.parse(file, null, null);
+        boolean found = false;
+        // test schema
+        SimpleFeatureType schema = fc.getSchema();
+        assertEquals(5, schema.getAttributeCount());
+        // test feature count
+        assertEquals(2, fc.size());
+        // loop features
+        SimpleFeatureIterator it = fc.features();
+        while (it.hasNext()){
+            SimpleFeature feature = it.next();
+            assertEquals(5, feature.getAttributeCount());
+            if ("Kauniainen".equals(feature.getAttribute("NIMI"))) {
+                found = true;
+                assertEquals("505", feature.getAttribute("KUNTA"));
+                assertEquals(Polygon.class, feature.getDefaultGeometry().getClass());
+            }
+        }
+        assertEquals(true, found);
+    }
 }
