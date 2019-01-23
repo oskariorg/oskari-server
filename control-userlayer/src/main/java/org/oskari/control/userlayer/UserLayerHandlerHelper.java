386b0f276fe3e8331a469cd296e472f433e2be2d
okauppinen <olli.kauppinen@nls.fi>
2019-01-23 19:20:56 +0200

    remove changes made to ServiceException and UserLayerHandlerHelper

--- control-userlayer/src/main/java/org/oskari/control/userlayer/UserLayerHandlerHelper.java
+++ control-userlayer/src/main/java/org/oskari/control/userlayer/UserLayerHandlerHelper.java
@@ -1,10 +1,5 @@
 package org.oskari.control.userlayer;
 
-import java.util.Map;
-import java.util.Set;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.oskari.map.userlayer.service.UserLayerDbService;
 
 import fi.nls.oskari.control.ActionConstants;
@@ -13,7 +8,6 @@ import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.control.ActionParamsException;
 import fi.nls.oskari.domain.map.userlayer.UserLayer;
-import fi.nls.oskari.util.JSONHelper;
 
 public class UserLayerHandlerHelper {
 
@@ -28,43 +22,5 @@ public class UserLayerHandlerHelper {
             throw new ActionDeniedException("UserLayer belongs to another user");
         }
         return userLayer;
-    }
-    public static JSONObject createErrorJSON (String error, String key, Set<String> set) {
-        JSONObject obj = createErrorJSON (error);
-        JSONHelper.put(obj, key, new JSONArray(set));
-        return obj;
-    }
-    public static JSONObject createErrorJSON (String error, String key, String value) {
-        JSONObject obj = JSONHelper.createJSONObject("error", error);
-        JSONHelper.putValue(obj, key, value);
-        return obj;
-    }
-    public static JSONObject createErrorJSON (String error) {
-        return JSONHelper.createJSONObject("error", error);
-    }
-    public static void addSetToErrorJSON (JSONObject obj, String key, Set<String> set) {
-        if (set.isEmpty()){
-            return;
-        }
-        if (obj == null) {
-            obj = new JSONObject();
-        }
-        JSONHelper.put(obj, key, new JSONArray(set));
-    }
-    public static void addMapToErrorJSON (JSONObject obj, String key, Map<String,String> map) {
-        if(map.isEmpty()){
-            return;
-        }
-        if (obj == null) {
-            obj = new JSONObject();
-        }
-        JSONHelper.putValue(obj, key, new JSONObject(map));
-    }
-    public static JSONObject addStringToErrorJSON (JSONObject obj, String key, String str) {
-        if (obj == null) {
-            obj = new JSONObject();
-        }
-        JSONHelper.putValue(obj, key, str);
-        return obj;
     }
 }
--- service-base/src/main/java/fi/nls/oskari/service/ServiceException.java
+++ service-base/src/main/java/fi/nls/oskari/service/ServiceException.java
@@ -1,23 +1,15 @@
 package fi.nls.oskari.service;
 
-import org.json.JSONObject;
-
 public class ServiceException extends Exception {
-    private JSONObject options;
+
     private static final long serialVersionUID = -3303389634376753825L;
 
 	public ServiceException(final String message, final Exception e) {
         super(message, e);
     }
+
     public ServiceException(final String message) {
         super(message);
     }
 
-    public ServiceException (final String message, final JSONObject options) {
-        super(message);
-        this.options = options;
-    }
-    public JSONObject getOptions() {
-        return options;
-    }
 }
