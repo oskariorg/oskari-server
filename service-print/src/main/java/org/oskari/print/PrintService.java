c1bdc5690680af85879ff619e7b0f39e1c92d282
Janne Heikkilä <janne.heikkila@maanmittauslaitos.fi>
2019-04-12 11:13:13 +0300

    Remove filtering zero opacity layers from PrintService, this is already handled by the ActionHandler

--- service-print/src/main/java/org/oskari/print/PrintService.java
+++ service-print/src/main/java/org/oskari/print/PrintService.java
@@ -5,11 +5,8 @@ import fi.nls.oskari.service.ServiceException;
 import fi.nls.oskari.service.capabilities.CapabilitiesCacheService;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.oskari.print.request.PrintLayer;
 import org.oskari.print.request.PrintRequest;
 import org.oskari.print.wmts.WMTSCapabilitiesCache;
 
@@ -30,24 +27,12 @@ public class PrintService {
     }
 
     public BufferedImage getPNG(PrintRequest request) throws ServiceException {
-        request.setLayers(filterLayersWithZeroOpacity(request.getLayers()));
         return PNG.getBufferedImage(request, wmtsCapsCache);
     }
 
     public void getPDF(PrintRequest request, PDDocument doc)
             throws IOException, ServiceException {
-        request.setLayers(filterLayersWithZeroOpacity(request.getLayers()));
         PDF.getPDF(request, doc, wmtsCapsCache);
-    }
-
-    private static List<PrintLayer> filterLayersWithZeroOpacity(List<PrintLayer> layers) {
-        List<PrintLayer> filtered = new ArrayList<>();
-        for (PrintLayer layer : layers) {
-            if (layer.getOpacity() > 0) {
-                filtered.add(layer);
-            }
-        }
-        return filtered;
     }
 
 }
