a48a72c0047b887db6381039cf274d4a3f0c9409
Sami Mäkinen <sami.makinen@nls.fi>
2019-04-30 17:23:02 +0300

    Remove commented out code

--- control-myplaces/src/main/java/fi/nls/oskari/control/myplaces/MyPlacesWFSHelper.java
+++ control-myplaces/src/main/java/fi/nls/oskari/control/myplaces/MyPlacesWFSHelper.java
@@ -73,28 +73,13 @@ public class MyPlacesWFSHelper extends UserLayerService {
         Expression _uuid = ff.property(MYPLACES_ATTR_UUID);
 
         Filter categoryIdEquals = ff.equals(_categoryId, ff.literal(categoryId));
-
         Filter uuidEquals = ff.equals(_uuid, ff.literal(uuid));
-
-/*
-// FIXME: Referencing publisher name requires the layer is oskari:my_places_categories instead of oskari:my_places
-// which brings more attributes that we want AND breaks transport
-// TODO: We might need to check if the use has right to view the layer that is not his/her own in another way
-// Leaving this logic out means that guests won't see the published user content layer
-        Expression _publisherName = ff.property(MYPLACES_ATTR_PUBLISHER_NAME);
-        Filter publisherNameNotNull = ff.not(ff.isNull(_publisherName));
-        Filter publisherNameNotEmpty = ff.notEqual(_publisherName, ff.literal(""));
-        Filter publisherNameNotNullNotEmpty = ff.and(publisherNameNotNull, publisherNameNotEmpty);
-        Filter uuidEqualsOrPublished = ff.or(uuidEquals, publisherNameNotNullNotEmpty);
-*/
-        Filter uuidEqualsOrPublished = uuidEquals;
-
         Filter bboxFilter = ff.bbox(MYPLACES_ATTR_GEOMETRY,
                 bbox.getMinX(), bbox.getMinY(),
                 bbox.getMaxX(), bbox.getMaxY(),
                 CRS.toSRS(bbox.getCoordinateReferenceSystem()));
 
-        return ff.and(Arrays.asList(categoryIdEquals, uuidEqualsOrPublished, bboxFilter));
+        return ff.and(Arrays.asList(categoryIdEquals, uuidEquals, bboxFilter));
     }
 
     public boolean hasViewPermission(String id, User user) {
