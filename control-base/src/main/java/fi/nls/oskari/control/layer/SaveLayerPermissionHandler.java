44e5a1036656780566797a59da71a8042f038856
Sami Mäkinen <sami.makinen@nls.fi>
2019-08-08 17:51:58 +0300

    Migrated SaveLayerPermission from Ibatis to Mybatis

--- control-base/src/main/java/fi/nls/oskari/control/layer/SaveLayerPermissionHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/SaveLayerPermissionHandler.java
@@ -1,15 +1,21 @@
 package fi.nls.oskari.control.layer;
 
 import fi.mml.portti.domain.permissions.Permissions;
-import fi.mml.portti.service.db.permissions.PermissionsService;
-import fi.mml.portti.service.db.permissions.PermissionsServiceIbatisImpl;
 import fi.nls.oskari.annotation.OskariActionRoute;
 import fi.nls.oskari.control.*;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.service.OskariComponentManager;
+import fi.nls.oskari.util.ResponseHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.oskari.permissions.PermissionService;
+import org.oskari.permissions.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @OskariActionRoute("SaveLayerPermission")
 public class SaveLayerPermissionHandler extends RestActionHandler {
@@ -17,11 +23,14 @@ public class SaveLayerPermissionHandler extends RestActionHandler {
     private static String PARAMETER_PERMISSION_DATA = "resource";
 
     private final static Logger log = LogFactory.getLogger(SaveLayerPermissionHandler.class);
-    private final static PermissionsService permissionsService = new PermissionsServiceIbatisImpl();
+    private PermissionService permissionsService;
+
+    public void init() {
+        permissionsService = OskariComponentManager.getComponentOfType(PermissionService.class);
+    }
 
     @Override
     public void handlePost(ActionParameters params) throws ActionException {
-        log.debug("PERMISSION HANDLER LAYER");
         // only accept admins
         params.requireAdminUser();
 
@@ -30,35 +39,42 @@ public class SaveLayerPermissionHandler extends RestActionHandler {
         permissions.getUniqueResourceName().setType(Permissions.RESOURCE_TYPE_MAP_LAYER);
 
         final JSONArray resources = parseJSONArray(params.getHttpParam(PARAMETER_PERMISSION_DATA));
-        final String whoMakesThisModification = params.getUser().getScreenname();
+        final List<String> layerMappings = new ArrayList<>();
 
         try {
             for (int i = 0; i < resources.length(); i++) {
-
                 final JSONObject layerPermission = resources.getJSONObject(i);
+                final String layerMapping = layerPermission.getString("namespace") + "+" + layerPermission.getString("resourceName");
+                final Optional<Resource> dbResource = permissionsService.findResource(ResourceType.maplayer, layerMapping);
+                if (!dbResource.isPresent()) {
+                    throw new ActionParamsException("Resource not found: " + layerMapping);
+                }
+                Resource resource = dbResource.get();
+                // clear previous
+                resource.setPermissions(null);
 
-                permissions.setExternalId(layerPermission.getString("roleId"));
-                permissions.getUniqueResourceName().setNamespace(layerPermission.getString("namespace"));
-                permissions.getUniqueResourceName().setName(layerPermission.getString("resourceName"));
-                
+                final int roleId = Integer.parseInt(layerPermission.getString("roleId"));
                 JSONArray perm = layerPermission.getJSONArray("permissions");
-                
-                for (int j = 0; j < perm.length(); j++)
-            	{
+                for (int j = 0; j < perm.length(); j++) {
                 	JSONObject obj = perm.getJSONObject(j);
-                	if (obj.getBoolean("value"))
-                	{
-                		addPermissions(permissions, obj.getString("key"));
+                	if (!obj.getBoolean("value")) {
+                	    // permission was not granted
+                	    return;
                 	}
-                	else
-                	{
-                		log.warn("Changing permissions (DELETE) by user '" + whoMakesThisModification + "': " + permissions);
-                		deletePermissions(permissions, obj.getString("key"));
-                	}
+                	Permission permission = new Permission();
+                    permission.setRoleId(roleId);
+                    permission.setType(obj.getString("key"));
+                    resource.addPermission(permission);
             	}
+                permissionsService.saveResource(resource);
+                layerMappings.add(resource.getMapping());
             }
+            // TODO: previously didn't respond at all. Should respond with updated data
+            ResponseHelper.writeResponse(params, "success");
         } catch (JSONException e) {
-            e.printStackTrace();
+            throw new ActionParamsException("Invalid input");
+        } finally {
+            log.info("Layer permissions updated by", params.getUser().getScreenname(), "Layers updated:", layerMappings);
         }
     }
 
@@ -70,16 +86,6 @@ public class SaveLayerPermissionHandler extends RestActionHandler {
         } catch (Exception e) {
             throw new ActionParamsException("Unable to parse param JSON:\n" + jsonArray);
         }
-    }
-
-    private void addPermissions(final Permissions permissions, final String permissionType) {
-        permissionsService.insertPermissions(permissions.getUniqueResourceName(),
-                permissions.getExternalId(), permissions.getExternalIdType(), permissionType);
-    }
-
-    private void deletePermissions(final Permissions permissions, final String permissionType) {
-        permissionsService.deletePermissions(permissions.getUniqueResourceName(), permissions.getExternalId(),
-                permissions.getExternalIdType(), permissionType);
     }
 
 }
