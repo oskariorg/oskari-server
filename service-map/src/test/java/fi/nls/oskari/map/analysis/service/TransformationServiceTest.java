8b0dc135b1caf6aab5a09050b0cfbe3b4d3e506b
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-08-12 13:38:27 +0300

    Update WFS-T creation tests

--- service-map/src/test/java/fi/nls/oskari/map/analysis/service/TransformationServiceTest.java
+++ service-map/src/test/java/fi/nls/oskari/map/analysis/service/TransformationServiceTest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
@@ -21,18 +21,56 @@ import fi.nls.oskari.util.IOHelper;
 
 public class TransformationServiceTest {
 
-    private static final String WPS = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><wfs:FeatureCollection xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:feature=\"http://www.opengis.net/gml\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:ows=\"http://www.opengis.net/ows\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\"><feature:boundedBy><feature:Envelope srsDimension=\"2\" srsName=\"http://www.opengis.net/gml/srs/epsg.xml#3067\"><feature:lowerCorner>389273.87 6674100.3344</feature:lowerCorner><feature:upperCorner>389273.87 6674100.3344</feature:upperCorner></feature:Envelope></feature:boundedBy><feature:featureMember><feature:analysis_data feature:id=\"fid--1592edd0_16c66a5ffe5_-7fe0\"><feature:boundedBy><feature:Envelope srsDimension=\"2\" srsName=\"http://www.opengis.net/gml/srs/epsg.xml#3067\"><feature:lowerCorner>389273.87 6674100.3344</feature:lowerCorner><feature:upperCorner>389273.87 6674100.3344</feature:upperCorner></feature:Envelope></feature:boundedBy><feature:geom><feature:MultiPoint srsDimension=\"2\" srsName=\"http://www.opengis.net/gml/srs/epsg.xml#3067\"><feature:pointMember><feature:Point srsDimension=\"2\"><feature:pos>389273.87 6674100.3344</feature:pos></feature:Point></feature:pointMember></feature:MultiPoint></feature:geom></feature:analysis_data></feature:featureMember></wfs:FeatureCollection>";
     private static final String UUID = "test-uuid";
     private static final long ANALYSIS_ID = 1L;
     private static final String GEOMETRY_PROPERTY = "geom";
     private static final String NS_PREFIX = "feature";
-    private static final String EXPECTED_FILE_PATH= "fi/nls/oskari/map/analysis/service/TransformationServiceTest-expected-wfst-result.xml";
-    private static Map<String, String> fieldTypes = null;
+    private static final String TEST_RESOURCE_FOLDER = "fi/nls/oskari/map/analysis/service/";
+    private static final String INPUT_FILE_PATH_BUFFER = TEST_RESOURCE_FOLDER
+            + "TransformationServiceTest-wps-input-buffer.xml";
+    private static final String EXPECTED_FILE_PATH_BUFFER = TEST_RESOURCE_FOLDER
+            + "TransformationServiceTest-expected-wfst-result-buffer.xml";
+    private static final String INPUT_FILE_PATH_DESCRIPTIVE_STATISTICS = TEST_RESOURCE_FOLDER
+            + "TransformationServiceTest-wps-input-descriptive-statistic.xml";
+    private static final String EXPECTED_FILE_PATH_DESCRIPTIVE_STATISTICS = TEST_RESOURCE_FOLDER
+            + "TransformationServiceTest-expected-wfst-result-descriptive-statistic.xml";
+   
+    
     private TransformationService service = new TransformationService();
+    
+    @BeforeClass
+    public static void setup() {
+        XMLUnit.setIgnoreWhitespace(true);
+    }
+    
+    @Test
+    public void testWpsFeatureCollectionToWfstBuffer() throws ServiceException, SAXException, IOException {
 
-    @Before
-    public void setup() {
-        fieldTypes = new HashMap<>();
+        Map<String, String> fieldTypes = new HashMap<>();
+        fieldTypes.put("onntyyppi", "string");
+        fieldTypes.put("lkmpp", "numeric");
+        fieldTypes.put("lkmjk", "numeric");
+        fieldTypes.put("geom", "string");
+        fieldTypes.put("lkmmuukulk", "numeric");
+        fieldTypes.put("lkmhapa", "numeric");
+        fieldTypes.put("lkmlaka", "numeric");
+        fieldTypes.put("lkmmo", "numeric");
+        fieldTypes.put("lkmmp", "numeric");
+        fieldTypes.put("x", "numeric");
+        fieldTypes.put("vvonn", "string");
+        fieldTypes.put("kkonn", "numeric");
+        fieldTypes.put("vakav", "string");
+        fieldTypes.put("y", "numeric");
+        
+        List<String> fields = new ArrayList<String>();
+        testWpsToWfs(INPUT_FILE_PATH_BUFFER, EXPECTED_FILE_PATH_BUFFER, fields, fieldTypes);
+    }
+
+    @Test
+    public void testWpsFeatureCollectionToWfstDescriptiveStatistics()
+            throws ServiceException, SAXException, IOException {
+
+        Map<String, String> fieldTypes = new HashMap<>();
         fieldTypes.put("Mediaani", "numeric");
         fieldTypes.put("Pienin_arvo", "numeric");
         fieldTypes.put("Summa", "numeric");
@@ -40,36 +78,32 @@ public class TransformationServiceTest {
         fieldTypes.put("Kohteiden_lukumäärä", "numeric");
         fieldTypes.put("Suurin_arvo", "numeric");
         fieldTypes.put("Keskiarvo", "numeric");
-    }
-
-    @Test
-    public void testWpsFeatureCollectionToWfst() throws ServiceException, SAXException, IOException {
+        
         List<String> fields = new ArrayList<String>();
-        String result = service.wpsFeatureCollectionToWfst(WPS, UUID, ANALYSIS_ID, fields, fieldTypes,
-                GEOMETRY_PROPERTY, NS_PREFIX);
-    
-       assertXmlIsValid(result);
+        testWpsToWfs(INPUT_FILE_PATH_DESCRIPTIVE_STATISTICS, EXPECTED_FILE_PATH_DESCRIPTIVE_STATISTICS, fields, fieldTypes);
     }
 
-    /**
-     * Initial idea to validate result against GeoServer wfs.xsd was dumped since schema
-     * contained multiple imports so test needs to be runned with proxy params to
-     * avoid timeouts. WFST also contains oskari.org feature namespace which did not
-     * pass schema validation. Because of mentioned reasons, validation is done
-     * without schema by comparing result contents to loaded xml test resource.
-     * @throws IOException 
-     * @throws SAXException 
-     */
-    private void assertXmlIsValid(String xml) throws IOException, SAXException {
-        XMLUnit.setIgnoreWhitespace(true);
-        String expectedResult = readResource(EXPECTED_FILE_PATH);
-        Diff xmlDiff = new Diff(expectedResult, xml);
-        assertTrue(String.format("Result xml does not equal expected: %s.",xmlDiff.toString()),xmlDiff.similar()); 
+    private void testWpsToWfs(String inputFilePath, String expectedFilePath, List<String> fields, Map<String, String> fieldTypes)
+            throws ServiceException, IOException, SAXException {
+        
+        String wpsFeatures = readResource(inputFilePath);
+        String expected = readResource(expectedFilePath);
+
+        String result = service.wpsFeatureCollectionToWfst(wpsFeatures, UUID, ANALYSIS_ID, fields, fieldTypes,
+                GEOMETRY_PROPERTY, NS_PREFIX);
+
+        assertXmlIsValid(expected, result);
     }
 
     private String readResource(String p) throws IOException {
         try (InputStream in = getClass().getClassLoader().getResourceAsStream(p)) {
             return new String(IOHelper.readBytes(in), StandardCharsets.UTF_8);
         }
+    }
+    
+    private void assertXmlIsValid(String expected, String actual) throws IOException, SAXException {
+        
+        Diff xmlDiff = new Diff(expected, actual);
+        assertTrue(String.format("Result xml does not equal expected: %s.", xmlDiff.toString()), xmlDiff.similar());
     }
 }
