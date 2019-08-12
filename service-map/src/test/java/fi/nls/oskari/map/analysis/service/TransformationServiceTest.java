78bd2a8712a1ab35a1c5ce68398e98fb50015010
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-08-12 14:10:01 +0300

    Add merge properties to features test

--- service-map/src/test/java/fi/nls/oskari/map/analysis/service/TransformationServiceTest.java
+++ service-map/src/test/java/fi/nls/oskari/map/analysis/service/TransformationServiceTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,7 +35,21 @@ public class TransformationServiceTest {
             + "TransformationServiceTest-wps-input-descriptive-statistic.xml";
     private static final String EXPECTED_FILE_PATH_DESCRIPTIVE_STATISTICS = TEST_RESOURCE_FOLDER
             + "TransformationServiceTest-expected-wfst-result-descriptive-statistic.xml";
+    
+    private static final String INPUT_FILE_PATH_MERGE_PROPERTIES_TO_FEATURES = TEST_RESOURCE_FOLDER
+            + "TransformationServiceTest-merge-properties-to-features-input-featureset.xml";
+    private static final String EXPECTED_FILE_PATH_MERGE_PROPERTIES_TO_FEATURES = TEST_RESOURCE_FOLDER
+            + "TransformationServiceTest-merge-properties-to-features-expected.xml";
+    
+    private static final String MERGE_PROPERTIES_TO_FEATURES_AGGREGATE_RESULTS = 
+            "{\"lkmhapa\":{\"Pienin arvo\":0,\"Suurin arvo\":1,\"Keskiarvo\":0.625,\"Mediaani\":1,\"Keskihajonta\":0.4841229182759271,\"Summa\":5,\"Kohteiden lukumäärä\":8},\"lkmlaka\":{\"Pienin arvo\":0,\"Suurin arvo\":2,\"Keskiarvo\":0.375,\"Mediaani\":0,\"Keskihajonta\":0.6959705453537527,\"Summa\":3,\"Kohteiden lukumäärä\":8},\"lkmmo\":{\"Pienin arvo\":0,\"Suurin arvo\":1,\"Keskiarvo\":0.25,\"Mediaani\":0,\"Keskihajonta\":0.4330127018922193,\"Summa\":2,\"Kohteiden lukumäärä\":8},\"lkmmp\":{\"Pienin arvo\":0,\"Suurin arvo\":1,\"Keskiarvo\":0.25,\"Mediaani\":0,\"Keskihajonta\":0.43301270189221935,\"Summa\":2,\"Kohteiden lukumäärä\":8},\"x\":{\"Pienin arvo\":273615.5613,\"Suurin arvo\":284578.2304,\"Keskiarvo\":276509.3736875,\"Mediaani\":275350.70005,\"Keskihajonta\":3385.6776776142774,\"Summa\":2212074.9895,\"Kohteiden lukumäärä\":8},\"kkonn\":{\"Pienin arvo\":1,\"Suurin arvo\":10,\"Keskiarvo\":6,\"Mediaani\":6,\"Keskihajonta\":2.7386127875258306,\"Summa\":48,\"Kohteiden lukumäärä\":8},\"y\":{\"Pienin arvo\":6638539.3841,\"Suurin arvo\":6644676.5617,\"Keskiarvo\":6640326.4626625,\"Mediaani\":6639571.472,\"Keskihajonta\":2022.9991372563804,\"Summa\":5.31226117013E7,\"Kohteiden lukumäärä\":8},\"lkmpp\":{\"Pienin arvo\":0,\"Suurin arvo\":1,\"Keskiarvo\":0.25,\"Mediaani\":0,\"Keskihajonta\":0.4330127018922193,\"Summa\":2,\"Kohteiden lukumäärä\":8},\"lkmjk\":{\"Pienin arvo\":0,\"Suurin arvo\":1,\"Keskiarvo\":0.125,\"Mediaani\":0,\"Keskihajonta\":0.33071891388307384,\"Summa\":1,\"Kohteiden lukumäärä\":8},\"lkmmuukulk\":{\"Pienin arvo\":0,\"Suurin arvo\":0,\"Keskiarvo\":0,\"Mediaani\":0,\"Keskihajonta\":0,\"Summa\":0,\"Kohteiden lukumäärä\":8}}";
+    
+    private static final List<String> MERGE_PROPERTIES_TO_FEATURES_ROW_ORDER  = 
+            Arrays.asList("kkonn", "lkmhapa", "lkmjk", "lkmlaka", "lkmmo", "lkmmp", "lkmmuukulk", "lkmpp", "x", "y");
    
+    private static final List<String> MERGE_PROPERTIES_TO_FEATURES_COLUMN_ORDER  = 
+            Arrays.asList("Kohteiden lukumäärä", "Summa", "Pienin arvo", "Suurin arvo", "Keskiarvo", "Keskihajonta", "Mediaani");
+    
     
     private TransformationService service = new TransformationService();
     
@@ -81,6 +96,19 @@ public class TransformationServiceTest {
         
         List<String> fields = new ArrayList<String>();
         testWpsToWfs(INPUT_FILE_PATH_DESCRIPTIVE_STATISTICS, EXPECTED_FILE_PATH_DESCRIPTIVE_STATISTICS, fields, fieldTypes);
+    }
+    
+    @Test
+    public void testMergePropertiesToFeatures() throws ServiceException, IOException, SAXException {
+        
+        String featureSet = readResource(INPUT_FILE_PATH_MERGE_PROPERTIES_TO_FEATURES);
+       
+        String result = service.mergePropertiesToFeatures(featureSet, MERGE_PROPERTIES_TO_FEATURES_AGGREGATE_RESULTS, 
+                MERGE_PROPERTIES_TO_FEATURES_ROW_ORDER, MERGE_PROPERTIES_TO_FEATURES_COLUMN_ORDER);
+        
+        String expected = readResource(EXPECTED_FILE_PATH_MERGE_PROPERTIES_TO_FEATURES);
+        
+        assertXmlIsValid(expected, result);
     }
 
     private void testWpsToWfs(String inputFilePath, String expectedFilePath, List<String> fields, Map<String, String> fieldTypes)
