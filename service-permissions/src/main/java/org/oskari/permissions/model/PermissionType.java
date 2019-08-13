826a28d246e99c78c6e65e19ce173f2ec700db74
Sami Mäkinen <sami.makinen@nls.fi>
2019-08-13 18:34:00 +0300

    Add metadata if the permission is layer specific

--- service-permissions/src/main/java/org/oskari/permissions/model/PermissionType.java
+++ service-permissions/src/main/java/org/oskari/permissions/model/PermissionType.java
@@ -1,24 +1,38 @@
 package org.oskari.permissions.model;
 
 public enum PermissionType {
-    PUBLISH("publish"),
-    VIEW_LAYER,
-    EDIT_LAYER("edit"),
+    PUBLISH("publish", true),
+    VIEW_LAYER(true),
+    // false because UI breaks...
+    EDIT_LAYER("edit", false),
+    // false because UI breaks...
     EDIT_LAYER_CONTENT,
-    VIEW_PUBLISHED,
-    DOWNLOAD("download"),
+    VIEW_PUBLISHED(true),
+    DOWNLOAD("download", true),
     EXECUTE,
     ADD_MAPLAYER;
 
+    // name of the permission if it's different than the name in layer JSON
     private String jsonKey;
+    // true to list this permission type on admin UI per layer
+    private boolean layerSpecific = false;
 
     PermissionType() {
         jsonKey = this.name();
     }
-    PermissionType(String jsonName) {
+    PermissionType(boolean layerSpecific) {
+        this();
+        this.layerSpecific  = layerSpecific;
+    }
+    PermissionType(String jsonName, boolean layerSpecific) {
+        this(layerSpecific);
         jsonKey = jsonName;
     }
+
     public String getJsonKey() {
         return jsonKey;
+    }
+    public boolean isLayerSpecific() {
+        return layerSpecific;
     }
 }
