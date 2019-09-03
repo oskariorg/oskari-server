fdcdc1f241302b03e4aa8249466d1f44eb6dc976
Sami Mäkinen <sami.makinen@nls.fi>
2019-09-03 16:55:38 +0300

    Remove unused import

--- servlet-map/src/main/java/fi/nls/oskari/spring/security/database/OskariDatabaseSecurityConfig.java
+++ servlet-map/src/main/java/fi/nls/oskari/spring/security/database/OskariDatabaseSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Database based authentication on Oskari
