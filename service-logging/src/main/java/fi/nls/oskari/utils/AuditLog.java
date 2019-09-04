22977bc849dde24c9b0917bf21b7337ee30334b3
Sami Mäkinen <zakarfin@gmail.com>
2019-09-04 21:46:51 +0300

    AuditLogging errors

--- service-logging/src/main/java/fi/nls/oskari/utils/AuditLog.java
+++ service-logging/src/main/java/fi/nls/oskari/utils/AuditLog.java
@@ -1,5 +1,7 @@
 package fi.nls.oskari.utils;
 
+import fi.nls.oskari.domain.User;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -17,9 +19,27 @@ public class AuditLog {
         ip = remote;
         this.email = email;
     }
+    public static AuditLog guest(String remote) {
+        return user(remote, "");
+    }
+    public static AuditLog user(String remote, User user) {
+        String email = null;
+        if (!user.isGuest()) {
+            email = user.getEmail();
+        }
+        return user(remote, email);
+    }
 
     public static AuditLog user(String remote, String email) {
         return new AuditLog(remote, email);
+    }
+    public AuditLog withParams(Map<String, String[]> params) {
+        if (params == null) {
+            return this;
+        }
+        params.entrySet().stream()
+                .forEach(p -> withParam(p.getKey(), p.getValue()));
+        return this;
     }
 
     public AuditLog withParam(String key, Object value) {
@@ -33,6 +53,18 @@ public class AuditLog {
     /*
      * Actual logging. Probably write out JSON so it can be parsed easily
      */
+    public void wasDenied(String msg) {
+        LOGGER.warn(email, ip, "unauthorized", msg, params);
+    }
+
+    public void errored(String msg) {
+        LOGGER.warn(email, ip, "unsuccessful", msg, params);
+    }
+
+    public void usedInvalidParams(String msg) {
+        LOGGER.warn(email, ip, "invalid params", msg, params);
+    }
+
     public void added(String msg) {
         LOGGER.info(email, ip, "added", msg, params);
     }
