64ba81f6f20342eb9ae110c63585fca1b59f0521
Sami Mäkinen <zakarfin@gmail.com>
2019-09-04 16:41:12 +0300

    New idea for audit API

--- service-logging/src/main/java/fi/nls/oskari/utils/AuditLog.java
+++ service-logging/src/main/java/fi/nls/oskari/utils/AuditLog.java
@@ -1,18 +1,47 @@
 package fi.nls.oskari.utils;
 
-public class AuditLog extends Log4JLogger {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-    private static final AuditLog instance = new AuditLog();
+// AuditLog.user("127.0.0.1", "me@my.com")
+//  .withParam("id", 123)
+//  .deleted("My places")
+public class AuditLog {
 
-    private AuditLog() {
-        super("AUDIT");
+    private static final Log4JLogger LOGGER = new Log4JLogger("AUDIT");
+    private final String ip;
+    private final String email;
+    private Map<String, Object> params;
+
+    private AuditLog(String remote, String email) {
+        ip = remote;
+        this.email = email;
     }
 
-    public static AuditLog get() {
-        return instance;
+    public static AuditLog user(String remote, String email) {
+        return new AuditLog(remote, email);
     }
 
-    public String addUserInfo(String remote, String email) {
-        return null;
+    public AuditLog withParam(String key, Object value) {
+        if (params == null) {
+            params = new LinkedHashMap();
+        }
+        params.put(key, value);
+        return this;
+    }
+
+    /*
+     * Actual logging. Probably write out JSON so it can be parsed easily
+     */
+    public void added(String msg) {
+        LOGGER.info(email, ip, "added", msg, params);
+    }
+
+    public void updated(String msg) {
+        LOGGER.info(email, ip, "updated", msg, params);
+    }
+
+    public void deleted(String msg) {
+        LOGGER.info(email, ip, "deleted", msg, params);
     }
 }
