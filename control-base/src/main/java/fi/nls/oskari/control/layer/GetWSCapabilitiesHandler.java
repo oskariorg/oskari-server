b597e3692f37ef53b2706bff0cbebf02e242756e
okauppinen <olli.kauppinen@nls.fi>
2019-10-11 00:12:32 +0300

    move old wfs3 capabilities handling to getwscapabilitieshandler

--- control-base/src/main/java/fi/nls/oskari/control/layer/GetWSCapabilitiesHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/GetWSCapabilitiesHandler.java
@@ -1,8 +1,12 @@
 package fi.nls.oskari.control.layer;
 
 
+import fi.mml.map.mapwindow.util.OskariLayerWorker;
+import fi.nls.oskari.domain.map.wfs.WFSLayerConfiguration;
+import fi.nls.oskari.map.layer.formatters.LayerJSONFormatterWFS;
+import fi.nls.oskari.service.capabilities.OskariLayerCapabilitiesHelper;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.oskari.service.wfs3.WFS3Capabilities;
 
 import fi.nls.oskari.annotation.OskariActionRoute;
 import fi.nls.oskari.control.*;
@@ -19,6 +23,16 @@ import fi.nls.oskari.wfs.GetGtWFSCapabilities;
 import fi.nls.oskari.wms.GetGtWMSCapabilities;
 import fi.nls.oskari.wmts.WMTSCapabilitiesParser;
 import fi.nls.oskari.wmts.domain.WMTSCapabilities;
+import org.oskari.service.wfs3.WFS3Service;
+import org.oskari.service.wfs3.model.WFS3CollectionInfo;
+import org.oskari.service.wfs3.model.WFS3Exception;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static fi.nls.oskari.service.capabilities.CapabilitiesConstants.KEY_LAYERS;
 
 /**
  * Get capabilites for layer and returns JSON formatted as Oskari layers
@@ -69,7 +83,7 @@ public class GetWSCapabilitiesHandler extends ActionHandler {
         ResponseHelper.writeResponse(params, capabilities);
     }
 
-    protected JSONObject getCapabilities(String url, String type, String version,
+    private JSONObject getCapabilities(String url, String type, String version,
             String user, String pw, String currentCrs) throws ActionException {
         try {
             switch (type) {
@@ -77,7 +91,7 @@ public class GetWSCapabilitiesHandler extends ActionHandler {
                 return GetGtWMSCapabilities.getWMSCapabilities(capabilitiesService, url, user, pw, version, currentCrs);
             case OskariLayer.TYPE_WFS:
                 if (VERSION_WFS3.equals(version)) {
-                    return WFS3Capabilities.getWFSCapabilities(url, user, pw, currentCrs);
+                    return getWFS3Capabilities(url, user, pw, currentCrs);
                 } else {
                     return GetGtWFSCapabilities.getWFSCapabilities(url, version, user, pw, currentCrs);
                 }
@@ -98,9 +112,61 @@ public class GetWSCapabilitiesHandler extends ActionHandler {
             throw new ActionException("Capabilities parsing failed: " + e.getMessage(), e);
         }
     }
+    public static JSONObject getWFS3Capabilities(String url, String user, String pw, String currentCrs) throws WFS3Exception, IOException {
+        WFS3Service service = WFS3Service.fromURL(url, user, pw);
+        List<JSONObject> layers = service.getCollections().stream()
+                .map(collectionInfo -> toOskariLayer(url, service, collectionInfo, null))
+                .map(layer -> wfsLayerToJSON(layer, currentCrs, user, pw))
+                .collect(Collectors.toList());
+        return JSONHelper.createJSONObject(KEY_LAYERS, new JSONArray(layers));
+    }
+    private static OskariLayer toOskariLayer(String url, WFS3Service service, WFS3CollectionInfo collection, Set<String> systemCRSs) {
+        OskariLayer layer = new OskariLayer();
+        layer.setType(OskariLayer.TYPE_WFS);
+        layer.setVersion(VERSION_WFS3);
+        layer.setUrl(url);
+        layer.setName(collection.getId());
+        layer.setMaxScale(1d);
+        layer.setMinScale(1500000d);
+        String title = collection.getTitle() != null ? collection.getTitle() : collection.getId();
+        for (String lang : PropertyUtil.getSupportedLanguages()) {
+            layer.setName(lang, title);
+        }
+        OskariLayerCapabilitiesHelper.setPropertiesFromCapabilitiesWFS(service, layer, systemCRSs);
 
-
-
-
-
+        return layer;
+    }
+    private static JSONObject wfsLayerToJSON(OskariLayer layer, String crs, String user, String pw) {
+        LayerJSONFormatterWFS formatter = new LayerJSONFormatterWFS();
+        String lang = PropertyUtil.getDefaultLanguage();
+        JSONObject obj = formatter.getJSON(layer, lang, false, crs);
+        OskariLayerWorker.modifyCommonFieldsForEditing(obj, layer);
+        WFSLayerConfiguration lc = layerToWfs30LayerConfiguration(layer, crs, user, pw);
+        JSONObject admin = JSONHelper.getJSONObject(obj, "admin");
+        JSONHelper.putValue(admin, "passthrough", JSONHelper.createJSONObject(lc.getAsJSON()));
+        // NOTE! Important to remove id since this is at template
+        obj.remove("id");
+        // Admin layer tools needs for listing layers
+        JSONHelper.putValue(obj, "title", layer.getName());
+        return obj;
+    }
+    private static WFSLayerConfiguration layerToWfs30LayerConfiguration (OskariLayer layer, String crs, String user, String pw) {
+        final WFSLayerConfiguration lc = new WFSLayerConfiguration();
+        // Use defaults for now, modify if needed
+        String name = layer.getName();
+        lc.setDefaults();
+        lc.setURL(layer.getUrl());
+        lc.setUsername(user);
+        lc.setPassword(pw);
+        lc.setLayerName(name); // or WFS3CollectionInfo getTitle()
+        lc.setLayerId("layer_" + name);
+        lc.setSRSName(crs);
+        lc.setGMLGeometryProperty("geometry");
+        lc.setWFSVersion(VERSION_WFS3);
+        lc.setFeatureElement(name);
+        lc.setFeatureNamespace("");
+        lc.setFeatureNamespaceURI("");
+        lc.setJobType("default");
+        return lc;
+    }
 }
