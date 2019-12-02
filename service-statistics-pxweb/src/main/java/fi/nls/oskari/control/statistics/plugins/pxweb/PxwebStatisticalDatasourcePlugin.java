afff9555074b26d56341f3047ee5889dca00ad1e
Sami Mäkinen <zakarfin@gmail.com>
2019-12-02 18:40:02 +0200

    URLencode data fetching url

--- service-statistics-pxweb/src/main/java/fi/nls/oskari/control/statistics/plugins/pxweb/PxwebStatisticalDatasourcePlugin.java
+++ service-statistics-pxweb/src/main/java/fi/nls/oskari/control/statistics/plugins/pxweb/PxwebStatisticalDatasourcePlugin.java
@@ -118,7 +118,7 @@ public class PxwebStatisticalDatasourcePlugin extends StatisticalDatasourcePlugi
             if(separatorIndex == -1) {
                 throw new ServiceRuntimeException("Unidentified indicator id: " + indicatorId);
             }
-            String indicatorSelector = indicatorId.substring(separatorIndex + 2);
+            String indicatorSelector = indicatorId.substring(separatorIndex + PxwebConfig.ID_SEPARATOR.length());
             indicatorId = indicatorId.substring(0, separatorIndex);
             params.addDimension(new StatisticalIndicatorDataDimension(config.getIndicatorKey(), indicatorSelector));
         }
@@ -181,7 +181,9 @@ public class PxwebStatisticalDatasourcePlugin extends StatisticalDatasourcePlugi
 
     private String createUrl(String baseUrl, String pathId) {
         if(!baseUrl.endsWith(".px")) {
-            return IOHelper.fixPath(baseUrl + "/" + IOHelper.urlEncode(pathId));
+            // URLencode id and replace any encoded / in path back from %2F
+            // Pxweb doesn't seem to like spaces as + so use payload encoding instead to change spaces to %20
+            return IOHelper.fixPath(baseUrl + "/" + IOHelper.urlEncodePayload(pathId).replace("%2F", "/"));
         }
         return baseUrl;
     }
