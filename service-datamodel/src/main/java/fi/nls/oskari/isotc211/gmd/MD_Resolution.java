a66617a3bb446e0406b44bb82190649bd1764bcb
Sami Mäkinen <sami.makinen@nls.fi>
2019-12-17 16:27:48 +0200

    Remove transport modules

--- service-datamodel/src/main/java/fi/nls/oskari/isotc211/gmd/MD_Resolution.java
+++
@@ -1,9 +0,0 @@
-package fi.nls.oskari.isotc211.gmd;
-
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-
-@JacksonXmlRootElement(namespace = "http://www.isotc211.org/2005/gmd")
-public class MD_Resolution extends MD_ResolutionType {
-
-   
-}
