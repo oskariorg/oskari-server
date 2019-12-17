253a300ce40c7ed51a95fb68ec14bfc0cfbb9c10
Sami Mäkinen <sami.makinen@nls.fi>
2019-12-17 16:27:48 +0200

    Remove transport

--- service-datamodel/src/main/java/fi/nls/oskari/fe/xml/util/ReferencePropertyType.java
+++
@@ -1,8 +0,0 @@
-package fi.nls.oskari.fe.xml.util;
-
-import java.util.List;
-
-public class ReferencePropertyType<T> extends Reference {
-
-    public List<T> ref;
-}
--- servlet-transport/src/test/java/fi/nls/oskari/work/fe/FeatureEngineTestHelper.java
+++
@@ -1,5 +0,0 @@
-package fi.nls.oskari.work.fe;
-
-public class FeatureEngineTestHelper {
-
-}
