92b3720aaf82f0455d9fac13b9cb4e2aa8e68881
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2020-01-29 14:05:10 +0200

    Format code

--- control-base/src/main/java/fi/nls/oskari/control/layer/DataProviderHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/DataProviderHandler.java
@@ -32,9 +32,9 @@ public class DataProviderHandler extends RestActionHandler {
 
 	@Override
 	public void handleGet(ActionParameters params) throws ActionException {
-		
+
 		final int id = params.getRequiredParamInt(PARAM_ID);
-		
+
 		try {
 			DataProvider d = dataProviderService.find(id);
 			ResponseHelper.writeResponse(params, d.getAsJSON());
@@ -42,79 +42,80 @@ public class DataProviderHandler extends RestActionHandler {
 			throw new ActionException("Data provider query failed", e);
 		}
 	}
+
 	@Override
 	public void handleDelete(ActionParameters params) throws ActionException {
-		
+
 		final int id = params.getRequiredParamInt(PARAM_ID);
 		final boolean deleteLayers = params.getRequiredParamBoolean(PARAM_DELETE_LAYERS);
-		
-		if(!dataProviderService.hasPermissionToUpdate(params.getUser(), id)) {
-            throw new ActionDeniedException("Unauthorized user tried to remove data provider id=" + id);
-        }
-		
-		if(deleteLayers) {
+
+		if (!dataProviderService.hasPermissionToUpdate(params.getUser(), id)) {
+			throw new ActionDeniedException("Unauthorized user tried to remove data provider id=" + id);
+		}
+
+		if (deleteLayers) {
 			deleteDataProviderAndLayers(id, params);
 		} else {
 			deleteDataProvider(id, params);
 		}
 	}
-	
+
 	/**
-	 * Method deletes data provider.
-	 * Data provider is removed from map layers belonging to the data provider prior delete.
+	 * Method deletes data provider. Data provider is removed from map layers
+	 * belonging to the data provider prior delete.
 	 *
-	 * @param int id
+	 * @param int              id
 	 * @param ActionParameters params
 	 * @throws ActionException
 	 */
 	private void deleteDataProvider(int id, ActionParameters params) throws ActionException {
 		try {
 			DataProvider dataProvider = dataProviderService.find(id);
-            if(dataProvider == null) {
-                throw new ActionParamsException("Dataprovider not found with id " + id);
-            }
+			if (dataProvider == null) {
+				throw new ActionParamsException("Dataprovider not found with id " + id);
+			}
 			List<OskariLayer> layers = mapLayerService.findByDataProviderId(id);
 			layers.stream().forEach(layer -> {
 				layer.removeDataprovider(id);
 				mapLayerService.update(layer);
 			});
 			dataProviderService.delete(id);
-			AuditLog.user(params.getClientIp(), params.getUser())
-	            .withParam("id", dataProvider.getId())
-	            .withParam("name", dataProvider.getName(PropertyUtil.getDefaultLanguage()))
-	            .deleted(AuditLog.ResourceType.DATAPROVIDER);
+			AuditLog.user(params.getClientIp(), params.getUser()).withParam("id", dataProvider.getId())
+					.withParam("name", dataProvider.getName(PropertyUtil.getDefaultLanguage()))
+					.deleted(AuditLog.ResourceType.DATAPROVIDER);
 
 			// write deleted organization as response
 			ResponseHelper.writeResponse(params, dataProvider.getAsJSON());
 		} catch (Exception e) {
-            throw new ActionException("Couldn't delete data provider with id:" + id, e);
-        }
+			throw new ActionException("Couldn't delete data provider with id:" + id, e);
+		}
 	}
+
 	/**
-	 * Method deletes data provider and map layers which belong to the data provider.
+	 * Method deletes data provider and map layers which belong to the data
+	 * provider.
 	 *
-	 * @param int id
+	 * @param int              id
 	 * @param ActionParameters params
 	 * @throws ActionException
 	 */
 	private void deleteDataProviderAndLayers(int id, ActionParameters params) throws ActionException {
 		try {
-            DataProvider organization = dataProviderService.find(id);
-            if(organization == null) {
-                throw new ActionParamsException("Dataprovider not found with id " + id);
-            }
-            // cascade in db will handle that layers are deleted
-            dataProviderService.delete(id);
+			DataProvider organization = dataProviderService.find(id);
+			if (organization == null) {
+				throw new ActionParamsException("Dataprovider not found with id " + id);
+			}
+			// cascade in db will handle that layers are deleted
+			dataProviderService.delete(id);
 
-            AuditLog.user(params.getClientIp(), params.getUser())
-                    .withParam("id", organization.getId())
-                    .withParam("name", organization.getName(PropertyUtil.getDefaultLanguage()))
-                    .deleted(AuditLog.ResourceType.DATAPROVIDER);
+			AuditLog.user(params.getClientIp(), params.getUser()).withParam("id", organization.getId())
+					.withParam("name", organization.getName(PropertyUtil.getDefaultLanguage()))
+					.deleted(AuditLog.ResourceType.DATAPROVIDER);
 
-            // write deleted organization as response
-            ResponseHelper.writeResponse(params, organization.getAsJSON());
-        } catch (Exception e) {
-            throw new ActionException("Couldn't delete data provider and its map layers - id:" + id, e);
-        }
+			// write deleted organization as response
+			ResponseHelper.writeResponse(params, organization.getAsJSON());
+		} catch (Exception e) {
+			throw new ActionException("Couldn't delete data provider and its map layers - id:" + id, e);
+		}
 	}
 }
