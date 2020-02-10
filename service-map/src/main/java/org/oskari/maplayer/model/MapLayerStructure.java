c2ead85b14fd03502ce20d3be73c0c9b47fb3af7
Sami Mäkinen <sami.makinen@nls.fi>
2020-02-10 17:48:13 +0200

    Refactor wms structure

--- service-map/src/main/java/org/oskari/maplayer/model/MapLayerStructure.java
+++ service-map/src/main/java/org/oskari/maplayer/model/MapLayerStructure.java
@@ -2,9 +2,17 @@ package org.oskari.maplayer.model;
 
 import java.util.List;
 
-public class ServiceCapabilitiesResultWMS extends ServiceCapabilitiesResult {
-
+public class MapLayerStructure {
+    private String name;
     private List<MapLayerStructure> structure;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public List<MapLayerStructure> getStructure() {
         return structure;
