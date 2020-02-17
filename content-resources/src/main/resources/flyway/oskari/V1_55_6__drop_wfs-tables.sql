67b2c93c87cb4c72e084027f28d78e29d4625f58
Sami Mäkinen <sami.makinen@nls.fi>
2020-02-17 15:13:17 +0200

    Drop migrated tables

---
+++ content-resources/src/main/resources/flyway/oskari/V1_55_6__drop_wfs-tables.sql
@@ -0,0 +1,5 @@
+drop table if exists portti_wfs_template_model cascade;
+drop table if exists portti_wfs_layers_styles cascade;
+drop table if exists portti_wfs_layer_style cascade;
+drop table if exists oskari_wfs_parser_config cascade;
+drop table if exists portti_wfs_layer;
