e82cae1fdab25ab13fef50083a66e0d1efe187f3
Sami Mäkinen <sami.makinen@nls.fi>
2020-02-24 18:30:43 +0200

    Allow URLs with non-problematic params

--- service-map/src/main/java/org/oskari/maplayer/admin/LayerValidator.java
+++ service-map/src/main/java/org/oskari/maplayer/admin/LayerValidator.java
@@ -1,17 +1,19 @@
 package org.oskari.maplayer.admin;
 
 import fi.nls.oskari.service.ServiceRuntimeException;
+import fi.nls.oskari.util.ConversionHelper;
+import fi.nls.oskari.util.IOHelper;
 import fi.nls.oskari.util.PropertyUtil;
 import org.oskari.util.HtmlHelper;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LayerValidator {
+
+    private static final Set<String> RESERVED_PARAMS = ConversionHelper.asSet("service", "request", "version");
 
     public static Map<String, Map<String, String>> validateLocale(Map<String, Map<String, String>> locale) throws ServiceRuntimeException {
         String lang = PropertyUtil.getDefaultLanguage();
@@ -24,17 +26,22 @@ public class LayerValidator {
     }
 
     public static String validateUrl(final String url) throws ServiceRuntimeException {
-        // TODO remove query part with ? check or with URL object
-        // TODO: can we remove the whole querystring or should we just remove problematic params like service and request?
-        String baseUrl = url.contains("?") ? url.substring(0, url.indexOf("?")) : url;
-        baseUrl = baseUrl.endsWith("/") ? baseUrl.substring(0, baseUrl.length() - 1) : baseUrl;
         try {
-            // check that it's a valid url by creating an URL object...
-            new URL(url);
-        } catch (MalformedURLException e) {
+            String baseURL = IOHelper.removeQueryString(url);
+            // remove problematic parameters from URL
+            Map<String, List<String>> params = IOHelper.parseQuerystring(url);
+            List<String> problematicParams = params.keySet()
+                    .stream().filter(key -> RESERVED_PARAMS.contains(key.toLowerCase())).collect(Collectors.toList());
+            // TODO: these are only problematic for OGC urls. Maybe we should skip validation for non-OGC layers?
+            for (String param : problematicParams) {
+                params.remove(param);
+            }
+            String querystring = IOHelper.createQuerystring(params);
+            // return whitelisted params and base url
+            return IOHelper.addQueryString(baseURL, querystring);
+        } catch (Exception e) {
             throw new ServiceRuntimeException("Invalid url: " + url, "invalid_field_value");
         }
-        return baseUrl;
     }
 
     public static String cleanGFIContent(String gfiContent) {
