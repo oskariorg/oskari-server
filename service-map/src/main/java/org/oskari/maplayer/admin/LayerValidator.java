162f2fc4d62f2676a9d818e6997e842a50d3e67d
Sami Mäkinen <zakarfin@gmail.com>
2020-03-04 17:03:55 +0200

    Make layer input validation more generic

--- service-map/src/main/java/org/oskari/maplayer/admin/LayerValidator.java
+++ service-map/src/main/java/org/oskari/maplayer/admin/LayerValidator.java
@@ -1,17 +1,28 @@
 package org.oskari.maplayer.admin;
 
+import fi.nls.oskari.domain.map.OskariLayer;
 import fi.nls.oskari.service.ServiceRuntimeException;
 import fi.nls.oskari.util.ConversionHelper;
 import fi.nls.oskari.util.IOHelper;
 import fi.nls.oskari.util.PropertyUtil;
+import org.oskari.maplayer.model.MapLayer;
 import org.oskari.util.HtmlHelper;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class LayerValidator {
+
+    private static Map<String, Set<String>> MANDATORY_FIELDS = new HashMap<>();
+    static {
+        MANDATORY_FIELDS.put(OskariLayer.TYPE_WMS, ConversionHelper.asSet("name", "url", "version"));
+        MANDATORY_FIELDS.put(OskariLayer.TYPE_WFS, ConversionHelper.asSet("name", "url", "version"));
+        MANDATORY_FIELDS.put(OskariLayer.TYPE_WMTS, ConversionHelper.asSet("name", "url", "version"));
+        MANDATORY_FIELDS.put(OskariLayer.TYPE_VECTOR_TILE, ConversionHelper.asSet("url"));
+        MANDATORY_FIELDS.put(OskariLayer.TYPE_ARCGIS93, ConversionHelper.asSet("url"));
+        //MANDATORY_FIELDS.put(OskariLayer.TYPE_3DTILES, ConversionHelper.asSet("url"));
+    }
 
     private static final Set<String> RESERVED_PARAMS = ConversionHelper.asSet("service", "request", "version");
 
@@ -71,4 +82,66 @@ public class LayerValidator {
         return HtmlHelper.cleanHTMLString(gfiContent, tags, attributes, protocols);
     }
 
+    // TODO: relay mandatory field info to frontend for UI so frontend can show what is mandatory
+    //  and validate before server is even called
+    public static Set<String> getMandatoryFields(String type) {
+        if (type == null || !MANDATORY_FIELDS.containsKey(type)) {
+            return Collections.emptySet();
+        }
+        return MANDATORY_FIELDS.get(type);
+    }
+
+    /**
+     * Validates input to be saved as a maplayer to the databse
+     * Note! Might modify values in input object like URL!!!!!
+     * @param input
+     * @throws ServiceRuntimeException if input is not valid and can't be automatically fixed
+     */
+    public static void validateLayerInput(MapLayer input) throws ServiceRuntimeException {
+        // TODO: add more validation for values
+        if (!hasValue(input.getType())) {
+            throw new ServiceRuntimeException("Required field missing 'type'");
+        }
+
+        Set<String> mandatoryFields = getMandatoryFields(input.getType());
+        for (String field: mandatoryFields) {
+            if (!hasValue(input, field)) {
+                throw new ServiceRuntimeException("Required field missing '" + field + "'");
+            }
+        }
+        if (mandatoryFields.contains("url")) {
+            // if url is mandatory -> validate that it's usable
+            input.setUrl(LayerValidator.validateUrl(input.getUrl()));
+        }
+        // at least default language must have name for any layer type
+        input.setLocale(LayerValidator.validateLocale(input.getLocale()));
+        // Run HTML-content through JSOUP
+        input.setGfi_content(LayerValidator.cleanGFIContent(input.getGfi_content()));
+    }
+
+    /**
+     * Tries to find a getter for field through reflection and assumes it returns a string.
+     * throws an exception if value is not set or is empty or getter can't be called.
+     * @param input maplayer input for initial db or admin functionality
+     * @param field name of the field to search for "url" for getUrl() etc. case-insensitive
+     * @return
+     */
+    private static boolean hasValue(MapLayer input, String field) {
+        for (Method m : MapLayer.class.getDeclaredMethods()) {
+            if (!m.getName().toLowerCase().equals("get" + field.toLowerCase())) {
+                continue;
+            }
+            try {
+                String value = (String) m.invoke(input);
+                return hasValue(value);
+            } catch (Exception e) {
+                throw new ServiceRuntimeException("Unable to call getter for " + field, e);
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasValue(String value) {
+        return value != null && !value.trim().isEmpty();
+    }
 }
--- service-map/src/test/java/org/oskari/maplayer/admin/LayerValidatorTest.java
+++ service-map/src/test/java/org/oskari/maplayer/admin/LayerValidatorTest.java
@@ -1,9 +1,12 @@
 package org.oskari.maplayer.admin;
 
+import fi.nls.oskari.domain.map.OskariLayer;
 import fi.nls.oskari.service.ServiceRuntimeException;
 import org.junit.Test;
+import org.oskari.maplayer.model.MapLayer;
 
 import java.net.URL;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -28,5 +31,57 @@ public class LayerValidatorTest {
         String url = "sgashahah";
         String validated = LayerValidator.validateUrl(url);
         fail("Should have thrown exception");
+    }
+
+    @Test
+    public void validateWMSLayer() {
+        MapLayer input = new MapLayer();
+        try {
+            LayerValidator.validateLayerInput(input);
+        } catch (ServiceRuntimeException e) {
+            assertEquals("Required field missing 'type'", e.getMessage());
+        }
+
+        input.setType(OskariLayer.TYPE_WMS);
+        try {
+            LayerValidator.validateLayerInput(input);
+        } catch (ServiceRuntimeException e) {
+            assertEquals("Required field missing 'name'", e.getMessage());
+        }
+        input.setName("testing");
+        try {
+            LayerValidator.validateLayerInput(input);
+        } catch (ServiceRuntimeException e) {
+            assertEquals("Required field missing 'url'", e.getMessage());
+        }
+
+        input.setUrl("http://oskari.org/testing");
+        try {
+            LayerValidator.validateLayerInput(input);
+        } catch (ServiceRuntimeException e) {
+            assertEquals("Required field missing 'version'", e.getMessage());
+        }
+
+        input.setVersion("1.1.0");
+        try {
+            LayerValidator.validateLayerInput(input);
+        } catch (ServiceRuntimeException e) {
+            //
+            assertEquals("Localization for layer names missing", e.getMessage());
+        }
+        input.setLocale(new HashMap<>());
+        try {
+            LayerValidator.validateLayerInput(input);
+        } catch (ServiceRuntimeException e) {
+            // Localization for layer names missing
+            assertEquals("Name missing for default language: en", e.getMessage());
+        }
+
+        HashMap locale = new HashMap<>();
+        HashMap en = new HashMap<>();
+        en.put("name", "testing");
+        locale.put("en", en);
+        input.setLocale(locale);
+        LayerValidator.validateLayerInput(input);
     }
 }
