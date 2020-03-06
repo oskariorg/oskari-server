21fa69ea38e56ed6b3910f33cdbefce976dc8a06
Sami Mäkinen <zakarfin@gmail.com>
2020-03-06 18:09:14 +0200

    Remove custom mock and use DummyUserService

--- control-admin/src/test/java/fi/nls/oskari/control/admin/LayerAdminMetadataHandlerTest.java
+++ control-admin/src/test/java/fi/nls/oskari/control/admin/LayerAdminMetadataHandlerTest.java
@@ -1,12 +1,14 @@
 package fi.nls.oskari.control.admin;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.HashSet;
 
+import fi.nls.oskari.service.DummyUserService;
+import fi.nls.oskari.util.PropertyUtil;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,69 +27,47 @@ import fi.nls.test.control.JSONActionRouteTest;
 import fi.nls.test.util.ResourceHelper;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({UserService.class, OskariComponentManager.class})
+@PrepareForTest({OskariComponentManager.class})
 public class LayerAdminMetadataHandlerTest extends JSONActionRouteTest {
     
     final private static LayerAdminMetadataHandler handler = new LayerAdminMetadataHandler();
     
     @BeforeClass
-    public static void setup() throws ServiceException {
-        
-        setupUserServiceMock();
+    public static void setup() throws Exception {
+        PropertyUtil.addProperty("oskari.user.service", DummyUserService.class.getCanonicalName(), true);
         setupPermissionsServiceMock();
         handler.init();
     }
-    private static void setupUserServiceMock() throws ServiceException {
-        
-        UserService userService = mock(UserService.class);
-        PowerMockito.mockStatic(UserService.class);
-        when(UserService.getInstance()).thenReturn(userService);
-        
-        Role role1 = new Role();
-        role1.setId(1L);
-        role1.setName("Guest");
-        Role role2 = new Role();
-        role2.setId(2L);
-        role2.setName("User");
-        Role role3 = new Role();
-        role3.setId(3L);
-        role3.setName("Admin");
-        
-        Role[] mockRoles = new Role[]{role1,role2,role3};
-        
-        doReturn(mockRoles).when(userService).getRoles();
-        doReturn(Role.getAdminRole()).when(userService).getRoleByName("Admin");
-
+    @AfterClass
+    public static void tearDown() {
+        PropertyUtil.clearProperties();
     }
-    private static void setupPermissionsServiceMock() throws ServiceException {
+
+    private static void setupPermissionsServiceMock() {
         PermissionService permissionService = mock(PermissionService.class);
         PowerMockito.mockStatic(OskariComponentManager.class);
         when(OskariComponentManager.getComponentOfType(PermissionService.class)).thenReturn(permissionService);
         
         doReturn(new HashSet<String>()).when(permissionService).getAdditionalPermissions();
-        when(permissionService.getPermissionName(anyString(),anyString())).thenAnswer(invocation -> { return (String) invocation.getArgument(0); });
+        doAnswer(invocation -> invocation.getArgument(0)).when(permissionService).getPermissionName(anyString(),anyString());
     }
     
     @Test
     public void testGetWithAdmin() throws Exception {
-   
         final ActionParameters params = createActionParams(getAdminUser());
         handler.handleAction(params);
-
         verifyResponseContent(ResourceHelper.readJSONResource("LayerAdminMetadataHandlerTests-expected.json", this));
     }
     
     
     @Test(expected = ActionDeniedException.class)
     public void testGetWithGuest() throws Exception {
-
         final ActionParameters params = createActionParams(getGuestUser());
         handler.handleAction(params);
     }
     
     @Test(expected = ActionDeniedException.class)
     public void testGetWithLoggedInNonAdminUser() throws Exception {
-
         final ActionParameters params = createActionParams(getLoggedInUser());
         handler.handleAction(params);
     }
