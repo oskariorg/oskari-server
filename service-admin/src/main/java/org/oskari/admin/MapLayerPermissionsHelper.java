ac93b311c95fa790e13a1b92f2cc48f42dcd5065
Sami Mäkinen <sami.makinen@nls.fi>
2020-03-16 14:18:26 +0200

    Support partial role update

--- service-admin/src/main/java/org/oskari/admin/MapLayerPermissionsHelper.java
+++ service-admin/src/main/java/org/oskari/admin/MapLayerPermissionsHelper.java
@@ -16,23 +16,31 @@ import java.util.Set;
 public class MapLayerPermissionsHelper {
 
     /*
+    Removes permissions for roles that are referenced in permissions parameter and updates them.
+    To remove permissions from a role send an empty array:
     "role_permissions": {
         "Guest" : ["VIEW_LAYER"],
         "User" : ["VIEW_LAYER"],
-        "Administrator" : ["VIEW_LAYER"]
+        "Administrator" : []
     }
+
+    Permissions for any roles that are NOT referenced will be kept as is (allows non-admins to modify).
     */
     public static void setLayerPermissions(int layerId, Map<String, Set<String>> permissions) {
         if (permissions == null || permissions.isEmpty()) {
-            // TODO: should we clear the layer permissions here for consistency?
             return;
         }
-        final Resource res = new Resource();
+
+        Resource res = getPermissionService()
+                .findResource(ResourceType.maplayer, Integer.toString(layerId))
+                .orElse(new Resource());
         res.setType(ResourceType.maplayer);
         res.setMapping(Integer.toString(layerId));
+        Set<String> roleNames = permissions.keySet();
+        removePermissionsForRoles(res, roleNames);
 
         MybatisRoleService roleService = getRoleService();
-        for (String roleName : permissions.keySet()) {
+        for (String roleName : roleNames) {
             final Role role = roleService.findRoleByName(roleName);
             if (role == null) {
                 // log.warn("Couldn't find matching role in DB:", roleName, "- Skipping!");
@@ -43,6 +51,9 @@ public class MapLayerPermissionsHelper {
                 continue;
             }
             for (String type : permissionTypes) {
+                if (type == null || type.trim().isEmpty()) {
+                    continue;
+                }
                 final Permission permission = new Permission();
                 permission.setExternalType(PermissionExternalType.ROLE);
                 permission.setExternalId((int) role.getId());
@@ -51,6 +62,25 @@ public class MapLayerPermissionsHelper {
             }
         }
         getPermissionService().saveResource(res);
+    }
+
+    /**
+     * Removes any existing role based permissions from resource
+     * @param resource
+     * @param roleNames names of roles that need to be purged
+     */
+    private static void removePermissionsForRoles(Resource resource, Set<String> roleNames) {
+        if (resource == null || roleNames == null) {
+            return;
+        }
+        MybatisRoleService roleService = getRoleService();
+        for (String roleName : roleNames) {
+            final Role role = roleService.findRoleByName(roleName);
+            if (role == null) {
+                continue;
+            }
+            resource.removePermissionsForExternalType(PermissionExternalType.ROLE, (int) role.getId());
+        }
     }
 
     public static void removePermissions(final int layerId) {
