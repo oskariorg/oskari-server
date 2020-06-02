f8eb8efdcc9dab1fd065c86be90cac536c334a3f
Janne Heikkilä <janne.heikkila@maanmittauslaitos.fi>
2020-06-02 10:02:40 +0300

    Add MyPlacesLayersServiceMybatisImpl

---
+++ service-myplaces/src/main/java/org/oskari/myplaces/service/mybatis/MyPlaceCategoryMapper.java
@@ -0,0 +1,54 @@
+package org.oskari.myplaces.service.mybatis;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import fi.nls.oskari.domain.map.MyPlaceCategory;
+
+public interface MyPlaceCategoryMapper {
+
+    @Results(id = "CategoryResult", value = {
+            @Result(property="id", column="id", id=true),
+            @Result(property="uuid", column="uuid"),
+            @Result(property="isDefault", column="default"),
+            @Result(property="publisher_name", column="publisher_name"),
+            @Result(property="category_name", column="category_name"),
+            @Result(property="options", column="options")
+    })
+    @Select("SELECT id, uuid, \"default\", publisher_name, category_name, options FROM categories WHERE id = #{id}")
+    public MyPlaceCategory getById(long id);
+
+    @ResultMap("CategoryResult")
+    @Select("SELECT id, uuid, \"default\", publisher_name, category_name, options FROM categories WHERE uuid = #{uuid}")
+    public List<MyPlaceCategory> getByUserId(String uuid);
+
+    @Insert("INSERT INTO categories (uuid, \"default\", publisher_name, category_name, options)"
+            + " VALUES (#{uuid}, #{isDefault}, #{publisher_name}, #{category_name}, #{options})")
+    @Options(useGeneratedKeys=true, keyColumn="id", keyProperty="id")
+    public void insert(MyPlaceCategory category);
+
+    @Update("UPDATE categories"
+            + " SET uuid = #{uuid}"
+            + ",\"default\" = #{isDefault}"
+            + ",publisher_name = #{publisher_name}"
+            + ",category_name = #{category_name}"
+            + ",options = #{options}"
+            + " WHERE id = #{id}")
+    public void update(MyPlaceCategory category);
+
+    @Delete("DELETE FROM categories WHERE id = #{id}")
+    public int delete(long id);
+
+    @Delete("DELETE FROM categories WHERE uuid = #{uuid}")
+    public int deleteByUuid(String uuid);
+
+
+}
---
+++ service-myplaces/src/test/java/org/oskari/myplaces/service/mybatis/MyPlacesLayersServiceMybatisImplTest.java
@@ -0,0 +1,103 @@
+package org.oskari.myplaces.service.mybatis;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import fi.nls.oskari.domain.map.MyPlaceCategory;
+import fi.nls.test.util.JSONTestHelper;
+
+public class MyPlacesLayersServiceMybatisImplTest {
+
+    @Test
+    public void happyCase() throws Exception {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;INIT=create domain if not exists json as text;MODE=PostgreSQL");
+
+        Path path = Paths.get(getClass().getClassLoader().getResource("categories_ddl.sql").toURI());
+        byte[] utf8 = Files.readAllBytes(path);
+        String ddl = new String(utf8, StandardCharsets.UTF_8);
+
+        try (Connection c = ds.getConnection();
+                Statement stmt = c.createStatement()) {
+            stmt.execute(ddl);
+        }
+
+        MyPlacesLayersServiceMybatisImpl service = new MyPlacesLayersServiceMybatisImpl(ds);
+
+        String uuid = UUID.randomUUID().toString();
+
+        JSONObject options = new JSONObject();
+        options.put("foo", "bar");
+        options.put("qux", 112);
+
+        MyPlaceCategory expected = new MyPlaceCategory();
+        expected.setUuid(uuid);
+        expected.setCategory_name("foobar");
+        expected.setPublisher_name("bazqux");
+        expected.setDefault(true);
+        expected.setOptions(options);
+
+        assertEquals(0, expected.getId());
+        assertEquals(1, service.insert(Arrays.asList(expected)));
+        assertEquals("Insert should modify id of the object", 1, expected.getId());
+
+        // Test getById
+        MyPlaceCategory actual = service.getById(1).get();
+        assertEquals(1, actual.getId());
+        assertEq(expected, actual);
+
+        // Test getByIds
+        List<MyPlaceCategory> actuals = service.getByIds(new long[] { 1, 2, 3 });
+        assertEquals("Only id 1 should exist", 1, actuals.size());
+        actual = actuals.get(0);
+        assertEquals(1, actual.getId());
+        assertEq(expected, actual);
+
+        // Test getByUserId
+        actuals = service.getByUserId(uuid);
+        assertEquals("User should have 1 category", 1, actuals.size());
+        actual = actuals.get(0);
+        assertEq(expected, actual);
+
+        // Test update
+        expected.setCategory_name("laalaa");
+        expected.setPublisher_name("faxnax");
+        expected.setDefault(false);
+        assertEquals(1, service.update(Arrays.asList(expected)));
+        actual = service.getById(1).get();
+        assertEq(expected, actual);
+        // Sanity test to check assertEq(MyPlaceCategory, MyPlaceCategory) works
+        assertEquals(1, actual.getId());
+        assertEquals("laalaa", actual.getCategory_name());
+        assertEquals("faxnax", actual.getPublisher_name());
+        assertEquals(false, actual.isDefault());
+        JSONTestHelper.shouldEqual(actual.getOptions(), new JSONObject("{'foo': 'bar', 'qux': 112}".replace('\'', '"')));
+
+        // Test delete
+        service.delete(new long[] { 1 });
+        assertEquals("After delete id 1 should not be available", false, service.getById(1).isPresent());
+        assertEquals("After delete user should have no categories", 0, service.getByUserId(uuid).size());
+    }
+
+    private void assertEq(MyPlaceCategory expected, MyPlaceCategory actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getUuid(), actual.getUuid());
+        assertEquals(expected.getCategory_name(), actual.getCategory_name());
+        assertEquals(expected.getPublisher_name(), actual.getPublisher_name());
+        JSONTestHelper.shouldEqual(actual.getOptions(), expected.getOptions());
+    }
+
+}
