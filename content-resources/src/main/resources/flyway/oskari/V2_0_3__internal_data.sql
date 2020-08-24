55cef061c0cfe42758ca8eb1e8ca17039767f683
Sami Mäkinen <sami.makinen@nls.fi>
2020-08-24 14:45:25 +0300

    Add initial roles

--- content-resources/src/main/resources/flyway/oskari/V2_0_3__internal_data.sql
+++ content-resources/src/main/resources/flyway/oskari/V2_0_3__internal_data.sql
@@ -1,2 +1,7 @@
--- so baselayers for user generated data (myplaces etc) have a dataprovider to use
-insert into oskari_dataprovider(locale) values('{ "en":{ "name":"Oskari.org"} }');
+-- Initial dataprovider for user generated data (myplaces etc) baselayers to use
+INSERT INTO oskari_dataprovider (locale) VALUES ('{ "en":{ "name":"Oskari.org"} }');
+
+-- Initial roles for the database
+INSERT INTO oskari_roles (name, is_guest) VALUES ('Guest', true);
+INSERT INTO oskari_roles (name, is_guest) VALUES ('User', false);
+INSERT INTO oskari_roles (name, is_guest) VALUES ('Admin', false);
