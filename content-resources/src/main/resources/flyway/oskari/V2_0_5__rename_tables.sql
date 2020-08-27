ad4fb7a7ce8c2d6b82e856b77f4fc604ccbcf7f9
Sami Mäkinen <sami.makinen@nls.fi>
2020-08-27 12:04:06 +0300

    Fix SQL syntax

--- content-resources/src/main/resources/flyway/oskari/V2_0_5__rename_tables.sql
+++ content-resources/src/main/resources/flyway/oskari/V2_0_5__rename_tables.sql
@@ -2,24 +2,25 @@ ALTER TABLE portti_bundle RENAME TO oskari_bundle;
 
 ALTER TABLE oskari_bundle
     ALTER COLUMN name TYPE TEXT,
-    ALTER COLUMN config TYPE TEXT DEFAULT '',
-    ALTER COLUMN state TYPE TEXT DEFAULT '',
+    ALTER COLUMN config TYPE TEXT,
+    ALTER COLUMN state TYPE TEXT,
     DROP COLUMN startup;
 
 ALTER TABLE portti_view RENAME TO oskari_appsetup;
 
 ALTER TABLE oskari_appsetup
-    ALTER COLUMN name TYPE TEXT NOT NULL,
+    ALTER COLUMN name TYPE TEXT,
     ALTER COLUMN description TYPE TEXT,
-    ALTER COLUMN application TYPE TEXT DEFAULT 'geoportal',
-    ALTER COLUMN domain TYPE TEXT DEFAULT '',
+    ALTER COLUMN application TYPE TEXT,
+    ALTER COLUMN domain TYPE TEXT,
     DROP COLUMN application_dev_prefix,
     DROP COLUMN old_id;
 
 ALTER TABLE portti_view_bundle_seq RENAME TO oskari_appsetup_bundles;
 
 ALTER TABLE oskari_appsetup_bundles
-    RENAME COLUMN view_id TO appsetup_id,
-    ALTER COLUMN name TYPE TEXT,
-    ALTER COLUMN bundleinstance TYPE TEXT DEFAULT '',
+    ALTER COLUMN bundleinstance TYPE TEXT,
     DROP COLUMN startup;
+
+ALTER TABLE oskari_appsetup_bundles
+    RENAME COLUMN view_id TO appsetup_id;
