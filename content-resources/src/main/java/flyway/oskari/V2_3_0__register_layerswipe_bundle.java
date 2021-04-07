fdbbad1d6e752e3d8764be9900f968f42b6663b4
Sami Mäkinen <sami.makinen@nls.fi>
2021-04-07 18:12:16 +0300

    Cleanup

--- content-resources/src/main/java/flyway/oskari/V2_3_0__register_layerswipe_bundle.java
+++ content-resources/src/main/java/flyway/oskari/V2_3_0__register_layerswipe_bundle.java
@@ -4,15 +4,10 @@ import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.oskari.helpers.BundleHelper;
 
-import java.sql.Connection;
-
 public class V2_3_0__register_layerswipe_bundle extends BaseJavaMigration {
 
     @Override
 	public void migrate(Context context) throws Exception {
-        Connection connection = context.getConnection();
-		
-		// BundleHelper checks if these bundles are already registered
-        BundleHelper.registerBundle(connection, "layerswipe");
+        BundleHelper.registerBundle(context.getConnection(), "layerswipe");
 	}
 }
