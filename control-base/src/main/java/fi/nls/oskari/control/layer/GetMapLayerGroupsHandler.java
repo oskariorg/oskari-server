55293db1033b5af05923f290757395397bdd277a
Sami Mäkinen <sami.makinen@nls.fi>
2021-06-02 15:28:18 +0300

    Cache layerlisting based on roles/lang etc

--- control-base/src/main/java/fi/nls/oskari/control/layer/GetMapLayerGroupsHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/layer/GetMapLayerGroupsHandler.java
@@ -5,9 +5,14 @@ import static fi.nls.oskari.control.ActionConstants.PARAM_LANGUAGE;
 import static fi.nls.oskari.control.ActionConstants.PARAM_SRS;
 import static fi.nls.oskari.control.ActionConstants.PARAM_ID;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import fi.nls.oskari.cache.Cache;
+import fi.nls.oskari.cache.CacheManager;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.map.layer.OskariLayerService;
 import fi.nls.oskari.service.OskariComponentManager;
 import fi.nls.oskari.util.ConversionHelper;
@@ -37,16 +42,21 @@ import org.oskari.service.util.ServiceFactory;
 @OskariActionRoute("GetHierarchicalMapLayerGroups")
 public class GetMapLayerGroupsHandler extends ActionHandler {
 
+    public static final String CACHE_NAME = "LayerList";
     private static final String KEY_GROUPS = "groups";
     private static final String KEY_LAYERS = "layers";
     private static final String KEY_ID = "id";
     private static final String KEY_ORDER_NUMBER = "orderNumber";
+    private static final Logger LOG = LogFactory.getLogger(GetMapLayerGroupsHandler.class);
+
 
     private static final List<String> PROXY_LYR_TYPES = Arrays.asList(
             OskariLayer.TYPE_WMS,
             OskariLayer.TYPE_WMTS,
             OskariLayer.TYPE_ARCGIS93,
             OskariLayer.TYPE_VECTOR_TILE);
+
+    private static final Cache<String> cache = CacheManager.getCache(CACHE_NAME);
 
     private OskariLayerService layerService;
     private OskariMapLayerGroupService groupService;
@@ -82,7 +92,34 @@ public class GetMapLayerGroupsHandler extends ActionHandler {
         final User user = params.getUser();
         final String lang = params.getHttpParam(PARAM_LANGUAGE, params.getLocale().getLanguage());
         final String crs = params.getHttpParam(PARAM_SRS);
-        final boolean isSecure = EnvHelper.isSecure(params);
+        final String requestedIds = params.getHttpParam(PARAM_ID);
+        final boolean forceProxy = params.getHttpParam(PARAM_FORCE_PROXY, false);
+        String response = null;
+        String cacheKey = getCacheKey(user, lang, crs, forceProxy);
+        if (requestedIds == null) {
+            // only use cache when the whole listing is requested
+            // FIXME!! Cache isn't flushed when permissions/layers are changed
+            response = cache.get(cacheKey);
+        }
+        if (response == null) {
+            // NOTE, isSecure doesn't change in instances without restarting it so it's safe to skip on the cache key
+            response = getList(user, lang, crs, requestedIds, forceProxy, EnvHelper.isSecure(params));
+            if (requestedIds == null) {
+                // only use cache when the whole listing is requested
+                cache.put(cacheKey, response);
+            }
+        }
+        try {
+            params.getResponse().setCharacterEncoding("UTF-8");
+            params.getResponse().setContentType("application/json;charset=UTF-8");
+            params.getResponse().getWriter().print(response);
+        } catch (IOException e) {
+            LOG.info("Couldn't write answer:", e.getMessage());
+            LOG.debug(e);
+        }
+    }
+
+    public String getList(User user, String lang, String crs, String requestedIds, boolean forceProxy, boolean isSecure) throws ActionException {
         final boolean isPublished = false;
 
         Map<Integer, List<MaplayerGroup>> groupsByParentId = groupService.findAll().stream()
@@ -92,8 +129,8 @@ public class GetMapLayerGroupsHandler extends ActionHandler {
                 .collect(Collectors.groupingBy(OskariLayerGroupLink::getGroupId));
 
         // Get all layers instead of using OskariLayerWorker.getLayersForUser() so we don't check permissions twice
-        List<OskariLayer> layers = getLayers(params.getHttpParam(PARAM_ID));
-        if (params.getHttpParam(PARAM_FORCE_PROXY, false)) {
+        List<OskariLayer> layers = getLayers(requestedIds);
+        if (forceProxy) {
             layers.stream()
                     .filter(layer -> PROXY_LYR_TYPES.contains(layer.getType()))
                     .forEach(layer -> layer.addAttribute("forceProxy", true));
@@ -105,7 +142,7 @@ public class GetMapLayerGroupsHandler extends ActionHandler {
             // getListOfMapLayers checks permissions
             JSONObject response = OskariLayerWorker.getListOfMapLayers(layers, user, lang, crs, isPublished, isSecure);
             response.put(KEY_GROUPS, getGroupJSON(groupsByParentId, linksByGroupId, sortedLayerIds, -1));
-            ResponseHelper.writeResponse(params, response);
+            return response.toString();
         } catch (JSONException e) {
             throw new ActionException("Failed to add groups", e);
         }
@@ -182,6 +219,21 @@ public class GetMapLayerGroupsHandler extends ActionHandler {
 
     private boolean contains(int[] sortedLayerIds, int layerId) {
         return Arrays.binarySearch(sortedLayerIds, layerId) >= 0;
+    }
+
+    private String getCacheKey(User user, String lang, String crs, boolean forceProxy) {
+        return "layers_"
+                + lang + "_"
+                + crs + "_"
+                + forceProxy + "_"
+                + getUserRolesKey(user);
+    }
+
+    private String getUserRolesKey(User user) {
+        return user.getRoles().stream()
+                .map(r -> Long.toString(r.getId()))
+                .sorted()
+                .collect(Collectors.joining("_"));
     }
 
 }
