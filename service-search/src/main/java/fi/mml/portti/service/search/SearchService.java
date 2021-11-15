f4bf625f12f03ab6b970c30d06a4afbef6b2e7d8
Sami Mäkinen <sami.makinen@nls.fi>
2021-11-15 16:21:29 +0200

    Allow passing params like 'limit' to search

--- service-search/src/main/java/fi/mml/portti/service/search/SearchService.java
+++ service-search/src/main/java/fi/mml/portti/service/search/SearchService.java
@@ -15,6 +15,7 @@ import java.util.Map;
 public abstract class SearchService extends OskariComponent {
 
 	private int maxCount = -1;
+	private int hardLimit = -1;
 
 	@Override
 	public void init() {
@@ -35,12 +36,32 @@ public abstract class SearchService extends OskariComponent {
 	/**
 	 * Returns a generic maximum results instruction for search functions.
 	 * SearchChannels/implementations may opt to use this to
+	 * Can be configured with search.max.results property.
+	 * Defaults to 100.
 	 * @return maxCount  - Search results max count
      */
 	public int getMaxResultsCount() {
 		if (maxCount == -1) {
 			maxCount = ConversionHelper.getInt(PropertyUtil.getOptional("search.max.results"), 100);
 		}
+		if (hardLimit > 0 && maxCount > hardLimit) {
+			// sanity-check limit can't be bigger than hard limit if it has been configured
+			maxCount = hardLimit;
+		}
 		return maxCount;
+	}
+
+	/**
+	 * Returns instance limit for maximum results instruction for search functions.
+	 * SearchChannels/implementations may opt to use this to.
+	 * Can be configured with search.max.results.hardlimit property.
+	 * Defaults to 10x max count.
+	 * @return hard limit for search results. Client can't ask more than this
+	 */
+	public int getMaxResultsHardLimit() {
+		if (hardLimit == -1) {
+			hardLimit = ConversionHelper.getInt(PropertyUtil.getOptional("search.max.results.hardlimit"), getMaxResultsCount() * 10);
+		}
+		return hardLimit;
 	}
 }
