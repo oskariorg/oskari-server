f8573900ffaba200262c7ee6edd42cdcc19cda0a
Janne Heikkilä <janne.heikkila@maanmittauslaitos.fi>
2021-12-14 09:15:17 +0200

    Non-integer layer value should not throw an exception

--- control-admin/src/main/java/fi/nls/oskari/control/admin/LayerAdminHelper.java
+++ control-admin/src/main/java/fi/nls/oskari/control/admin/LayerAdminHelper.java
@@ -2,41 +2,38 @@ package fi.nls.oskari.control.admin;
 
 import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.domain.map.OskariLayer;
-import fi.nls.oskari.log.LogFactory;
-import fi.nls.oskari.log.Logger;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LayerAdminHelper {
-    private static final Logger LOG = LogFactory.getLogger(LayerAdminHelper.class);
 
     protected static Set<OskariLayer> getTimeseriesReferencedLayers(int layerId, List<OskariLayer> layers) throws ActionException {
-        Set<OskariLayer> timeseriesLayers = new HashSet<>();
-        for (OskariLayer layer : layers) {
-            JSONObject options = layer.getOptions();
-            try {
-                if (options != null && options.has("timeseries")) {
-                    JSONObject timeseriesOptions = options.getJSONObject("timeseries");
-                    JSONObject timeseriesMetadata = timeseriesOptions.optJSONObject("metadata");
-                    if (timeseriesMetadata == null) {
-                        continue;
-                    }
-                    Integer metadataLayerId = timeseriesMetadata.getInt("layer");
-                    if (metadataLayerId == layerId) {
-                        LOG.debug("Found timeseries referenced layer, layerId: " + layerId);
-                        timeseriesLayers.add(layer);
-                    }
-                }
-            } catch (JSONException e) {
-                throw new ActionException("Cannot parse layer metadata options for layer: " +
-                        layer.getName() + ", id: " + layer.getId());
-            }
+        return layers.stream()
+                .filter(it -> isReferencedByTimeseriesMetadata(layerId, it))
+                .collect(Collectors.toSet());
+    }
+
+    protected static boolean isReferencedByTimeseriesMetadata(int layerId, OskariLayer layer) {
+        JSONObject options = layer.getOptions();
+        if (options == null) {
+            return false;
         }
-        return timeseriesLayers;
+
+        JSONObject timeseriesOptions = options.optJSONObject("timeseries");
+        if (timeseriesOptions == null) {
+            return false;
+        }
+
+        JSONObject timeseriesMetadata = timeseriesOptions.optJSONObject("metadata");
+        if (timeseriesMetadata == null) {
+            return false;
+        }
+
+        int metadataLayerId = timeseriesMetadata.optInt("layer", -1);
+        return metadataLayerId == layerId;
     }
 
 }
