8457cdb2bb9e58da7b7ca478ddd1b7f1b36ddb6c
Sami Mäkinen <sami.makinen@nls.fi>
2021-12-22 19:33:03 +0200

    Changes for switching wdtinc -> no.ecc for mvt encoding

--- service-mvt/src/main/java/org/oskari/service/mvt/SimpleFeaturesMVTEncoder.java
+++ service-mvt/src/main/java/org/oskari/service/mvt/SimpleFeaturesMVTEncoder.java
@@ -1,11 +1,8 @@
 package org.oskari.service.mvt;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+import no.ecc.vectortile.VectorTileEncoder;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.opengis.feature.simple.SimpleFeature;
@@ -25,11 +22,7 @@ import org.locationtech.jts.geom.TopologyException;
 import org.locationtech.jts.geom.util.GeometryEditor;
 import org.locationtech.jts.operation.predicate.RectangleIntersects;
 import org.locationtech.jts.simplify.VWSimplifier;
-import com.wdtinc.mapbox_vector_tile.VectorTile;
-import com.wdtinc.mapbox_vector_tile.adapt.jts.IUserDataConverter;
-import com.wdtinc.mapbox_vector_tile.adapt.jts.JtsAdapter;
-import com.wdtinc.mapbox_vector_tile.build.MvtLayerBuild;
-import com.wdtinc.mapbox_vector_tile.build.MvtLayerProps;
+
 
 public class SimpleFeaturesMVTEncoder {
 
@@ -37,61 +30,22 @@ public class SimpleFeaturesMVTEncoder {
 
     public static byte[] encodeToByteArray(SimpleFeatureCollection sfc,
             String layer, double[] bbox, int extent, int buffer) {
-        return encode(sfc, layer, bbox, extent, buffer).toByteArray();
-    }
 
-    public static VectorTile.Tile encode(SimpleFeatureCollection sfc,
-            String layer, double[] bbox, int extent, int buffer) {
+        VectorTileEncoder encoder = new VectorTileEncoder();
         List<Geometry> mvtGeoms = asMVTGeoms(sfc, bbox, extent, buffer);
+        mvtGeoms.stream()
+                .map(geom -> SimpleFeatureConverter.fromGeometry(geom))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .forEach(f -> encoder.addFeature(layer, f.properties, f.geom));
+// Add one or more features with a layer name, a Map with attributes and a JTS Geometry.
+// The Geometry uses (0,0) in upper left and (256,256) in lower right.
 
-        VectorTile.Tile.Layer.Builder layerBuilder = VectorTile.Tile.Layer.newBuilder();
-        layerBuilder.setVersion(2);
-        layerBuilder.setName(layer);
-        layerBuilder.setExtent(extent);
-
-        MvtLayerProps layerProps = new MvtLayerProps();
-        IUserDataConverter converter = new SimpleFeatureConverter();
-        List<VectorTile.Tile.Feature> features = JtsAdapter.toFeatures(mvtGeoms, layerProps, converter);
-
-        layerBuilder.addAllFeatures(features);
-        MvtLayerBuild.writeProps(layerBuilder, layerProps);
-
-        VectorTile.Tile.Builder tileBuilder = VectorTile.Tile.newBuilder();
-        tileBuilder.addLayers(layerBuilder.build());
-
-        return tileBuilder.build();
+// Finally, get the byte array
+        byte[] encoded = encoder.encode();
+        return encoded;
+        //return encode(sfc, layer, bbox, extent, buffer).toByteArray();
     }
-
-    /* Not currently used - this uses asMVTGeoms() is an optimized version of this function
-    public static List<Geometry> asMVTGeoms2(SimpleFeatureCollection sfc, double[] bbox, int extent, int buffer) {
-        Envelope tileEnvelope = new Envelope(bbox[0], bbox[2], bbox[1], bbox[3]);
-        Envelope clipEnvelope = new Envelope(tileEnvelope);
-        if (buffer > 0) {
-            double bufferSizePercent = (double) buffer / extent;
-            double dx = bufferSizePercent * tileEnvelope.getWidth();
-            double dy = bufferSizePercent * tileEnvelope.getHeight();
-            clipEnvelope.expandBy(dx, dy);
-        }
-
-        List<Geometry> geoms = new ArrayList<>();
-        try (SimpleFeatureIterator it = sfc.features()) {
-            while (it.hasNext()) {
-                SimpleFeature sf = it.next();
-                Object g = sf.getDefaultGeometry();
-                if (g == null) {
-                    continue;
-                }
-
-                Geometry geom = (Geometry) g;
-                geom.setUserData(sf);
-                geoms.add(geom);
-            }
-        }
-
-        return JtsAdapter.createTileGeom(geoms, tileEnvelope, clipEnvelope, GF,
-                new MvtLayerParams(256, extent), new GeomMinSizeFilter(6, 6)).mvtGeoms;
-    }
-     */
 
     public static List<Geometry> asMVTGeoms(SimpleFeatureCollection sfc, double[] bbox, int extent, int buffer) {
         if (sfc.isEmpty()) {
