21d7ab63d8d53199c47f5127a05d6421015ebcc5
okauppinen <olli.kauppinen@nls.fi>
2022-01-25 15:40:55 +0200

    add locale to userdata layer json

--- service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterMYPLACES.java
+++ service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterMYPLACES.java
@@ -1,5 +1,6 @@
 package fi.nls.oskari.map.layer.formatters;
 
+import fi.nls.oskari.util.JSONHelper;
 import org.json.JSONObject;
 
 import fi.nls.oskari.domain.map.MyPlaceCategory;
@@ -14,8 +15,15 @@ public class LayerJSONFormatterMYPLACES extends LayerJSONFormatterUSERDATA {
 
     public JSONObject getJSON(OskariLayer baseLayer, MyPlaceCategory category, String srs, String lang) {
         category.getWFSLayerOptions().setProperty(KEY_IS_DEFAULT, category.isDefault());
+        final JSONObject layerJson = super.getJSON(baseLayer, category, srs, lang);
 
-        return super.getJSON(baseLayer, category, srs, lang);
+        // If user doesn't have category, default category is added with empty locale
+        // Categories isn't always handled by MyPlaces bundle so default localized names are stored in baselayer
+        if (category.getNames().isEmpty()) {
+            // use locale from baselayer
+            JSONHelper.putValue(layerJson, KEY_LOCALE, baseLayer.getLocale());
+        }
+        return layerJson;
     }
 
 }
