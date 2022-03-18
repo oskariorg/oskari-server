6c4248b1fe7334a01716537dbd77498713452973
jampukka <janne.z.heikkila@gmail.com>
2022-03-18 15:23:39 +0200

    Fix error with nested ANDs

--- service-wfs-client/src/main/java/org/oskari/service/wfs/client/OskariWFSFilterFactory.java
+++ service-wfs-client/src/main/java/org/oskari/service/wfs/client/OskariWFSFilterFactory.java
@@ -1,17 +1,23 @@
 package org.oskari.service.wfs.client;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fi.nls.oskari.domain.map.OskariLayer;
-import fi.nls.oskari.domain.map.wfs.WFSLayerCapabilities;
-import fi.nls.oskari.service.ServiceRuntimeException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.CRS;
 import org.json.JSONObject;
+import org.opengis.filter.And;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fi.nls.oskari.domain.map.OskariLayer;
+import fi.nls.oskari.domain.map.wfs.WFSLayerCapabilities;
+import fi.nls.oskari.service.ServiceRuntimeException;
 
 public class OskariWFSFilterFactory {
     private static FilterFactory ff = CommonFactoryFinder.getFilterFactory();
@@ -50,7 +56,44 @@ public class OskariWFSFilterFactory {
         } else if (toAppend == null) {
             return main;
         } else {
-            return ff.and(main, toAppend);
+            if (main instanceof And) {
+                List<Filter> toAnd = new ArrayList<>(((And) main).getChildren());
+                toAnd.add(toAppend);
+                return ff.and(toAnd);
+            } else if (toAppend instanceof And) {
+                List<Filter> toAnd = new ArrayList<>(((And) toAppend).getChildren());
+                toAnd.add(main);
+                return ff.and(toAnd);
+            } else {
+                return smartAnd(main, toAppend);
+            }
+        }
+    }
+
+    // Avoid unncessary nesting of ANDs (x AND y) AND (z AND w)
+    // => x AND y AND z AND w
+    private static Filter smartAnd(final Filter a, final Filter b) {
+        if (a instanceof And && b instanceof And) {
+            List<Filter> _a = ((And) a).getChildren();
+            List<Filter> _b = ((And) b).getChildren();
+            List<Filter> and = new ArrayList<>(_a.size() + _b.size());
+            and.addAll(_a);
+            and.addAll(_b);
+            return ff.and(and);
+        } else if (a instanceof And) {
+            List<Filter> _a = ((And) a).getChildren();
+            List<Filter> and = new ArrayList<>(_a.size() + 1);
+            and.addAll(_a);
+            and.add(b);
+            return ff.and(and);
+        } else if (b instanceof And) {
+            List<Filter> _b = ((And) b).getChildren();
+            List<Filter> and = new ArrayList<>(_b.size() + 1);
+            and.add(a);
+            and.addAll(_b);
+            return ff.and(and);
+        } else {
+            return ff.and(a, b);
         }
     }
 }
