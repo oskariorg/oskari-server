5e88a620409144325dd8c6b8250db594cb6da325
Sami Mäkinen <sami.makinen@nls.fi>
2022-03-21 12:02:07 +0200

    Use metadata id parser from CapabilitiesService

--- service-map/src/test/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterTest.java
+++ service-map/src/test/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterTest.java
@@ -57,23 +57,6 @@ public class LayerJSONFormatterTest {
     }
 
     @Test
-    public void getFixedDataUrl() {
-        assertNull("Null metadata returns null id", LayerJSONFormatter.getFixedDataUrl(null));
-        assertNull("Empty metadata returns null id", LayerJSONFormatter.getFixedDataUrl(""));
-        assertEquals("Non http-starting metadata returns as is", "testing", LayerJSONFormatter.getFixedDataUrl("testing"));
-        assertNull("Url without querystring returns null id", LayerJSONFormatter.getFixedDataUrl("http://mydomain.org"));
-        assertNull("Url without id|uuid returns null id", LayerJSONFormatter.getFixedDataUrl("http://mydomain.org?my=key"));
-        assertEquals("Url with id returns id value simple", "key", LayerJSONFormatter.getFixedDataUrl("http://mydomain.org?uuid=key"));
-        Map<String, String> expected = new HashMap<>();
-        expected.put("http://mydomain.org?test=test&id=key&post=test", "key");
-        expected.put("http://mydomain.org?test=test&uuid=key2&post=test", "key2");
-        expected.put("http://mydomain.org?test=test&Id=key&post=test", "key");
-        expected.put("http://mydomain.org?test=test&uuId=Key&post=test", "Key");
-        for (String url : expected.keySet()) {
-            assertEquals("Url with id returns id value", expected.get(url), LayerJSONFormatter.getFixedDataUrl(url));
-        }
-    }
-    @Test
     public void legendWMS() throws Exception {
         OskariLayer layer = initLayer(OskariLayer.TYPE_WMS);
         JSONObject layerJSON = FORMATTER.getJSON(layer, LANG, false, CRS);
