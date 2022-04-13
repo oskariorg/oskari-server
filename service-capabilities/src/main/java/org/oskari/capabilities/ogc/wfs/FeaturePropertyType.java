650190054cc235263e220fd74017c83e397975f6
Sami Mäkinen <sami.makinen@nls.fi>
2022-04-13 15:05:37 +0300

    Ignore isGeometry() for JSON serialization

--- service-capabilities/src/main/java/org/oskari/capabilities/ogc/wfs/FeaturePropertyType.java
+++ service-capabilities/src/main/java/org/oskari/capabilities/ogc/wfs/FeaturePropertyType.java
@@ -1,9 +1,12 @@
 package org.oskari.capabilities.ogc.wfs;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class FeaturePropertyType {
     public String name;
     public String type;
 
+    @JsonIgnore
     public boolean isGeometry() {
         return "GeometryPropertyType".equals(type);
     }
