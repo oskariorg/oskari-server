1d715ac9a7fd5084e947498380b6480e98ef208c
Sami Mäkinen <sami.makinen@nls.fi>
2022-04-13 16:35:49 +0300

    Follow redirects when requesting DescribeFeatureType

--- service-capabilities/src/main/java/org/oskari/capabilities/ogc/wfs/DescribeFeatureTypeProvider.java
+++ service-capabilities/src/main/java/org/oskari/capabilities/ogc/wfs/DescribeFeatureTypeProvider.java
@@ -3,11 +3,19 @@ package org.oskari.capabilities.ogc.wfs;
 import fi.nls.oskari.util.IOHelper;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 // separated provider to help mocking in JUnit test
 public class DescribeFeatureTypeProvider {
     public String getDescribeContent(String url, String user, String pass) throws IOException {
-        // TODO: follow redirects
-        return IOHelper.getURL(url, user, pass);
+        HttpURLConnection con = IOHelper.getConnection(url, user, pass);
+        con = IOHelper.followRedirect(con, user, pass, 5);
+        int sc = con.getResponseCode();
+        if (sc != HttpURLConnection.HTTP_OK) {
+            String msg = "Unexpected status code: " + sc  + " from: " + url;
+            throw new IOException(msg);
+        }
+
+        return IOHelper.readString(con);
     }
 }
