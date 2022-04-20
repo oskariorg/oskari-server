f0674be88cb4022515f8c743e7b363f1fd23dff3
Huotarinen Ville <ville.huotarinen@maanmittauslaitos.fi>
2022-04-20 15:15:13 +0300

    Switch tabs to spaces and remove getDomains function

--- service-capabilities/src/main/java/org/oskari/capabilities/MetadataHelper.java
+++ service-capabilities/src/main/java/org/oskari/capabilities/MetadataHelper.java
@@ -13,78 +13,64 @@ import java.util.List;
 import java.util.Map;
 
 public class MetadataHelper {
-	private static final Logger LOG = LogFactory.getLogger(MetadataHelper.class);
-	/**
-	 * Helper for parsing metadata uuid from url.
-	 * @param url
-	 * @return
-	 */
-	public static String getIdFromMetadataUrl(String url) {
-		if (url == null || url.isEmpty()) {
-			return null;
-		}
-		if (!url.toLowerCase().startsWith("http")) {
-			// not a url -> return as is
-			return url;
-		}
+    private static final Logger LOG = LogFactory.getLogger(MetadataHelper.class);
+    /**
+     * Helper for parsing metadata uuid from url.
+     * @param url
+     * @return
+     */
+    public static String getIdFromMetadataUrl(String url) {
+        if (url == null || url.isEmpty()) {
+            return null;
+        }
+        if (!url.toLowerCase().startsWith("http")) {
+            // not a url -> return as is
+            return url;
+        }
 
-		ArrayList<String> allowedDomains = new ArrayList<String>(Arrays.asList(PropertyUtil.getCommaSeparatedList("service.metadata.domains")));
-		allowedDomains.add(getDomainName(PropertyUtil.get("service.metadata.url")));
-		
+        ArrayList<String> allowedDomains = new ArrayList<String>(Arrays.asList(PropertyUtil.getCommaSeparatedList("service.metadata.domains")));
+        allowedDomains.add(PropertyUtil.get("service.metadata.url"));
 
-		// check if allowedDomains arraylist contains the metadata url
-		if (!isDomainAllowed(url, allowedDomains)) {
-			return null;
-		}
+        // check if allowedDomains arraylist contains the metadata url
+        if (!isDomainAllowed(url, allowedDomains)) {
+            return null;
+        }
 
-		try {
-			Map<String, List<String>> params = IOHelper.parseQuerystring(url);
-			String idParam = params.keySet().stream()
-					.filter(key -> "uuid".equalsIgnoreCase(key) || "id".equalsIgnoreCase(key))
-					.findFirst()
-					.orElse(null);
-			if (idParam == null) {
-				// param not in url
-				return null;
-			}
-			List<String> values = params.getOrDefault(idParam, Collections.emptyList());
-			if (values.isEmpty()) {
-				// param was present but has no value
-				return null;
-			}
-			return values.get(0);
-		} catch (Exception ignored) {
-			// propably just not valid URL
-			LOG.ignore("Unexpected error parsing metadataid", ignored);
-		}
-		LOG.debug("Couldn't parse uuid from metadata url:", url);
-		return null;
-	}
+        try {
+            Map<String, List<String>> params = IOHelper.parseQuerystring(url);
+            String idParam = params.keySet().stream()
+                    .filter(key -> "uuid".equalsIgnoreCase(key) || "id".equalsIgnoreCase(key))
+                    .findFirst()
+                    .orElse(null);
+            if (idParam == null) {
+                // param not in url
+                return null;
+            }
+            List<String> values = params.getOrDefault(idParam, Collections.emptyList());
+            if (values.isEmpty()) {
+                // param was present but has no value
+                return null;
+            }
+            return values.get(0);
+        } catch (Exception ignored) {
+            // propably just not valid URL
+            LOG.ignore("Unexpected error parsing metadataid", ignored);
+        }
+        LOG.debug("Couldn't parse uuid from metadata url:", url);
+        return null;
+    }
 
-	public static boolean isDomainAllowed(String url, List<String> allowedDomains) {	
-		try {	
-			URI uri = new URI(url);
-			for (String a : allowedDomains) {
-				if (uri.getHost().endsWith(a)) {
-					return true;
-				}
-			} 
-		} catch (Exception e) {
-			LOG.debug("Unexpected error converting url-string to uri:", e);
-		}
-		return false;
-	}
-
-	public static String getDomainName(String url) {
-		try {	
-			URI uri = new URI(url);
-			String domain = uri.getHost();
-			return domain.startsWith("www.") ? domain.substring(4) : domain;
-
-		} catch (Exception e) {
-			LOG.debug("Unexpected error converting url-string to uri:", e);
-		}
-		LOG.debug("Couldn't convert url-string to uri:", url);
-		return null;
-	}
+    public static boolean isDomainAllowed(String url, List<String> allowedDomains) {	
+        try {	
+            URI uri = new URI(url);
+            for (String a : allowedDomains) {
+                if (uri.getHost().endsWith(a)) {
+                    return true;
+                }
+            } 
+        } catch (Exception e) {
+            LOG.debug("Unexpected error converting url-string to uri:", e);
+        }
+        return false;
+    }
 }
--- service-capabilities/src/test/java/org/oskari/capabilities/MetadataHelperTest.java
+++ service-capabilities/src/test/java/org/oskari/capabilities/MetadataHelperTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import fi.nls.oskari.util.PropertyUtil;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -15,39 +16,41 @@ import static org.junit.Assert.*;
 
 public class MetadataHelperTest {
 
-	@BeforeClass
-	public static void setup() throws Exception {
-		PropertyUtil.addProperty("service.metadata.domains", "paikkatietohakemisto.fi, mydomain.org", true);
-		PropertyUtil.addProperty("service.metadata.url", "http://www.mydomain2.org", true);
-	}
-	
-	@AfterClass
-	public static void tearDown() {
-		PropertyUtil.clearProperties();
-	}
+    @BeforeClass
+    public static void setup() throws Exception {
+        PropertyUtil.addProperty("service.metadata.domains", "paikkatietohakemisto.fi, mydomain.org", true);
+        PropertyUtil.addProperty("service.metadata.url", "http://propertyurl.org", true);
+    }
 
-	@Test
-	public void testGetIdFromMetadataUrl() {
-		assertNull("Null metadata returns null id", MetadataHelper.getIdFromMetadataUrl(null));
-		assertNull("Empty metadata returns null id", MetadataHelper.getIdFromMetadataUrl(""));
-		assertEquals("Non http-starting metadata returns as is", "testing", MetadataHelper.getIdFromMetadataUrl("testing"));
-		assertNull("Url without querystring returns null id", MetadataHelper.getIdFromMetadataUrl("http://mydomain.org"));
-		assertNull("Url without id|uuid returns null id", MetadataHelper.getIdFromMetadataUrl("http://mydomain.org?my=key"));
-		assertEquals("Url with id returns id value simple", "key", MetadataHelper.getIdFromMetadataUrl("http://mydomain.org?uuid=key"));
-		Map<String, String> expected = new HashMap<>();
-		expected.put("http://mydomain.org?test=test&id=key&post=test", "key");
-		expected.put("http://mydomain.org?test=test&uuid=key2&post=test", "key2");
-		expected.put("http://mydomain.org?test=test&Id=key&post=test", "key");
-		expected.put("http://mydomain.org?test=test&uuId=Key&post=test", "Key");
-		for (String url : expected.keySet()) {
-			assertEquals("Url with id returns id value", expected.get(url), MetadataHelper.getIdFromMetadataUrl(url));
-		}
-	}
-	
-	@Test
-	public void testIsDomainAllowed() {
-		List<String> allowedDomains = Arrays.asList(new String[]{"paikkatietohakemisto.fi", "mydomain.org"});
-		assertTrue("Url that is included in the url-array returns true", MetadataHelper.isDomainAllowed("http://www.paikkatietohakemisto.fi?uuid=key", allowedDomains));
-		assertFalse("Url that is not included in the url-array returns false", MetadataHelper.isDomainAllowed("http://www.unallowed.fi?uuid=key", allowedDomains));
-	}
+    @AfterClass
+    public static void tearDown() {
+        PropertyUtil.clearProperties();
+    }
+
+    @Test
+    public void testGetIdFromMetadataUrl() {
+        assertNull("Null metadata returns null id", MetadataHelper.getIdFromMetadataUrl(null));
+        assertNull("Empty metadata returns null id", MetadataHelper.getIdFromMetadataUrl(""));
+        assertEquals("Non http-starting metadata returns as is", "testing", MetadataHelper.getIdFromMetadataUrl("testing"));
+        assertNull("Url without querystring returns null id", MetadataHelper.getIdFromMetadataUrl("http://mydomain.org"));
+        assertNull("Url without id|uuid returns null id", MetadataHelper.getIdFromMetadataUrl("http://mydomain.org?my=key"));
+        assertEquals("Url with id returns id value simple", "key", MetadataHelper.getIdFromMetadataUrl("http://mydomain.org?uuid=key"));
+        Map<String, String> expected = new HashMap<>();
+        expected.put("http://mydomain.org?test=test&id=key&post=test", "key");
+        expected.put("http://mydomain.org?test=test&uuid=key2&post=test", "key2");
+        expected.put("http://mydomain.org?test=test&Id=key&post=test", "key");
+        expected.put("http://mydomain.org?test=test&uuId=Key&post=test", "Key");
+        for (String url : expected.keySet()) {
+            assertEquals("Url with id returns id value", expected.get(url), MetadataHelper.getIdFromMetadataUrl(url));
+        }
+    }
+
+    @Test
+    public void testIsDomainAllowed() {
+        ArrayList<String> allowedDomains = new ArrayList<String>();
+        allowedDomains.add("paikkatietohakemisto.fi");
+        allowedDomains.add("mydomain.org");
+        assertTrue("Url that is included in the url-array returns true", MetadataHelper.isDomainAllowed("http://www.paikkatietohakemisto.fi?uuid=key", allowedDomains));
+        assertFalse("Url that is not included in the url-array returns false", MetadataHelper.isDomainAllowed("http://www.unallowed.fi?uuid=key", allowedDomains));
+    }
 }
