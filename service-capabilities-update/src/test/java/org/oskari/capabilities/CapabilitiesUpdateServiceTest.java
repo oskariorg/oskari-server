81de96d590a3beea12b0bb33c0e1b538af428624
Sami Mäkinen <sami.makinen@nls.fi>
2022-04-26 12:32:10 +0300

    Fix test case for automatic WFS updates

--- service-capabilities-update/src/test/java/org/oskari/capabilities/CapabilitiesUpdateServiceTest.java
+++ service-capabilities-update/src/test/java/org/oskari/capabilities/CapabilitiesUpdateServiceTest.java
@@ -13,7 +13,7 @@ public class CapabilitiesUpdateServiceTest {
     public void testCanUpdate() {
         assertTrue("Should allow WMTS", CapabilitiesUpdateService.canUpdate(OskariLayer.TYPE_WMTS));
         assertTrue("Should allow WMS", CapabilitiesUpdateService.canUpdate(OskariLayer.TYPE_WMS));
-        assertFalse("Should NOT allow WFS", CapabilitiesUpdateService.canUpdate(OskariLayer.TYPE_WFS));
+        assertTrue("Should allow WFS", CapabilitiesUpdateService.canUpdate(OskariLayer.TYPE_WFS));
         assertFalse("Should NOT allow analysis", CapabilitiesUpdateService.canUpdate(OskariLayer.TYPE_ANALYSIS));
         assertFalse("Should NOT allow argcis93", CapabilitiesUpdateService.canUpdate(OskariLayer.TYPE_ARCGIS93));
         assertFalse("Should NOT allow stats", CapabilitiesUpdateService.canUpdate(OskariLayer.TYPE_STATS));
