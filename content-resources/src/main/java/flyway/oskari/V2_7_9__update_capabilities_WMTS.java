253048c0ae1192a0bf145fdd924e9157e6d00373
Sami Mäkinen <sami.makinen@nls.fi>
2022-05-05 16:08:31 +0300

    Improve comment

--- content-resources/src/main/java/flyway/oskari/V2_7_9__update_capabilities_WMTS.java
+++ content-resources/src/main/java/flyway/oskari/V2_7_9__update_capabilities_WMTS.java
@@ -8,8 +8,7 @@ import org.flywaydb.core.api.migration.Context;
 public class V2_7_9__update_capabilities_WMTS extends V2_7_0__update_WMTS_capabilities {
     @Override
     public void migrate(Context context) throws Exception {
-        // There's a newer migration that will populate WMTS-capabilities (2.7.0)
-        // no need to do it twice
+        // NO-OP since 2.7.12 updates wmts-layers
     }
 
 }
