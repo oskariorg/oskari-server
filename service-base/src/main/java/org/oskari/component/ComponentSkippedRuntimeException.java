02154f68126d6034ccc9f1adbff09348ef17da65
Sami Mäkinen <sami.makinen@nls.fi>
2022-10-11 14:50:16 +0300

    Fix formatting

--- service-base/src/main/java/org/oskari/component/ComponentSkippedRuntimeException.java
+++ service-base/src/main/java/org/oskari/component/ComponentSkippedRuntimeException.java
@@ -3,10 +3,9 @@ package org.oskari.component;
 import fi.nls.oskari.service.ServiceRuntimeException;
 
 public class ComponentSkippedRuntimeException extends ServiceRuntimeException {
+    private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
-
-	public ComponentSkippedRuntimeException(final String message, final Throwable e) {
+    public ComponentSkippedRuntimeException(final String message, final Throwable e) {
         super(message, e);
     }
 
