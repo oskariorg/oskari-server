9bd8e4016b4da0cf5066ab10909d6151170df65c
Sami Mäkinen <sami.makinen@nls.fi>
2023-04-21 15:50:00 +0300

    Add info about auto completion support for options

--- control-base/src/main/java/fi/nls/oskari/control/data/SearchOptionsHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/data/SearchOptionsHandler.java
@@ -6,6 +6,7 @@ import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.domain.User;
+import fi.nls.oskari.search.channel.SearchAutocomplete;
 import fi.nls.oskari.search.channel.SearchableChannel;
 import fi.nls.oskari.service.OskariComponentManager;
 import fi.nls.oskari.util.JSONHelper;
@@ -52,6 +53,7 @@ public class SearchOptionsHandler extends ActionHandler {
             JSONObject json = new JSONObject();
             JSONHelper.putValue(json, "id", channel.getId());
             JSONHelper.putValue(json, "isDefault", channel.isDefaultChannel());
+            JSONHelper.putValue(json, "isSuggestions", channel instanceof SearchAutocomplete);
             JSONHelper.putValue(json, "locale", channel.getUILabels().optJSONObject(params.getLocale().getLanguage()));
             channelsJSONArray.put(json);
         }
