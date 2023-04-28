a98cd6d4bfc6d63e5dd7e403f365828ec427af8c
okauppinen <olli.kauppinen@nls.fi>
2023-04-28 11:33:44 +0300

    fallback to wfs options default style

--- service-map/src/main/java/fi/nls/oskari/map/style/VectorStyleService.java
+++ service-map/src/main/java/fi/nls/oskari/map/style/VectorStyleService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import fi.nls.oskari.domain.User;
 import fi.nls.oskari.domain.map.style.VectorStyle;
+import fi.nls.oskari.domain.map.wfs.WFSLayerOptions;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.service.OskariComponent;
@@ -31,7 +32,7 @@ public abstract class VectorStyleService extends OskariComponent  {
         VectorStyle defaultStyle = getDefaultStyle();
         if (defaultStyle == null) {
             log.error("Can't find default vector style");
-            return new JSONObject();
+            return WFSLayerOptions.getDefaultOskariStyle();
         }
         return JSONHelper.getJSONObject(defaultStyle.getStyle(), KEY_FEATURE_STYLE);
     }
