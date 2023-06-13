319d889a8069609b9ee74db84c86007cc982e78d
Sami Mäkinen <sami.makinen@nls.fi>
2023-06-13 16:50:32 +0300

    Remove wildcard mapping as its not working with resourcehandlers

--- servlet-map/src/main/java/fi/nls/oskari/spring/GlobalExceptionHandlerController.java
+++ servlet-map/src/main/java/fi/nls/oskari/spring/GlobalExceptionHandlerController.java
@@ -1,27 +1,25 @@
 package fi.nls.oskari.spring;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
-
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class GlobalExceptionHandlerController {
     /**
      * Catch all that didn't match
      * */
+/*
+    This prevents resources configured in SpringConfig.addResourceHandlers() to be called.
+    We should find another way to do this.
     @GetMapping("/**")
-    @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
     public String handle(HttpServletResponse resp) {
         resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         return "error/404";
     }
+*/
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handle(NoHandlerFoundException ex) {
