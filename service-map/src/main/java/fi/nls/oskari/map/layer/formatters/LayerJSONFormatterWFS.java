7b8430db813fbff66824017336a59bcb3b5a070c
okauppinen <olli.kauppinen@nls.fi>
2023-08-08 11:26:16 +0300

    remove options and attributes from WFS json

--- service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterWFS.java
+++ service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterWFS.java
@@ -18,6 +18,9 @@ public class LayerJSONFormatterWFS extends LayerJSONFormatter {
 
         final JSONObject layerJson = getBaseJSON(layer, lang, isSecure, crs);
         JSONHelper.putValue(layerJson, KEY_ISQUERYABLE, true);
+        // getBaseJSON adds these but model builder isn't using them. Frontend uses DescribeLayer response
+        layerJson.remove("attributes");
+        layerJson.remove("options");
         return layerJson;
     }
 }
