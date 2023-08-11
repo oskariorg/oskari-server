86db73e49891047c2e007dda03aab735224a8cae
okauppinen <olli.kauppinen@nls.fi>
2023-08-11 09:37:45 +0300

    add attention_text to props

--- service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterMYPLACES.java
+++ service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterMYPLACES.java
@@ -10,6 +10,8 @@ import org.json.JSONObject;
 import fi.nls.oskari.domain.map.MyPlaceCategory;
 import fi.nls.oskari.domain.map.OskariLayer;
 
+import java.util.List;
+
 /**
  * MyPlaces category to Oskari layer JSON
  */
@@ -35,12 +37,19 @@ public class LayerJSONFormatterMYPLACES extends LayerJSONFormatterUSERDATA {
         JSONArray props = new JSONArray();
         JSONObject format = wfsAttr.getFieldFormatMetadata().orElse(new JSONObject());
         JSONObject locale = wfsAttr.getLocalization(lang).orElse(new JSONObject());
+        // not visible/selected properties should be found from filter, format and/or locale
+        // MyPlaces has hard coded values (columns in db and frontend), so just add to list
+        List<String> selected = wfsAttr.getSelectedAttributes(lang);
+        selected.add("attention_text");
         wfsAttr.getSelectedAttributes(lang).stream().forEach(name -> {
             JSONObject prop = JSONHelper.createJSONObject("name", name);
             JSONHelper.putValue(prop, "type", "string");
             JSONHelper.putValue(prop, "rawType", "String");
             JSONHelper.putValue(prop, "label", locale.optString(name, null));
             JSONHelper.putValue(prop, "format", JSONHelper.getJSONObject(format, name));
+            if (name == "attention_text") {
+                JSONHelper.putValue(prop, "hidden", true);
+            }
             props.put(prop);
         });
         props.put(DEAULT_GEOMETRY_PROPERTY);
