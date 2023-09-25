9a7dd930910fce68bed98682d58351eb69772fc6
Sami Mäkinen <sami.makinen@nls.fi>
2023-09-25 13:14:44 +0300

    Rename to next available migration version

--- content-resources/src/main/java/flyway/oskari/V2_13_1__register_metadatasearch_bundle.java
+++ content-resources/src/main/java/flyway/oskari/V2_13_1__register_metadatasearch_bundle.java
@@ -9,7 +9,7 @@ import java.sql.Connection;
 /**
  * Register metadatasearch bundle (React impl) as intended replacement for metadatacatalogue bundle (jQuery impl).
  */
-public class V2_13_0__register_metadatasearch_bundle extends BaseJavaMigration {
+public class V2_13_1__register_metadatasearch_bundle extends BaseJavaMigration {
 
     public void migrate(Context context) throws Exception {
         Connection connection = context.getConnection();
