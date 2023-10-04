6e448ed29993b778ee55ed7944d2dad91eb39641
Sami Mäkinen <sami.makinen@nls.fi>
2023-10-04 18:11:55 +0300

    User permission helper to get layer instead of direct db query

--- control-base/src/main/java/fi/nls/oskari/control/data/GetGeoPointDataHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/data/GetGeoPointDataHandler.java
@@ -4,6 +4,7 @@ import fi.nls.oskari.annotation.OskariActionRoute;
 import fi.nls.oskari.control.ActionException;
 import fi.nls.oskari.control.ActionHandler;
 import fi.nls.oskari.control.ActionParameters;
+import fi.nls.oskari.control.layer.PermissionHelper;
 import fi.nls.oskari.domain.map.OskariLayer;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
@@ -11,18 +12,19 @@ import fi.nls.oskari.map.data.domain.GFIRequestParams;
 import fi.nls.oskari.map.data.domain.GFIRestQueryParams;
 import fi.nls.oskari.map.data.service.GetGeoPointDataService;
 import fi.nls.oskari.map.layer.OskariLayerService;
-import fi.nls.oskari.map.layer.OskariLayerServiceMybatisImpl;
+import fi.nls.oskari.service.OskariComponentManager;
 import fi.nls.oskari.util.ConversionHelper;
 import fi.nls.oskari.util.ResponseHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.oskari.permissions.PermissionService;
+
 import static fi.nls.oskari.control.ActionConstants.*;
 
 @OskariActionRoute("GetFeatureInfoWMS")
 public class GetGeoPointDataHandler extends ActionHandler {
-
-	private final OskariLayerService mapLayerService = new OskariLayerServiceMybatisImpl();
+	private PermissionHelper permissionHelper;
 	private final GetGeoPointDataService geoPointService = new GetGeoPointDataService();
 
 	private Logger log = LogFactory.getLogger(GetGeoPointDataHandler.class);
@@ -35,6 +37,15 @@ public class GetGeoPointDataHandler extends ActionHandler {
     private static final String PARAM_HEIGHT = "height";
     private static final String PARAM_ZOOM = "zoom";
     private static final String PARAM_PARAMS = "params";
+
+	/**
+	 *  Init method
+	 */
+	public void init() {
+		permissionHelper = new PermissionHelper(
+				OskariComponentManager.getComponentOfType(OskariLayerService.class),
+				OskariComponentManager.getComponentOfType(PermissionService.class));
+	}
 
 	@Override
     public void handleAction(final ActionParameters params) throws ActionException {
@@ -57,21 +68,29 @@ public class GetGeoPointDataHandler extends ActionHandler {
                 continue;
 			}
 
-			final OskariLayer layer = mapLayerService.find(layerId);
-			final String layerType = layer.getType();
+			// returns a layer or throws appropriate ActionException.
+			// User shouldn't be able to create queries that generate error, but a hand-crafted one could
+			// -> we want to log this by throwing an exception
+			OskariLayer layer = permissionHelper.getLayer(layerId, params.getUser(), false);
+			String layerType = layer.getType();
+			GFIRequestParams gfiParams = getParams(layerType);
+			if (gfiParams == null) {
+				// unsupported layer type
+				continue;
+			}
+			gfiParams.setBbox(params.getRequiredParam(PARAM_BBOX));
+			gfiParams.setLat(lat);
+			gfiParams.setLayer(layer);
+			gfiParams.setLon(lon);
+			gfiParams.setSRSName(srs);
 
 			if (OskariLayer.TYPE_WMS.equals(layerType)) {
-			    final GFIRequestParams gfiParams = new GFIRequestParams();
-			    gfiParams.setBbox(params.getRequiredParam(PARAM_BBOX));
+				// additional wms params
 			    gfiParams.setHeight(params.getHttpParam(PARAM_HEIGHT));
-			    gfiParams.setLat(lat);
-			    gfiParams.setLayer(layer);
-			    gfiParams.setLon(lon);
 			    gfiParams.setWidth(params.getHttpParam(PARAM_WIDTH));
 			    gfiParams.setX(params.getHttpParam(PARAM_X));
 			    gfiParams.setY(params.getHttpParam(PARAM_Y));
 			    gfiParams.setZoom(zoom);
-                gfiParams.setSRSName(srs);
                 gfiParams.setAdditionalParams(allLayerAdditionalParams.optJSONObject(id));
 			    
 			    final JSONObject response = geoPointService.getWMSFeatureInfo(gfiParams);
@@ -79,16 +98,7 @@ public class GetGeoPointDataHandler extends ActionHandler {
                     data.put(response);
                 }
 			} else if (OskariLayer.TYPE_ARCGIS93.equals(layerType)) {
-				final GFIRestQueryParams gfiParams = new GFIRestQueryParams();
-
-				gfiParams.setBbox(params.getRequiredParam(PARAM_BBOX));
-				gfiParams.setLat(lat);
-				gfiParams.setLayer(layer);
-				gfiParams.setLon(lon);
-
-				gfiParams.setSRSName(srs);
-
-				final JSONObject response = geoPointService.getRESTFeatureInfo(gfiParams);
+				final JSONObject response = geoPointService.getRESTFeatureInfo((GFIRestQueryParams) gfiParams);
 				if (response != null) {
 					data.put(response);
 				}
@@ -103,6 +113,15 @@ public class GetGeoPointDataHandler extends ActionHandler {
 		} catch (JSONException je) {
 		    throw new ActionException("Could not populate GFI JSON: " + log.getAsString(data), je);
 		}
+	}
+
+	private GFIRequestParams getParams(String layerType) {
+		if (OskariLayer.TYPE_WMS.equals(layerType)) {
+			return new GFIRequestParams();
+		} else if (OskariLayer.TYPE_ARCGIS93.equals(layerType)) {
+			return new GFIRestQueryParams();
+		}
+		return null;
 	}
 
 	private JSONObject getAllLayerAdditionalParams(final ActionParameters params) {
