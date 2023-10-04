931145dc69ec19c8dd6875ce981348222defebb7
Sami Mäkinen <sami.makinen@nls.fi>
2023-10-04 18:14:18 +0300

    Format file to get rid of tab indents

--- control-base/src/main/java/fi/nls/oskari/control/data/GetGeoPointDataHandler.java
+++ control-base/src/main/java/fi/nls/oskari/control/data/GetGeoPointDataHandler.java
@@ -24,10 +24,10 @@ import static fi.nls.oskari.control.ActionConstants.*;
 
 @OskariActionRoute("GetFeatureInfoWMS")
 public class GetGeoPointDataHandler extends ActionHandler {
-	private PermissionHelper permissionHelper;
-	private final GetGeoPointDataService geoPointService = new GetGeoPointDataService();
+    private PermissionHelper permissionHelper;
+    private final GetGeoPointDataService geoPointService = new GetGeoPointDataService();
 
-	private Logger log = LogFactory.getLogger(GetGeoPointDataHandler.class);
+    private final Logger log = LogFactory.getLogger(GetGeoPointDataHandler.class);
 
     private static final String PARAM_LAYERS = "layerIds";
     private static final String PARAM_X = "x";
@@ -38,98 +38,96 @@ public class GetGeoPointDataHandler extends ActionHandler {
     private static final String PARAM_ZOOM = "zoom";
     private static final String PARAM_PARAMS = "params";
 
-	/**
-	 *  Init method
-	 */
-	public void init() {
-		permissionHelper = new PermissionHelper(
-				OskariComponentManager.getComponentOfType(OskariLayerService.class),
-				OskariComponentManager.getComponentOfType(PermissionService.class));
-	}
+    @Override
+    public void init() {
+        permissionHelper = new PermissionHelper(
+                OskariComponentManager.getComponentOfType(OskariLayerService.class),
+                OskariComponentManager.getComponentOfType(PermissionService.class));
+    }
 
-	@Override
+    @Override
     public void handleAction(final ActionParameters params) throws ActionException {
-	     
-		final String layerIds = params.getRequiredParam(PARAM_LAYERS);
-		final String[] layerIdsArr = layerIds.split(",");
-		final JSONObject allLayerAdditionalParams = getAllLayerAdditionalParams(params);
+
+        final String layerIds = params.getRequiredParam(PARAM_LAYERS);
+        final String[] layerIdsArr = layerIds.split(",");
+        final JSONObject allLayerAdditionalParams = getAllLayerAdditionalParams(params);
 
         final double lat = ConversionHelper.getDouble(params.getHttpParam(PARAM_LAT), -1);
         final double lon = ConversionHelper.getDouble(params.getHttpParam(PARAM_LON), -1);
         final int zoom = ConversionHelper.getInt(params.getHttpParam(PARAM_ZOOM), 0);
-        
-        final JSONArray data = new JSONArray();
-		final String srs = params.getHttpParam(PARAM_SRS, "EPSG:3067");
 
-		for (String id : layerIdsArr) {
-			final int layerId = ConversionHelper.getInt(id, -1);
-			if (layerId == -1) {
+        final JSONArray data = new JSONArray();
+        final String srs = params.getHttpParam(PARAM_SRS, "EPSG:3067");
+
+        for (String id : layerIdsArr) {
+            final int layerId = ConversionHelper.getInt(id, -1);
+            if (layerId == -1) {
                 log.warn("Couldn't parse layer id", id);
                 continue;
-			}
+            }
 
-			// returns a layer or throws appropriate ActionException.
-			// User shouldn't be able to create queries that generate error, but a hand-crafted one could
-			// -> we want to log this by throwing an exception
-			OskariLayer layer = permissionHelper.getLayer(layerId, params.getUser(), false);
-			String layerType = layer.getType();
-			GFIRequestParams gfiParams = getParams(layerType);
-			if (gfiParams == null) {
-				// unsupported layer type
-				continue;
-			}
-			gfiParams.setBbox(params.getRequiredParam(PARAM_BBOX));
-			gfiParams.setLat(lat);
-			gfiParams.setLayer(layer);
-			gfiParams.setLon(lon);
-			gfiParams.setSRSName(srs);
+            // returns a layer or throws appropriate ActionException.
+            // User shouldn't be able to create queries that generate error, but a hand-crafted one could
+            // -> we want to log this by throwing an exception
+            OskariLayer layer = permissionHelper.getLayer(layerId, params.getUser(), false);
+            String layerType = layer.getType();
+            GFIRequestParams gfiParams = getParams(layerType);
+            if (gfiParams == null) {
+                // unsupported layer type
+                continue;
+            }
+            gfiParams.setBbox(params.getRequiredParam(PARAM_BBOX));
+            gfiParams.setLat(lat);
+            gfiParams.setLayer(layer);
+            gfiParams.setLon(lon);
+            gfiParams.setSRSName(srs);
 
-			if (OskariLayer.TYPE_WMS.equals(layerType)) {
-				// additional wms params
-			    gfiParams.setHeight(params.getHttpParam(PARAM_HEIGHT));
-			    gfiParams.setWidth(params.getHttpParam(PARAM_WIDTH));
-			    gfiParams.setX(params.getHttpParam(PARAM_X));
-			    gfiParams.setY(params.getHttpParam(PARAM_Y));
-			    gfiParams.setZoom(zoom);
+            if (OskariLayer.TYPE_WMS.equals(layerType)) {
+                // additional wms params
+                gfiParams.setHeight(params.getHttpParam(PARAM_HEIGHT));
+                gfiParams.setWidth(params.getHttpParam(PARAM_WIDTH));
+                gfiParams.setX(params.getHttpParam(PARAM_X));
+                gfiParams.setY(params.getHttpParam(PARAM_Y));
+                gfiParams.setZoom(zoom);
                 gfiParams.setAdditionalParams(allLayerAdditionalParams.optJSONObject(id));
-			    
-			    final JSONObject response = geoPointService.getWMSFeatureInfo(gfiParams);
+
+                final JSONObject response = geoPointService.getWMSFeatureInfo(gfiParams);
                 if (response != null) {
                     data.put(response);
                 }
-			} else if (OskariLayer.TYPE_ARCGIS93.equals(layerType)) {
-				final JSONObject response = geoPointService.getRESTFeatureInfo((GFIRestQueryParams) gfiParams);
-				if (response != null) {
-					data.put(response);
-				}
-			}
-		}
+            } else if (OskariLayer.TYPE_ARCGIS93.equals(layerType)) {
+                final JSONObject response = geoPointService.getRESTFeatureInfo((GFIRestQueryParams) gfiParams);
+                if (response != null) {
+                    data.put(response);
+                }
+            }
+        }
 
-		try {
-	        final JSONObject rootJson = new JSONObject();
+        try {
+            final JSONObject rootJson = new JSONObject();
             rootJson.put("data", data);
-			rootJson.put("layerCount", data.length());
-	        ResponseHelper.writeResponse(params, rootJson);
-		} catch (JSONException je) {
-		    throw new ActionException("Could not populate GFI JSON: " + log.getAsString(data), je);
-		}
-	}
+            rootJson.put("layerCount", data.length());
+            ResponseHelper.writeResponse(params, rootJson);
+        } catch (JSONException je) {
+            throw new ActionException("Could not populate GFI JSON: " + log.getAsString(data), je);
+        }
+    }
 
-	private GFIRequestParams getParams(String layerType) {
-		if (OskariLayer.TYPE_WMS.equals(layerType)) {
-			return new GFIRequestParams();
-		} else if (OskariLayer.TYPE_ARCGIS93.equals(layerType)) {
-			return new GFIRestQueryParams();
-		}
-		return null;
-	}
+    private GFIRequestParams getParams(String layerType) {
+        if (OskariLayer.TYPE_WMS.equals(layerType)) {
+            return new GFIRequestParams();
+        } else if (OskariLayer.TYPE_ARCGIS93.equals(layerType)) {
+            return new GFIRestQueryParams();
+        }
+        return null;
+    }
 
-	private JSONObject getAllLayerAdditionalParams(final ActionParameters params) {
-		try {
-			return new JSONObject(params.getHttpParam(PARAM_PARAMS, "{}"));
-		} catch (JSONException e) {
-			log.warn("Couldn't parse params from POST request", e);
-			return new JSONObject();
-		}
-	}
+    private JSONObject getAllLayerAdditionalParams(final ActionParameters params) {
+        try {
+            return new JSONObject(params.getHttpParam(PARAM_PARAMS, "{}"));
+        } catch (JSONException e) {
+            log.warn("Couldn't parse params from POST request", e);
+            return new JSONObject();
+        }
+    }
 }
