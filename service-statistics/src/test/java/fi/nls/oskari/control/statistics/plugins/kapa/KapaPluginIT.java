a474139af1f18b763f42c200f96117158e90b5a3
Sami Mäkinen <sami.makinen@nls.fi>
2023-11-03 16:00:24 +0200

    Remove ref to BasicDataSource

--- service-statistics/src/test/java/fi/nls/oskari/control/statistics/plugins/kapa/KapaPluginIT.java
+++ service-statistics/src/test/java/fi/nls/oskari/control/statistics/plugins/kapa/KapaPluginIT.java
@@ -3,7 +3,7 @@ package fi.nls.oskari.control.statistics.plugins.kapa;
 import fi.nls.oskari.control.statistics.data.*;
 import fi.nls.oskari.control.statistics.plugins.*;
 import fi.nls.oskari.control.statistics.plugins.db.StatisticalDatasource;
-import org.apache.commons.dbcp2.BasicDataSource;
+import fi.nls.test.util.TestHelper;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,6 +27,7 @@ import java.io.InputStream;
 import java.io.StringBufferInputStream;
 import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -51,25 +52,10 @@ public class KapaPluginIT {
     private static String testIndicatorDataResponse = ResourceHelper.readStringResource("KapaIndicatorData.json",
             KapaPluginIT.class);
     private static String url = "";
-
-    public static class DatasourceHelperMock extends DatasourceHelper {
-        public DatasourceHelperMock() {
-            super();
-        }
-        @Override
-        public DataSource getDataSource(String name) {
-            BasicDataSource basicDataSource = new BasicDataSource();
-            basicDataSource.setDriverClassName("org.postgresql.Driver");
-            basicDataSource.setUrl(PropertyUtil.get("db.url"));
-            basicDataSource.setUsername(PropertyUtil.get("db.username"));
-            basicDataSource.setPassword(PropertyUtil.get("db.password"));
-            return basicDataSource;
-        }
-    }
     
     @BeforeClass
-    public static void init() throws IOException, IllegalArgumentException, IllegalAccessException {
-        PropertyUtil.loadProperties("/oskari-ext.properties");
+    public static void init() throws IOException, IllegalArgumentException, SQLException {
+        TestHelper.registerTestDataSource();
         PowerMockito.mockStatic(IOHelper.class);
         when(IOHelper.getConnection(any(String.class))).then(new Answer<HttpURLConnection>() {
             @Override
@@ -109,8 +95,6 @@ public class KapaPluginIT {
             }
             
         });
-        Field field = PowerMockito.field(DatasourceHelper.class, "INSTANCE");
-        field.set(DatasourceHelper.class, new DatasourceHelperMock());
     }
     @AfterClass
     public static void tearDown() {
@@ -118,8 +102,7 @@ public class KapaPluginIT {
     }
 
     @Test(timeout=120000)
-    public void testKapaPlugin()
-            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public void testKapaPlugin() {
         manager.reset();
         StatisticalDatasource source = new StatisticalDatasource();
         source.setId(1);
