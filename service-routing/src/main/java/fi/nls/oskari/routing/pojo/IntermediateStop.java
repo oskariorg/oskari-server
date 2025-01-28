375e4723ece1b2c8121cb617a629ec9f19635825
Oskari <oskari.rintamaki@sitowise.com>
2025-01-28 16:37:46 +0200

    JAVAX XML BIND AND ANNOTATION TO JAKARTA

--- service-routing/src/main/java/fi/nls/oskari/routing/pojo/IntermediateStop.java
+++ service-routing/src/main/java/fi/nls/oskari/routing/pojo/IntermediateStop.java
@@ -2,7 +2,7 @@ package fi.nls.oskari.routing.pojo;
 
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Generated;
+import jakarta.annotation.Generated;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
