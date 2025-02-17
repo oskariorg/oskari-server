93e777c7a09be21066d0f2d15e96966f3474a869
Sami Mäkinen <sami.makinen@nls.fi>
2025-02-17 18:49:59 +0200

    Move most of SQLs from XML to Java-annotations while removing problematic java.util.Map as parameterType

--- service-map/src/main/java/fi/nls/oskari/map/view/AppSetupMapper.java
+++ service-map/src/main/java/fi/nls/oskari/map/view/AppSetupMapper.java
@@ -8,14 +8,46 @@ import java.util.List;
 import java.util.Map;
 
 public interface AppSetupMapper {
+
     @Select("SELECT MAX(id) FROM oskari_appsetup WHERE is_default = TRUE AND type = #{type}")
     Long getDefaultViewId(String type);
-    List<View> getViews(final Map<String, Object> params);
+
+    @Select("SELECT id, name, description, type, uuid, only_uuid, application, page, creator, is_public, is_default, domain, lang, metadata, created, updated" +
+            " FROM oskari_appsetup" +
+            " ORDER BY created ASC" +
+            " OFFSET ${offset} limit ${pageSize}")
+    @ResultMap("viewWithConf")
+    List<View> getViews(@Param("offset") int offset, @Param("pageSize") int pageSize);
+
+
+    @Select("SELECT id, name, description, type, uuid, only_uuid, application, page, creator, is_public, is_default, domain, lang, metadata, created, updated" +
+            " FROM oskari_appsetup" +
+            " WHERE id = #{viewId}")
+    @ResultMap("viewWithConf")
     View getViewWithConfByViewId(long viewId);
+
+    @Select("SELECT id, name, description, type, uuid, only_uuid, application, page, creator, is_public, is_default, domain, lang, metadata, created, updated" +
+            " FROM oskari_appsetup" +
+            " WHERE uuid::text = #{uuId}")
+    @ResultMap("viewWithConf")
     View getViewWithConfByUuId(String uuId);
+
+    // This doesn't have a mapping, FIXME: remove
     View getViewWithConfByOldId(long oldId);
+
+    @Select("SELECT id, name, description, type, uuid, only_uuid, application, page, creator, is_public, is_default, domain, lang, metadata, created, updated" +
+            " FROM oskari_appsetup" +
+            " WHERE name = #{name}")
+    @ResultMap("viewWithConf")
     View getViewWithConfByViewName(String viewName);
+
+    @Select("SELECT id, name, description, type, uuid, only_uuid, application, page, creator, is_public, is_default, domain, lang, metadata, created, updated" +
+            " FROM oskari_appsetup" +
+            " WHERE creator = #{userId}" +
+            " ORDER BY name ASC")
+    @ResultMap("viewWithConf")
     List<View> getViewsWithConfByUserId(long userId);
+
     List<Bundle> getBundlesByViewId(long viewId);
 
     @Insert("INSERT INTO oskari_appsetup ( name, \"description\", type, page, application, " +
@@ -38,10 +70,38 @@ public interface AppSetupMapper {
 
     @Update("UPDATE oskari_appsetup SET is_default = FALSE, updated = NOW() WHERE creator = #{userId} AND is_default = TRUE AND type = 'USER'")
     void resetUsersDefaultViews(long userId);
+
+    @Update("UPDATE oskari_appsetup SET" +
+            " name = #{name}," +
+            " description = #{description}," +
+            " application = #{application}," +
+            " page = #{page}," +
+            " domain = #{pubDomain}," +
+            " lang = #{lang}," +
+            " is_public = #{isPublic}," +
+            " is_default = #{isDefault}," +
+            " metadata = #{metadataAsString}," +
+            " updated = #{updated}" +
+            " WHERE id = #{id}")
     void update(View view);
-    void updateUsage(View view);
+
+    @Update("UPDATE oskari_appsetup SET " +
+            " used=now()," +
+            " usagecount=(SELECT usagecount+1 FROM oskari_appsetup WHERE id=#{id}) " +
+            " WHERE id=#{id}")
+    void updateUsage(long id);
+
+    @Insert("INSERT INTO oskari_appsetup_bundles (appsetup_id, bundle_id, seqno, state, config, bundleinstance)" +
+            " VALUES ( #{viewId}, #{bundleId}, #{seqNo}, #{state}, #{config}, #{bundleinstance})")
     void addBundle(Bundle bundle);
-    int updateBundleSettingsInView(final Map<String, Object> params);
+
+
+    @Update("UPDATE oskari_appsetup_bundles SET" +
+            " config = #{bundle.config}," +
+            " state = #{bundle.state}," +
+            " bundleinstance = #{bundle.bundleinstance}" +
+            " WHERE appsetup_id = #{view_id} AND bundle_id = #{bundle.bundleId}")
+    int updateBundleSettingsInView(@Param("view_id") long viewId, @Param("bundle") Bundle bundle);
 
     @Select("SELECT id FROM oskari_appsetup WHERE is_default = TRUE AND type = 'DEFAULT' AND creator = -1")
     List<Long> getDefaultViewIds();
