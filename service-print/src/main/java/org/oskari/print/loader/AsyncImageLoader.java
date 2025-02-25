ef3bd8c07add2331c05fc90e967eb3d14a7850f7
Olli Kauppinen <olli.kauppinen@nls.fi>
2025-02-25 09:25:11 +0200

    use resilience4j instead of hystrix

--- service-print/src/main/java/org/oskari/print/loader/AsyncImageLoader.java
+++ service-print/src/main/java/org/oskari/print/loader/AsyncImageLoader.java
@@ -1,23 +1,103 @@
 package org.oskari.print.loader;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.function.Supplier;
 
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.service.ServiceRuntimeException;
+import fi.nls.oskari.util.IOHelper;
+import fi.nls.oskari.util.PropertyUtil;
+import io.github.resilience4j.bulkhead.ThreadPoolBulkhead;
+import io.github.resilience4j.bulkhead.ThreadPoolBulkheadConfig;
+import io.github.resilience4j.bulkhead.ThreadPoolBulkheadRegistry;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import io.github.resilience4j.timelimiter.TimeLimiter;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.oskari.print.request.PrintLayer;
 import org.oskari.print.request.PrintRequest;
 
 import fi.nls.oskari.domain.map.OskariLayer;
-import fi.nls.oskari.service.ServiceException;
+import org.oskari.service.wfs.client.OskariFeatureClient;
+
+import javax.imageio.ImageIO;
 
 public class AsyncImageLoader {
+    private static final Logger LOG = LogFactory.getLogger(AsyncImageLoader.class);
+    private static final String GROUP_KEY = "print";
+    private static final String FORMAT = "image/png";
+    private static final int RETRY_COUNT = 3;
+    private static final int SLEEP_BETWEEN_RETRIES_MS = 50;
 
-    public static final String GROUP_KEY = "LoadImageFromURL";
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
+    private final RetryRegistry retryRegistry;
+    private final TimeLimiter timeLimiter;
+    private final ThreadPoolBulkhead bulkhead;
+    private final ScheduledExecutorService executor;
 
-    public static Map<Integer, Future<BufferedImage>> initLayers(PrintRequest request)
-            throws ServiceException {
+    public AsyncImageLoader() {
+        int failRequests = PropertyUtil.getOptional("oskari." + GROUP_KEY + ".failrequests", 5);
+        int rollingwindow = PropertyUtil.getOptional("oskari." + GROUP_KEY + ".rollingwindow", 100000);
+        int waitDuration = PropertyUtil.getOptional("oskari." + GROUP_KEY + ".sleepwindow", 20000);
+        int slidingWindow = failRequests * 2; // failing rate 50%
+
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .waitDurationInOpenState(Duration.ofMillis(waitDuration))
+                .permittedNumberOfCallsInHalfOpenState(failRequests)
+                .minimumNumberOfCalls(slidingWindow)
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
+                .slidingWindowSize(slidingWindow)
+                .build();
+        circuitBreakerRegistry = CircuitBreakerRegistry.of(circuitBreakerConfig);
+
+        RetryConfig retryConfig = RetryConfig.custom()
+                .maxAttempts(RETRY_COUNT)
+                .waitDuration(Duration.ofMillis(SLEEP_BETWEEN_RETRIES_MS))
+                .ignoreExceptions(ServiceRuntimeException.class)
+                .failAfterMaxAttempts(true)
+                .build();
+        retryRegistry = RetryRegistry.of(retryConfig);
+
+        int poolSize = PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.pool.size", 10);
+        int poolLimit = PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.pool.limit", 100);
+        int queueSize = PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.pool.queue", 100);
+        ThreadPoolBulkheadConfig bulkheadConfig = ThreadPoolBulkheadConfig.custom()
+                .maxThreadPoolSize(poolSize)
+                .coreThreadPoolSize(poolSize/2)
+                .queueCapacity(queueSize)
+                .build();
+        ThreadPoolBulkheadRegistry registry = ThreadPoolBulkheadRegistry.of(bulkheadConfig);
+        bulkhead = registry.bulkhead(GROUP_KEY);
+
+        executor = Executors.newScheduledThreadPool(3);
+
+        int timeout = PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.timeoutms", 15000);
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(timeout)).build();
+        timeLimiter = TimeLimiterRegistry.of(timeLimiterConfig).timeLimiter(GROUP_KEY);
+    }
+
+    public Map<Integer, Future<BufferedImage>> initImageLayers(PrintRequest request) {
         final Map<Integer, Future<BufferedImage>> images = new HashMap<>();
 
         final List<PrintLayer> requestedLayers = request.getLayers();
@@ -31,23 +111,91 @@ public class AsyncImageLoader {
         final String srsName = request.getSrsName();
 
         for (PrintLayer layer : requestedLayers) {
+            Supplier<BufferedImage> supplier = null;
             switch (layer.getType()) { 
             case OskariLayer.TYPE_WMS:
-                images.put(layer.getZIndex(), new CommandLoadImageWMS(layer, 
-                        width, height, bbox, srsName,request.getTime()).queue());
+                supplier = () -> CommandLoadImageWMS.loadImage(layer, width, height, bbox, srsName,request.getTime());
                 break;
             case OskariLayer.TYPE_WMTS:
-                images.put(layer.getZIndex(), new CommandLoadImageWMTS(layer, width, height, bbox, srsName,
-                        request.getResolution()).queue());
+                supplier = () -> CommandLoadImageWMTS.loadImage(layer, width, height, bbox, srsName, request.getResolution(), this);
                 break;
             case OskariLayer.TYPE_ARCGIS93:
-                images.put(layer.getZIndex(), new CommandLoadImageArcGISREST(layer,
-                        width, height, bbox, srsName).queue());
+                supplier = () -> CommandLoadImageArcGISREST.loadImage(layer, width, height, bbox, srsName);
                 break;
             }
-
+            if (supplier != null) {
+                images.put(layer.getZIndex(), runImageSupplier(layer.getLayerId(), supplier));
+            }
         }
 
         return images;
+    }
+
+    public Map<Integer, Future<SimpleFeatureCollection>> initVectorLayers(PrintRequest request,
+                                                                           OskariFeatureClient featureClient) {
+        Map<Integer, Future<SimpleFeatureCollection>> featureCollections = new HashMap<>();
+
+        List<PrintLayer> requestedLayers = request.getLayers();
+        if (requestedLayers == null) {
+            return featureCollections;
+        }
+
+        CoordinateReferenceSystem crs = request.getCrs();
+        double[] bbox = request.getBoundingBox();
+        ReferencedEnvelope bbox1 = new ReferencedEnvelope(bbox[0], bbox[2], bbox[1], bbox[3], crs);
+
+        for (PrintLayer layer : requestedLayers) {
+            if (!layer.getType().equals(OskariLayer.TYPE_WFS)) {
+                continue;
+            }
+            String commandKey = Integer.toString(layer.getId());
+            Supplier<SimpleFeatureCollection> supplier = () -> CommandLoadFeatureWFS.getFeatures(featureClient, layer, bbox1, crs);
+            featureCollections.put(layer.getZIndex(), runFeatureSupplier(commandKey, supplier));
+        }
+        return featureCollections;
+    }
+
+    public Future<BufferedImage> runImageSupplier(String commandKey, Supplier<BufferedImage> supplier) {
+        try {
+            return Decorators.ofSupplier(supplier)
+                    .withThreadPoolBulkhead(bulkhead)
+                    .withTimeLimiter(timeLimiter, executor)
+                    .withCircuitBreaker(circuitBreakerRegistry.circuitBreaker(commandKey))
+                    .withRetry(retryRegistry.retry(commandKey), executor)
+                    .withFallback(throwable -> null)
+                    .get().toCompletableFuture();
+        } catch (CompletionException e) {
+            throw new RuntimeException(e.getCause());
+        }
+    }
+
+    public Future<SimpleFeatureCollection> runFeatureSupplier(String commandKey, Supplier<SimpleFeatureCollection> supplier) {
+        try {
+            return Decorators.ofSupplier(supplier)
+                    .withThreadPoolBulkhead(bulkhead)
+                    .withTimeLimiter(timeLimiter, executor)
+                    .withCircuitBreaker(circuitBreakerRegistry.circuitBreaker(commandKey))
+                    .withRetry(retryRegistry.retry(commandKey), executor)
+                    .withFallback(throwable -> new DefaultFeatureCollection())
+                    .get().toCompletableFuture();
+        } catch (CompletionException e) {
+            throw new RuntimeException(e.getCause());
+        }
+    }
+
+    public static BufferedImage loadImageFromURL(String uri, String user, String pass) {
+        LOG.debug("Loading print content from:", uri);
+        try {
+            HttpURLConnection conn = IOHelper.getConnection(uri, user, pass);
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
+                // short-circuit 404 as we get these a lot in the log
+                throw new ServiceRuntimeException("Not found");
+            }
+            try (InputStream in = new BufferedInputStream(conn.getInputStream())) {
+                return ImageIO.read(in);
+            }
+        } catch (IOException e) {
+            throw new ServiceRuntimeException(e.getMessage(), e);
+        }
     }
 }
--- service-print/src/main/java/org/oskari/print/loader/CommandLoadFeatureWFS.java
+++ service-print/src/main/java/org/oskari/print/loader/CommandLoadFeatureWFS.java
@@ -3,59 +3,17 @@ package org.oskari.print.loader;
 import java.util.Optional;
 
 import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.feature.DefaultFeatureCollection;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
 import org.oskari.print.request.PrintLayer;
 import org.oskari.service.user.UserLayerService;
 import org.oskari.service.wfs.client.OskariFeatureClient;
 
-import com.netflix.hystrix.HystrixCommand;
-import com.netflix.hystrix.HystrixCommandGroupKey;
-import com.netflix.hystrix.HystrixCommandKey;
-import com.netflix.hystrix.HystrixCommandProperties;
-import com.netflix.hystrix.HystrixThreadPoolProperties;
-
 import fi.nls.oskari.domain.map.OskariLayer;
-import fi.nls.oskari.util.PropertyUtil;
 
-public class CommandLoadFeatureWFS extends HystrixCommand<SimpleFeatureCollection> {
-    
-    private static final String GROUP_KEY = "print";
-    
-    private OskariFeatureClient featureClient;
-    private PrintLayer layer;
-    private String uuid;
-    private ReferencedEnvelope bbox;
-    private CoordinateReferenceSystem crs;
-    
-    public CommandLoadFeatureWFS(OskariFeatureClient featureClient, PrintLayer layer,
-            String uuid, ReferencedEnvelope bbox, CoordinateReferenceSystem crs) {
-        super(Setter
-                .withGroupKey(HystrixCommandGroupKey.Factory.asKey(AsyncImageLoader.GROUP_KEY))
-                .andCommandKey(HystrixCommandKey.Factory.asKey(Integer.toString(layer.getId())))
-                .andThreadPoolPropertiesDefaults(
-                        HystrixThreadPoolProperties.Setter()
-                        .withCoreSize(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.pool.size", 10))
-                        .withMaxQueueSize(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.pool.limit", 100))
-                        .withQueueSizeRejectionThreshold(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.pool.queue", 100)))
-                .andCommandPropertiesDefaults(
-                        HystrixCommandProperties.Setter()
-                        // .withExecutionTimeoutInMilliseconds(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.timeoutms", 15000))
-                        .withExecutionTimeoutInMilliseconds(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".job.timeoutms", 150_000))
-                        .withCircuitBreakerRequestVolumeThreshold(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".failrequests", 5))
-                        .withMetricsRollingStatisticalWindowInMilliseconds(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".rollingwindow", 100_000))
-                        .withCircuitBreakerSleepWindowInMilliseconds(PropertyUtil.getOptional("oskari." + GROUP_KEY + ".sleepwindow", 20_000)))
-        );
-        this.featureClient = featureClient;
-        this.layer = layer;
-        this.uuid = uuid;
-        this.bbox = bbox;
-        this.crs = crs;
-    }
-
-    @Override
-    protected SimpleFeatureCollection run() throws Exception {
+public class CommandLoadFeatureWFS {
+    public static SimpleFeatureCollection getFeatures(OskariFeatureClient featureClient, PrintLayer layer,
+            ReferencedEnvelope bbox, CoordinateReferenceSystem crs) {
         try {
             String id = layer.getLayerId();
             OskariLayer oskariLayer = layer.getOskariLayer();
@@ -66,10 +24,4 @@ public class CommandLoadFeatureWFS extends HystrixCommand<SimpleFeatureCollectio
             throw e;
         }
     }
-    
-    @Override
-    protected SimpleFeatureCollection getFallback() {
-        return new DefaultFeatureCollection();
-    }
-
 }
