db849db03d9eaecaf031ea7fc14daeee7b86221a
Pekka Helesuo <pekka.helesuo@siili.com>
2025-03-10 10:11:23 +0200

    test for returning requestparams & fix failing

--- service-routing/src/test/java/fi/nls/oskari/routing/RoutingServiceOpenTripPlannerImplTest.java
+++ service-routing/src/test/java/fi/nls/oskari/routing/RoutingServiceOpenTripPlannerImplTest.java
@@ -5,6 +5,7 @@ import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.util.IOHelper;
 import fi.nls.oskari.util.PropertyUtil;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import org.mockito.MockedStatic;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -28,8 +30,8 @@ public class RoutingServiceOpenTripPlannerImplTest {
     private static final String MAP_SRS = "EPSG:3067";
     private static final String JSON_ENCODING = "UTF-8";
 
-    private HttpURLConnection mockHttpURLConnection;
-    private MockedStatic<IOHelper> mockIOHelper;
+    protected HttpURLConnection mockHttpURLConnection;
+    protected MockedStatic<IOHelper> mockIOHelper;
     @BeforeEach
     public void initialize()throws Exception{
         PropertyUtil.addProperty("routing.srs", ROUTING_SRS);
@@ -70,6 +72,8 @@ public class RoutingServiceOpenTripPlannerImplTest {
         RouteResponse response = ROUTING_SERVICE.getRoute(routeParams);
 
         Assertions.assertTrue(response.isSuccess());
+        Assertions.assertNotNull(response.getPlan());
+        Assertions.assertNotNull(response.getRequestParameters());
     }
 
     @Test
@@ -82,6 +86,41 @@ public class RoutingServiceOpenTripPlannerImplTest {
         RouteResponse response = ROUTING_SERVICE.getRoute(routeParams);
 
         Assertions.assertFalse(response.isSuccess());
-
+        Assertions.assertTrue(response.getErrorMessage().startsWith("Validation error (SubselectionRequired@[planConnection/edges/node/legs/start])"));
+        Assertions.assertNull(response.getPlan());
+        Assertions.assertNull(response.getRequestParameters());
     }
+
+    @Test
+    public void testParseWhenParamsAreNullOrEmpty() throws Exception {
+        Assertions.assertThrows(RuntimeException.class, () -> { ROUTING_SERVICE.getRoute(null); });
+        Assertions.assertThrows(RuntimeException.class, () -> { ROUTING_SERVICE.getRoute(new RouteParams()); });
+    }
+
+    @Test
+    public void testRequestParamsReturnedOk() throws Exception {
+        String responseJson = new String(getClass().getResourceAsStream("digitransit-response-success-v2.json").readAllBytes());
+        when(IOHelper.post((String)any(), any(), (byte[])any())).thenReturn(this.mockHttpURLConnection);
+        when(IOHelper.readString((InputStream)any(), (String)any())).thenReturn(responseJson);
+
+        RouteParams routeParams = initRouteParams();
+        RouteResponse response = ROUTING_SERVICE.getRoute(routeParams);
+
+        JSONObject requestParams = response.getRequestParameters();
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        Assertions.assertNotNull(requestParams);
+        Assertions.assertEquals(requestParams.get("date"), routeParams.getDate().format(dateFormatter));
+        Assertions.assertEquals(requestParams.get("wheelchair"), routeParams.getIsWheelChair());
+        Assertions.assertEquals(requestParams.get("arriveBy"), routeParams.getIsArriveBy());
+        Assertions.assertEquals(requestParams.get("time"), routeParams.getDate().format(timeFormatter));
+        Assertions.assertEquals(requestParams.get("locale"), routeParams.getLang());
+
+        Assertions.assertEquals(routeParams.getFrom().getX(), ((JSONObject)requestParams.get("fromPlace")).get("lon"));
+        Assertions.assertEquals(routeParams.getFrom().getY(), ((JSONObject)requestParams.get("fromPlace")).get("lat"));
+        Assertions.assertEquals(routeParams.getTo().getX(), ((JSONObject)requestParams.get("toPlace")).get("lon"));
+        Assertions.assertEquals(routeParams.getTo().getY(), ((JSONObject)requestParams.get("toPlace")).get("lat"));
+    }
+
 }
