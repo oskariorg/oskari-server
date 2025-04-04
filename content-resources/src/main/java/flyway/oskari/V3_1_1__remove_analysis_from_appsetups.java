65ff772b8cc0311dd9ee4802aa8fd1969b887f0d
Pekka Helesuo <pekka.helesuo@siili.com>
2025-04-04 12:28:17 +0300

    remove analysis layer(s) from selected layers in mapfull state

--- content-resources/src/main/java/flyway/oskari/V3_1_1__remove_analysis_from_appsetups.java
+++ content-resources/src/main/java/flyway/oskari/V3_1_1__remove_analysis_from_appsetups.java
@@ -22,14 +22,75 @@ import java.util.Optional;
 public class V3_1_1__remove_analysis_from_appsetups extends BaseJavaMigration {
     private static final Logger LOG = LogFactory.getLogger(flyway.oskari.V3_0_1__infobox_colourscheme_to_theme.class);
     private static final String ANALYSIS_LAYER_PLUGIN_ID = "Oskari.mapframework.bundle.mapanalysis.plugin.AnalysisLayerPlugin";
+    private static final String ANALYSIS_LAYER_PREFIX = "analysis_";
     private static final int BATCH_SIZE = 1000;
     @Override
     public void migrate (Context context) throws Exception {
         Connection conn = context.getConnection();
+        removeAnalysisLayerFromMapfullState(conn);
         removeAnalysisLayerPluginFromMapmodules(conn);
         removeAnalyseFromAppsetupBundles(conn);
         removeAnalyseFromBundles(conn);
     }
+
+    private void removeAnalysisLayerFromMapfullState(Connection conn) throws SQLException {
+        JSONArray mapfullConfigs = getMapfullConfigsWithAnalysisLayer(conn);
+        String sql = """
+            UPDATE oskari_appsetup_bundles bundles SET state = v.state 
+            FROM (VALUES
+                ${values}
+            ) AS v(appsetup_id, bundle_id, state)
+            WHERE bundles.appsetup_id = v.appsetup_id AND bundles.bundle_id = v.bundle_id
+        """;
+
+        List<String> values = new ArrayList<>();
+        Statement statement = conn.createStatement();
+        conn.setAutoCommit(false);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.YYYY HH:mm:ss");
+        LOG.debug(OffsetDateTime.now().format(dtf) + " Starting update.");
+        for (int i = 0, counter = 0; i < mapfullConfigs.length(); i++, counter++) {
+            JSONObject mapfull = mapfullConfigs.optJSONObject(i);
+            JSONObject state = new JSONObject(mapfull.getString("state"));
+
+            if (state == null || !state.has("selectedLayers")) {
+                continue;
+            }
+
+            JSONArray selectedLayers = Optional.ofNullable(state.getJSONArray("selectedLayers")).orElse(new JSONArray());
+            JSONArray newSelectedLayers = new JSONArray();
+            for (int j = 0; j < selectedLayers.length(); j++) {
+                JSONObject layer = selectedLayers.optJSONObject(j);
+                if (layer != null) {
+                    String id = layer.get("id").toString();
+                    if (!id.startsWith(ANALYSIS_LAYER_PREFIX)) {
+                        newSelectedLayers.put(layer);
+                    }
+                }
+            }
+            state.put("selectedLayers", newSelectedLayers);
+
+            int appsetupId = mapfull.getInt("appsetup_id");
+            int bundleId = mapfull.getInt("bundle_id");
+            String valueString = "(" + appsetupId + ", " + bundleId + ", '" + state.toString() + "')";
+            values.add(valueString);
+
+            // execute once a batch size or when we reach the end
+            if (counter == BATCH_SIZE - 1 || i == mapfullConfigs.length() - 1) {
+                String query = sql.replace("${values}", String.join(",\n", values));
+                statement.addBatch(query);
+                counter = 0;
+                values.clear();;
+            }
+        }
+
+        int[] results = statement.executeBatch();
+        LOG.debug("results: ", results.length);
+        conn.commit();
+        conn.setAutoCommit(true);
+        LOG.debug(OffsetDateTime.now().format(dtf) + " Finished removing analysis layers from " + mapfullConfigs.length() + " views.");
+    }
+
 
     private void removeAnalysisLayerPluginFromMapmodules(Connection conn) throws SQLException {
         JSONArray mapfullConfigs = getMapfullConfigsWithAnalysis(conn);
@@ -91,7 +152,7 @@ public class V3_1_1__remove_analysis_from_appsetups extends BaseJavaMigration {
     }
 
     private JSONArray getMapfullConfigsWithAnalysis(Connection conn) throws SQLException {
-        final String sql = "SELECT * FROM oskari_appsetup_bundles WHERE config LIKE '%AnalysisLayerPlugin%'";
+        final String sql = "SELECT appsetup_id, bundle_id, config FROM oskari_appsetup_bundles WHERE config LIKE '%AnalysisLayerPlugin%'";
         JSONArray mapFullConfigs = new JSONArray();
 
         try (final PreparedStatement statement = conn.prepareStatement(sql)) {
@@ -102,6 +163,28 @@ public class V3_1_1__remove_analysis_from_appsetups extends BaseJavaMigration {
                     mapfullConf.put("appsetup_id", rs.getInt("appsetup_id"));
                     mapfullConf.put("bundle_id", rs.getInt("bundle_id"));
                     mapfullConf.put("config", rs.getString("config"));
+                    mapFullConfigs.put(mapfullConf);
+                }
+            } catch (JSONException e) {
+                LOG.warn("Failed to fetch configs for migration ", e);
+            }
+        }
+
+        return mapFullConfigs;
+    }
+
+    private JSONArray getMapfullConfigsWithAnalysisLayer(Connection conn) throws SQLException {
+        final String sql = "SELECT appsetup_id, bundle_id, state FROM oskari_appsetup_bundles WHERE state LIKE '%selectedLayers%id%analysis%'";
+        JSONArray mapFullConfigs = new JSONArray();
+
+        try (final PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            try (ResultSet rs = statement.executeQuery()) {
+                while(rs.next()) {
+                    JSONObject mapfullConf = new JSONObject();
+                    mapfullConf.put("appsetup_id", rs.getInt("appsetup_id"));
+                    mapfullConf.put("bundle_id", rs.getInt("bundle_id"));
+                    mapfullConf.put("state", rs.getString("state"));
                     mapFullConfigs.put(mapfullConf);
                 }
             } catch (JSONException e) {
