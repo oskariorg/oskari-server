2d775a27917d23f81277def8485ac3957874e89a
Sami Mäkinen <sami.makinen@nls.fi>
2025-10-24 19:46:48 +0300

    Updating fails due to JSONObject deserialization

--- control-myfeatures/src/test/java/org/oskari/control/myfeatures/MyFeaturesLayerHandlerTest.java
+++ control-myfeatures/src/test/java/org/oskari/control/myfeatures/MyFeaturesLayerHandlerTest.java
@@ -9,10 +9,12 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.UUID;
 
+import fi.nls.test.util.ResourceHelper;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Envelope;
 import org.oskari.map.myfeatures.service.MyFeaturesService;
@@ -122,6 +124,58 @@ public class MyFeaturesLayerHandlerTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testupdatingLayer() throws Exception {
+        String json = ResourceHelper.readStringResource("/update-payload.json", this);
+        MyFeaturesLayer layer = new MyFeaturesLayer();
+        layer.setId(UUID.fromString("d4ea8bb7-b323-4c8e-ab5d-a529ee9416f8"));
+        layer.setName("en", "foo");
+        layer.setDesc("fi", "bar");
+        layer.setOwnerUuid("foo");
+        layer.setExtent(new Envelope(0, 10, 0, 10));
 
+        User user = new User();
+        user.setUuid("foo");
 
+        MyFeaturesLayerHandler handler = new MyFeaturesLayerHandler();
+        handler.setService(
+                when(mock(MyFeaturesService.class).getLayersByOwnerUuid(user.getUuid()))
+                        .thenReturn(Arrays.asList(layer)).getMock());
+        handler.init();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ActionParameters params = mockParams(baos);
+        when(params.getUser()).thenReturn(user);
+        when(params.getPayLoad()).thenReturn(json);
+
+        handler.handlePut(params);
+        String expected = "TODO: expect something - Currently throws a parse error before we get here";
+        String actual = baos.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    private ActionParameters mockParams(ByteArrayOutputStream baos) throws IOException  {
+
+        Locale locale = mock(Locale.class);
+        when(locale.getLanguage()).thenReturn("fi");
+
+        ActionParameters params = mock(ActionParameters.class);
+        when(params.getLocale()).thenReturn(locale);
+
+        ServletOutputStream sos = spy(ServletOutputStream.class);
+        doAnswer(invocation -> {
+            baos.write(
+                    invocation.getArgument(0, byte[].class),
+                    invocation.getArgument(1, Integer.class),
+                    invocation.getArgument(2, Integer.class));
+            return null;
+
+        }).when(sos).write(any(byte[].class), anyInt(), anyInt());
+
+        HttpServletResponse servletResponse = mock(HttpServletResponse.class);
+        when(servletResponse.getOutputStream()).thenReturn(sos);
+        when(params.getResponse()).thenReturn(servletResponse);
+        return params;
+    }
 }
