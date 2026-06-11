672396f3b71bca1438a8624c20d2d26e70bd2c8d
Pekka Helesuo <pekka.helesuo@siili.com>
2026-06-11 16:11:36 +0300

    trim whitespace from metadataid

--- service-map/src/main/java/org/oskari/maplayer/model/MapLayer.java
+++ service-map/src/main/java/org/oskari/maplayer/model/MapLayer.java
@@ -176,7 +176,7 @@ public class MapLayer {
     }
 
     public void setMetadataid(String metadataid) {
-        this.metadataid = metadataid;
+        this.metadataid = metadataid == null ? null : metadataid.replaceAll("\\s+", "");
     }
 
     public Map<String, String> getParams() {
