fce6a164312d2c7c1ea5445b2a3bf7a9b8886e7b
Pekka Helesuo <pekka.helesuo@siili.com>
2026-06-12 11:31:36 +0300

    just trim start and end

--- service-map/src/main/java/org/oskari/maplayer/model/MapLayer.java
+++ service-map/src/main/java/org/oskari/maplayer/model/MapLayer.java
@@ -176,7 +176,7 @@ public class MapLayer {
     }
 
     public void setMetadataid(String metadataid) {
-        this.metadataid = metadataid == null ? null : metadataid.replaceAll("\\s+", "");
+        this.metadataid = metadataid == null ? null : metadataid.trim();
     }
 
     public Map<String, String> getParams() {
